2266453f70cbcd101d4698f3580121743673a0a7
Sergey Shalnov <shssf@users.noreply.github.com>
2020-09-05 13:27:01 -0500

    fix python path and readme (#1)

--- 0.env.sh
+++ 0.env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
+
 # We can not use common setup script because
 # using Intel Python brakes build and run procedure
 export ONEAPI_ROOT=/opt/intel/oneapi
 . ${ONEAPI_ROOT}/mkl/latest/env/vars.sh
 . ${ONEAPI_ROOT}/compiler/latest/env/vars.sh
 . ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
+
+export PYTHONPATH=$PYTHONPATH:${THEDIR}
