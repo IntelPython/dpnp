71fef1a2c6df0e351bcb7674204e39e997989fc9
Rubtsowa <mariyarubtsowa@yandex.ru>
2020-09-09 20:48:37 +0300

    change impl abs

--- dpnp/dpnp_iface_mathematical.py
+++ dpnp/dpnp_iface_mathematical.py
@@ -102,7 +102,7 @@ def absolute(input):
     input1 = dpnp.asnumpy(input) if is_input_dparray else input
 
     # TODO need to put dparray memory into NumPy call
-    result_numpy = numpy.abs(input)
+    result_numpy = numpy.abs(input1)
     result = result_numpy
     if isinstance(result, numpy.ndarray):
         result = dparray(result_numpy.shape, dtype=result_numpy.dtype)
