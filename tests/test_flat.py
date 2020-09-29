694d23460aa08b4dc28f03f65ed085269899289e
Rubtsowa <36762665+Rubtsowa@users.noreply.github.com>
2020-09-29 20:34:58 +0300

    add flat (#84)

---
+++ tests/test_flat.py
@@ -0,0 +1,41 @@
+import pytest
+
+import dpnp as inp
+
+import numpy
+
+
+@pytest.mark.parametrize("type",
+                         [numpy.int64],
+                         ids=['int64'])
+def test_flat(type):
+    a = numpy.array([1, 0, 2, -3, -1, 2, 21, -9])
+    ia = inp.array(a)
+
+    result = ia.flat[0]
+    expected = a.flat[0]
+    numpy.testing.assert_array_equal(expected, result)
+
+
+@pytest.mark.parametrize("type",
+                         [numpy.int64],
+                         ids=['int64'])
+def test_flat2(type):
+    a = numpy.arange(1, 7).reshape(2, 3)
+    ia = inp.array(a)
+
+    result = ia.flat[3]
+    expected = a.flat[3]
+    numpy.testing.assert_array_equal(expected, result)
+
+
+@pytest.mark.parametrize("type",
+                         [numpy.int64],
+                         ids=['int64'])
+def test_flat3(type):
+    a = numpy.arange(1, 7).reshape(2, 3).T
+    ia = inp.array(a)
+
+    result = ia.flat[3]
+    expected = a.flat[3]
+    numpy.testing.assert_array_equal(expected, result)
