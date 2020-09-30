8c1a580380a813241b942a5129837856a3403b09
Vyacheslav Smirnov <vyacheslav.s.smirnov@intel.com>
2020-09-30 17:04:42 +0300

    Comment extra tests, leave only import

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -35,18 +35,18 @@ clang++ --version
 echo ${LD_LIBRARY_PATH}
 echo ${PATH}
 
-readelf -d ${PROJ_ROOT}/dpnp/libdpnp_backend_c.so
-ldd ${PROJ_ROOT}/dpnp/libdpnp_backend_c.so
+# readelf -d ${PROJ_ROOT}/dpnp/libdpnp_backend_c.so
+# ldd ${PROJ_ROOT}/dpnp/libdpnp_backend_c.so
 
-readelf -d ${PROJ_ROOT}/dpnp/dparray.cpython*
-ldd ${PROJ_ROOT}/dpnp/dparray.cpython*
+# readelf -d ${PROJ_ROOT}/dpnp/dparray.cpython*
+# ldd ${PROJ_ROOT}/dpnp/dparray.cpython*
 
-echo =========example3==============
-clang++ -g -fPIC ${PROJ_ROOT}/examples/example3.cpp -I${PROJ_ROOT}/dpnp -I${PROJ_ROOT}/dpnp/backend -L${PROJ_ROOT}/dpnp -Wl,-rpath='$ORIGIN'/dpnp -ldpnp_backend_c -o example3
-./example3
+# echo =========example3==============
+# clang++ -g -fPIC ${PROJ_ROOT}/examples/example3.cpp -I${PROJ_ROOT}/dpnp -I${PROJ_ROOT}/dpnp/backend -L${PROJ_ROOT}/dpnp -Wl,-rpath='$ORIGIN'/dpnp -ldpnp_backend_c -o example3
+# ./example3
 
-echo
-echo =========example1==============
-python examples/example1.py
+# echo
+# echo =========example1==============
+# python examples/example1.py
 
 python -c "import dpnp"
