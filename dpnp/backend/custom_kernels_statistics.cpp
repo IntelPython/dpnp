beae23dde4af729f1a395237a49328db81dcbbba
densmirn <53897164+densmirn@users.noreply.github.com>
2020-10-06 20:06:19 +0300

    MEAN add MKL kernel (#119)
    * MEAN add MKL kernel

--- dpnp/backend/custom_kernels_statistics.cpp
+++ dpnp/backend/custom_kernels_statistics.cpp
@@ -25,6 +25,7 @@
 
 #include <iostream>
 #include <mkl_blas_sycl.hpp>
+#include <mkl_stats_sycl.hpp>
 
 #include <backend_iface.hpp>
 #include "backend_pstl.hpp"
@@ -32,6 +33,7 @@
 #include "queue_sycl.hpp"
 
 namespace mkl_blas = oneapi::mkl::blas::row_major;
+namespace mkl_stats = oneapi::mkl::stats;
 
 template <typename _DataType>
 class custom_cov_c_kernel;
@@ -198,17 +200,28 @@ void custom_mean_c(void* array1_in, void* result1, const size_t* shape, size_t n
         return;
     }
 
-    _DataType* sum = reinterpret_cast<_DataType*>(dpnp_memory_alloc_c(1 * sizeof(_DataType)));
+    if constexpr (std::is_same<_DataType, double>::value || std::is_same<_DataType, float>::value)
+    {
+        _ResultType* array = reinterpret_cast<_DataType*>(array1_in);
 
-    custom_sum_c<_DataType>(array1_in, sum, size);
+        // https://docs.oneapi.com/versions/latest/onemkl/mkl-stats-make_dataset.html
+        auto dataset = mkl_stats::make_dataset<mkl_stats::layout::row_major>(1, size, array);
 
-    result[0] = static_cast<_ResultType>(sum[0]) / static_cast<_ResultType>(size);
+        // https://docs.oneapi.com/versions/latest/onemkl/mkl-stats-mean.html
+        cl::sycl::event event = mkl_stats::mean(DPNP_QUEUE, dataset, result);
 
-    dpnp_memory_free_c(sum);
+        event.wait();
+    }
+    else
+    {
+        _DataType* sum = reinterpret_cast<_DataType*>(dpnp_memory_alloc_c(1 * sizeof(_DataType)));
 
-#if 0
-    std::cout << "mean result " << result[0] << "\n";
-#endif
+        custom_sum_c<_DataType>(array1_in, sum, size);
+
+        result[0] = static_cast<_ResultType>(sum[0]) / static_cast<_ResultType>(size);
+
+        dpnp_memory_free_c(sum);
+    }
 }
 
 template void custom_mean_c<double, double>(
