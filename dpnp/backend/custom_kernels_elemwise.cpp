f25ab0227e3ba095ca9f6e23c335d9a6602741d1
Denis Smirnov <denis.smirnov@intel.com>
2020-10-09 20:42:24 +0300

    ELEMWISE 1arg_2types fix indentations

--- dpnp/backend/custom_kernels_elemwise.cpp
+++ dpnp/backend/custom_kernels_elemwise.cpp
@@ -67,46 +67,46 @@
 
 #include <custom_1arg_2type_tbl.hpp>
 
-#define MACRO_CUSTOM_1ARG_2TYPES_MKL_OP(__name__, __operation__, __mkl_operation__)                                        \
-    template <typename _KernelNameSpecialization>                                                                          \
-    class custom_elemwise_##__name__##_c_kernel;                                                                           \
-                                                                                                                           \
-    template <typename _DataType_input, typename _DataType_output>                                                         \
-    void custom_elemwise_##__name__##_c(void* array1_in, void* result1, size_t size)                                       \
-    {                                                                                                                      \
-        cl::sycl::event event;                                                                                             \
-        _DataType_input* array1 = reinterpret_cast<_DataType_input*>(array1_in);                                           \
-        _DataType_output* result = reinterpret_cast<_DataType_output*>(result1);                                           \
-                                                                                                                           \
-        if constexpr (std::is_same<_DataType_input, double>::value || std::is_same<_DataType_input, float>::value)         \
-        {                                                                                                                  \
-            event = __mkl_operation__(DPNP_QUEUE, size, array1, result);                                                   \
-        }                                                                                                                  \
-        else                                                                                                               \
-        {                                                                                                                  \
-            cl::sycl::range<1> gws(size);                                                                                  \
-            auto kernel_parallel_for_func = [=](cl::sycl::id<1> global_id) {                                               \
-                size_t i = global_id[0]; /*for (size_t i = 0; i < size; ++i)*/                                             \
-                {                                                                                                          \
-                    _DataType_output input_elem = array1[i];                                                               \
-                    result[i] = __operation__(input_elem);                                                                 \
-                }                                                                                                          \
-            };                                                                                                             \
-                                                                                                                           \
-            auto kernel_func = [&](cl::sycl::handler& cgh) {                                                               \
-                cgh.parallel_for<class custom_elemwise_##__name__##_c_kernel<_DataType_input>>(gws,                        \
-                                                                                               kernel_parallel_for_func);  \
-            };                                                                                                             \
-                                                                                                                           \
-            event = DPNP_QUEUE.submit(kernel_func);                                                                        \
-        }                                                                                                                  \
-                                                                                                                           \
-        event.wait();                                                                                                      \
-    }                                                                                                                      \
-                                                                                                                           \
-    template void custom_elemwise_##__name__##_c<double, double>(void* array1_in, void* result1, size_t size);             \
-    template void custom_elemwise_##__name__##_c<float, float>(void* array1_in, void* result1, size_t size);               \
-    template void custom_elemwise_##__name__##_c<long, double>(void* array1_in, void* result1, size_t size);               \
+#define MACRO_CUSTOM_1ARG_2TYPES_MKL_OP(__name__, __operation__, __mkl_operation__)                                    \
+    template <typename _KernelNameSpecialization>                                                                      \
+    class custom_elemwise_##__name__##_c_kernel;                                                                       \
+                                                                                                                       \
+    template <typename _DataType_input, typename _DataType_output>                                                     \
+    void custom_elemwise_##__name__##_c(void* array1_in, void* result1, size_t size)                                   \
+    {                                                                                                                  \
+        cl::sycl::event event;                                                                                         \
+        _DataType_input* array1 = reinterpret_cast<_DataType_input*>(array1_in);                                       \
+        _DataType_output* result = reinterpret_cast<_DataType_output*>(result1);                                       \
+                                                                                                                       \
+        if constexpr (std::is_same<_DataType_input, double>::value || std::is_same<_DataType_input, float>::value)     \
+        {                                                                                                              \
+            event = __mkl_operation__(DPNP_QUEUE, size, array1, result);                                               \
+        }                                                                                                              \
+        else                                                                                                           \
+        {                                                                                                              \
+            cl::sycl::range<1> gws(size);                                                                              \
+            auto kernel_parallel_for_func = [=](cl::sycl::id<1> global_id) {                                           \
+                size_t i = global_id[0]; /*for (size_t i = 0; i < size; ++i)*/                                         \
+                {                                                                                                      \
+                    _DataType_output input_elem = array1[i];                                                           \
+                    result[i] = __operation__(input_elem);                                                             \
+                }                                                                                                      \
+            };                                                                                                         \
+                                                                                                                       \
+            auto kernel_func = [&](cl::sycl::handler& cgh) {                                                           \
+                cgh.parallel_for<class custom_elemwise_##__name__##_c_kernel<_DataType_input>>(                        \
+                    gws, kernel_parallel_for_func);                                                                    \
+            };                                                                                                         \
+                                                                                                                       \
+            event = DPNP_QUEUE.submit(kernel_func);                                                                    \
+        }                                                                                                              \
+                                                                                                                       \
+        event.wait();                                                                                                  \
+    }                                                                                                                  \
+                                                                                                                       \
+    template void custom_elemwise_##__name__##_c<double, double>(void* array1_in, void* result1, size_t size);         \
+    template void custom_elemwise_##__name__##_c<float, float>(void* array1_in, void* result1, size_t size);           \
+    template void custom_elemwise_##__name__##_c<long, double>(void* array1_in, void* result1, size_t size);           \
     template void custom_elemwise_##__name__##_c<int, double>(void* array1_in, void* result1, size_t size);
 
 #include <custom_1arg_2type_mkl_tbl.hpp>
