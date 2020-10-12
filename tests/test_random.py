2c31c2b147f4e3f71140d97a680c9508d96f5828
Samir Nasibli <samir.nasibli@intel.com>
2020-10-11 21:04:21 -0500

    tests: more tests for random funcs
    * added test_randn_normal_distribution
    
    * added test_radnom_seed
    
    * skipped tests
    
    * scipy import under the coment currently : TODO

--- tests/test_random.py
+++ tests/test_random.py
@@ -2,7 +2,8 @@ import pytest
 
 import dpnp.random
 import numpy
-
+# from scipy import stats
+from numpy.testing import assert_array_equal
 
 @pytest.mark.parametrize("func",
                          [dpnp.random.rand,
@@ -49,3 +50,36 @@ def test_random_check_otput(func):
     for i in range(res.size):
         assert res[i] >= 0.0
         assert res[i] < 1.0
+
+
+def test_randn_normal_distribution():
+    pts = 1000
+    alpha = 0.05
+    dpnp.random.seed(28041990)
+    x = dpnp.random.randn(pts)
+    _, p = stats.normaltest(x)
+    assert p > alpha
+
+
+@pytest.mark.parametrize("func",
+                         [dpnp.random.random,
+                          dpnp.random.random_sample,
+                          dpnp.random.randf,
+                          dpnp.random.sample,
+                          dpnp.random.rand],
+                         ids=['random', 'random_sample',
+                              'randf', 'sample',
+                              'rand'])
+def test_radnom_seed(func):
+    seed = 28041990
+    size = 100
+    shape = (100, 1)
+    if func in [dpnp.random.rand, dpnp.random.randn]:
+        args = size
+    else:
+        args = shape
+    dpnp.random.seed(seed)
+    a1 = func(args)
+    dpnp.random.seed(seed)
+    a2 = func(args)
+    assert_array_equal(a1, a2)
