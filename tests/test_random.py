90c2a97298b4eec7e5922a6494cb16fce4c521a7
Samir Nasibli <samir.nasibli@intel.com>
2020-10-14 07:37:16 -0500

    minor fixes for tests/test_random.py
    * added comments for test_randn_normal_distribution
    
     * using assert_allclose in test_radnom_seed

--- tests/test_random.py
+++ tests/test_random.py
@@ -3,7 +3,7 @@ import pytest
 import dpnp.random
 import numpy
 # from scipy import stats
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_allclose
 
 @pytest.mark.parametrize("func",
                          [dpnp.random.rand,
@@ -53,11 +53,23 @@ def test_random_check_otput(func):
 
 
 def test_randn_normal_distribution():
+    """ Check if the sample obtained from the dpnp.random.randn differs from
+    the normal distribution.
+    Using ``scipy.stats.normaltest``.
+
+    It is based on D’Agostino and Pearson’s test that combines skew
+    and kurtosis to produce an omnibus test of normality,
+    see: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.normaltest.html
+    """
     pts = 1000
     alpha = 0.05
     dpnp.random.seed(28041990)
     x = dpnp.random.randn(pts)
     _, p = stats.normaltest(x)
+    # null hypothesis: x comes from a normal distribution.
+    # The p-value is interpreted against an alpha of 5% and finds that the test
+    # dataset does not significantly deviate from normal.
+    # If p > alpha, the null hypothesis cannot be rejected.
     assert p > alpha
 
 
@@ -82,4 +94,4 @@ def test_radnom_seed(func):
     a1 = func(args)
     dpnp.random.seed(seed)
     a2 = func(args)
-    assert_array_equal(a1, a2)
+    assert_allclose(a1, a2, rtol=1e-07, atol=0)
