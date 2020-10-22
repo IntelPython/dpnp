f981f750cdc52c4162aed26df3180ebe503eb4b3
Rubtsowa <36762665+Rubtsowa@users.noreply.github.com>
2020-10-22 21:14:41 +0300

    add kernel for linalg.det() (#177)
    Co-authored-by: Sergey Shalnov <shssf@users.noreply.github.com>

--- dpnp/backend/backend_iface.hpp
+++ dpnp/backend/backend_iface.hpp
@@ -231,6 +231,22 @@ INP_DLLEXPORT void custom_cov_c(void* array1_in, void* result1, size_t nrows, si
 
 /**
  * @ingroup BACKEND_API
+ * @brief math library implementation of det function
+ *
+ * @param [in]  array   Input array with data.
+ *
+ * @param [out] result  Output array.
+ *
+ * @param [in]  shape   Shape of input array.
+ *
+ * @param [in]  ndim    Number of elements in shape.
+ *
+ */
+template <typename _DataType>
+INP_DLLEXPORT void custom_det_c(void* array1_in, void* result1, size_t* shape, size_t ndim);
+
+/**
+ * @ingroup BACKEND_API
  * @brief math library implementation of matrix_rank function
  *
  * @param [in]  array   Input array with data.
