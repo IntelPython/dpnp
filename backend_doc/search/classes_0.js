713e552538dd500c06df4f2818c2b3c246667091
shssf <shssf@users.noreply.github.com>
2020-11-16 18:21:40 +0000

    deploy: 2aa9db176fa14d2e8f04fe3403e336caf33a760c

--- backend_doc/search/classes_0.js
+++ backend_doc/search/classes_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['_5fargsort_5fless_175',['_argsort_less',['../struct__argsort__less.html',1,'']]],
-  ['_5fsort_5fless_176',['_sort_less',['../struct__sort__less.html',1,'']]]
+  ['_5fargsort_5fless_176',['_argsort_less',['../struct__argsort__less.html',1,'']]],
+  ['_5fsort_5fless_177',['_sort_less',['../struct__sort__less.html',1,'']]]
 ];
