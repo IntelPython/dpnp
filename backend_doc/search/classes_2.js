c33cb6e396ea1c860cddd1289a43543780c17f26
shssf <shssf@users.noreply.github.com>
2020-11-16 16:17:15 +0000

    deploy: 7ce1bf5a047fd43e226c257dc53248dcaf1705c3

--- backend_doc/search/classes_2.js
+++ backend_doc/search/classes_2.js
@@ -1,18 +1,18 @@
 var searchData=
 [
-  ['custom_5fargmax_5fc_5fkernel_177',['custom_argmax_c_kernel',['../classcustom__argmax__c__kernel.html',1,'']]],
-  ['custom_5fargmin_5fc_5fkernel_178',['custom_argmin_c_kernel',['../classcustom__argmin__c__kernel.html',1,'']]],
-  ['custom_5fargsort_5fc_5fkernel_179',['custom_argsort_c_kernel',['../classcustom__argsort__c__kernel.html',1,'']]],
-  ['custom_5fcholesky_5fc_5fkernel_180',['custom_cholesky_c_kernel',['../classcustom__cholesky__c__kernel.html',1,'']]],
-  ['custom_5fcov_5fc_5fkernel_181',['custom_cov_c_kernel',['../classcustom__cov__c__kernel.html',1,'']]],
-  ['custom_5fdet_5fc_5fkernel_182',['custom_det_c_kernel',['../classcustom__det__c__kernel.html',1,'']]],
-  ['custom_5felemwise_5fabsolute_5fc_5fkernel_183',['custom_elemwise_absolute_c_kernel',['../classcustom__elemwise__absolute__c__kernel.html',1,'']]],
-  ['custom_5felemwise_5ftranspose_5fc_5fkernel_184',['custom_elemwise_transpose_c_kernel',['../classcustom__elemwise__transpose__c__kernel.html',1,'']]],
-  ['custom_5fmatrix_5frank_5fc_5fkernel_185',['custom_matrix_rank_c_kernel',['../classcustom__matrix__rank__c__kernel.html',1,'']]],
-  ['custom_5fmax_5fc_5fkernel_186',['custom_max_c_kernel',['../classcustom__max__c__kernel.html',1,'']]],
-  ['custom_5fmin_5fc_5fkernel_187',['custom_min_c_kernel',['../classcustom__min__c__kernel.html',1,'']]],
-  ['custom_5fprod_5fc_5fkernel_188',['custom_prod_c_kernel',['../classcustom__prod__c__kernel.html',1,'']]],
-  ['custom_5fsort_5fc_5fkernel_189',['custom_sort_c_kernel',['../classcustom__sort__c__kernel.html',1,'']]],
-  ['custom_5fsum_5fc_5fkernel_190',['custom_sum_c_kernel',['../classcustom__sum__c__kernel.html',1,'']]],
-  ['custom_5fvar_5fc_5fkernel_191',['custom_var_c_kernel',['../classcustom__var__c__kernel.html',1,'']]]
+  ['custom_5fargmax_5fc_5fkernel_178',['custom_argmax_c_kernel',['../classcustom__argmax__c__kernel.html',1,'']]],
+  ['custom_5fargmin_5fc_5fkernel_179',['custom_argmin_c_kernel',['../classcustom__argmin__c__kernel.html',1,'']]],
+  ['custom_5fargsort_5fc_5fkernel_180',['custom_argsort_c_kernel',['../classcustom__argsort__c__kernel.html',1,'']]],
+  ['custom_5fcholesky_5fc_5fkernel_181',['custom_cholesky_c_kernel',['../classcustom__cholesky__c__kernel.html',1,'']]],
+  ['custom_5fcov_5fc_5fkernel_182',['custom_cov_c_kernel',['../classcustom__cov__c__kernel.html',1,'']]],
+  ['custom_5fdet_5fc_5fkernel_183',['custom_det_c_kernel',['../classcustom__det__c__kernel.html',1,'']]],
+  ['custom_5felemwise_5fabsolute_5fc_5fkernel_184',['custom_elemwise_absolute_c_kernel',['../classcustom__elemwise__absolute__c__kernel.html',1,'']]],
+  ['custom_5felemwise_5ftranspose_5fc_5fkernel_185',['custom_elemwise_transpose_c_kernel',['../classcustom__elemwise__transpose__c__kernel.html',1,'']]],
+  ['custom_5fmatrix_5frank_5fc_5fkernel_186',['custom_matrix_rank_c_kernel',['../classcustom__matrix__rank__c__kernel.html',1,'']]],
+  ['custom_5fmax_5fc_5fkernel_187',['custom_max_c_kernel',['../classcustom__max__c__kernel.html',1,'']]],
+  ['custom_5fmin_5fc_5fkernel_188',['custom_min_c_kernel',['../classcustom__min__c__kernel.html',1,'']]],
+  ['custom_5fprod_5fc_5fkernel_189',['custom_prod_c_kernel',['../classcustom__prod__c__kernel.html',1,'']]],
+  ['custom_5fsort_5fc_5fkernel_190',['custom_sort_c_kernel',['../classcustom__sort__c__kernel.html',1,'']]],
+  ['custom_5fsum_5fc_5fkernel_191',['custom_sum_c_kernel',['../classcustom__sum__c__kernel.html',1,'']]],
+  ['custom_5fvar_5fc_5fkernel_192',['custom_var_c_kernel',['../classcustom__var__c__kernel.html',1,'']]]
 ];
