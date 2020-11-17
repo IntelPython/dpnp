0c6a2222d639c379a784bacb6a9635e035d6653f
Denis Smirnov <denis.smirnov@intel.com>
2020-11-17 10:20:40 +0300

    DOCS Enable block limitations in docstrings

--- dpnp/dpnp_iface_statistics.py
+++ dpnp/dpnp_iface_statistics.py
@@ -365,6 +365,10 @@ def cov(m, y=None, rowvar=True, bias=False, ddof=None, fweights=None, aweights=N
     --------
     corrcoef : Normalized covariance matrix
 
+    Limitations
+    -----------
+    Some limitations.
+
     Notes
     -----
     Assume that the observations are in the columns of the observation
