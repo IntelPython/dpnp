9c20cdc03ca2cdcd83f494a9f1bb6e4662bb5deb
shssf <shssf@users.noreply.github.com>
2020-11-21 02:17:11 +0000

    deploy: 8801c76ca980482a4b3777d35be29758d33f77f9

--- backend_doc/search/classes_0.js
+++ backend_doc/search/classes_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['_5fargsort_5fless_185',['_argsort_less',['../struct__argsort__less.html',1,'']]],
-  ['_5fsort_5fless_186',['_sort_less',['../struct__sort__less.html',1,'']]]
+  ['_5fargsort_5fless_200',['_argsort_less',['../struct__argsort__less.html',1,'']]],
+  ['_5fsort_5fless_201',['_sort_less',['../struct__sort__less.html',1,'']]]
 ];
