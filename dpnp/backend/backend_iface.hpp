81d8b8ae0731cdda631294ef72b8665e0608040d
Samir Nasibli <samir.nasibli@intel.com>
2020-11-25 15:26:01 -0600

    API: changed backend multinomial function api

--- dpnp/backend/backend_iface.hpp
+++ dpnp/backend/backend_iface.hpp
@@ -817,17 +817,23 @@ INP_DLLEXPORT void custom_rng_lognormal_c(void* result, _DataType mean, _DataTyp
  * @ingroup BACKEND_API
  * @brief math library implementation of random number generator (multinomial distribution)
  *
- * @param [in]  size     Number of elements in `result` arrays.
+ * @param [in]  size          Number of elements in `result` arrays.
  *
- * @param [in]  ntrial   Number of independent trials.
+ * @param [in]  ntrial        Number of independent trials.
  *
- * @param [in]  p_vector Probability vector of possible outcomes (k length).
+ * @param [in]  p_vector      Probability vector of possible outcomes (k length).
  *
- * @param [out] result   Output array.
+ * @param [in]  p_vector_size Length of `p_vector`.
+ *
+ * @param [out] result        Output array.
  *
  */
 template <typename _DataType>
-INP_DLLEXPORT void custom_rng_multinomial_c(void* result, int ntrial, std::vector<double>& p_vector, size_t size);
+INP_DLLEXPORT void custom_rng_multinomial_ccustom_rng_multinomial_c(void* result,
+                                                                    int ntrial,
+                                                                    const double* p_vector,
+                                                                    const size_t p_vector_size,
+                                                                    size_t size);
 
 /**
  * @ingroup BACKEND_API
