66d0ce46a99bf15b61c725e2e3e6eebe5e7b8ab1
Sergey Shalnov <shssf@users.noreply.github.com>
2020-12-01 08:49:05 -0600

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -12,11 +12,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-import comparison_generator
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath('.'))
+import comparison_generator
 
 
 # Generate comparison table.
