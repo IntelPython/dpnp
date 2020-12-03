0f80c61df226ac9cd78de489107a14ea1d29c5f7
Alexander-Makaryev <alexander.makaryev@gmail.com>
2020-12-03 06:42:36 -0600

    hardcode math lib paths

--- utils/dpnp_build_utils.py
+++ utils/dpnp_build_utils.py
@@ -218,7 +218,8 @@ def _find_mathlib_in_mathlib_root(verbose=False):
     rel_lib_paths = ["libmkl_sycl.so"]
     rel_libdir_path = os.path.join("lib", "intel64")
 
-    return find_library("MKLROOT", rel_header_paths, rel_lib_paths, rel_libdir_path=rel_libdir_path, verbose=verbose)
+    # return find_library("MKLROOT", rel_header_paths, rel_lib_paths, rel_libdir_path=rel_libdir_path, verbose=verbose)
+    return (["/opt/intel/oneapi/mkl/latest/include"], ["/opt/intel/oneapi/mkl/latest/lib/intel64"])
 
 
 def find_mathlib(verbose=False):
