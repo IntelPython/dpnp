9a8771c2ec249f1d32116450446ab486c54eeb77
Alexander-Makaryev <40917969+Alexander-Makaryev@users.noreply.github.com>
2020-12-03 21:14:21 +0300

    azure CI oneAPI package names changes (#341)
    * package names changes

--- utils/dpnp_build_utils.py
+++ utils/dpnp_build_utils.py
@@ -216,9 +216,11 @@ def _find_mathlib_in_mathlib_root(verbose=False):
     """
     rel_header_paths = ["mkl.h"]
     rel_lib_paths = ["libmkl_sycl.so"]
-    rel_libdir_path = os.path.join("lib", "intel64")
+    rel_include_path = os.path.join("latest", "include") 
+    rel_libdir_path = os.path.join("latest", "lib", "intel64")
 
-    return find_library("MKLROOT", rel_header_paths, rel_lib_paths, rel_libdir_path=rel_libdir_path, verbose=verbose)
+    return find_library("MKLROOT", rel_header_paths, rel_lib_paths, rel_include_path=rel_include_path,
+                        rel_libdir_path=rel_libdir_path, verbose=verbose)
 
 
 def find_mathlib(verbose=False):
