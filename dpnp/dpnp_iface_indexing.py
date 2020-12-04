672a9722e4738fc7d6ed3c6165fbe04569c56667
Alexander-Makaryev <alexander.makaryev@gmail.com>
2020-12-04 04:54:16 -0600

    style

--- dpnp/dpnp_iface_indexing.py
+++ dpnp/dpnp_iface_indexing.py
@@ -140,8 +140,9 @@ def diag_indices_from(arr):
         # original limitation
         # For more than d=2, the strided formula is only valid for arrays with
         # all dimensions equal, so we check first.
-        if not numpy.alltrue(numpy.diff(arr.shape) == 0): # TODO: replace alltrue and diff funcs with dpnp own ones
-            checker_throw_value_error("diag_indices_from", "arr.shape", arr.shape, "All dimensions of input must be of equal length")
+        if not numpy.alltrue(numpy.diff(arr.shape) == 0):  # TODO: replace alltrue and diff funcs with dpnp own ones
+            checker_throw_value_error("diag_indices_from", "arr.shape", arr.shape,
+                                      "All dimensions of input must be of equal length")
 
         return dpnp_diag_indices(arr.shape[0], arr.ndim)
 
