b0624174e190faabf06f388b5df36e5fb869eecc
Rubtsowa <36762665+Rubtsowa@users.noreply.github.com>
2020-12-08 22:27:00 +0300

    Tril indices from (#384)
    * add tril_indices

---
+++ tests/test_indexing.py
@@ -0,0 +1,38 @@
+import pytest
+
+import dpnp
+
+import numpy
+
+
+@pytest.mark.parametrize("m",
+                         [None, 0, 1, 2, 3, 4],
+                         ids=['None', '0', '1', '2', '3', '4'])
+@pytest.mark.parametrize("k",
+                         [0, 1, 2, 3, 4, 5],
+                         ids=['0', '1', '2', '3', '4', '5'])
+@pytest.mark.parametrize("n",
+                         [1, 2, 3, 4, 5, 6],
+                         ids=['1', '2', '3', '4', '5', '6'])
+def test_tril_indices(n, k, m):
+    result = dpnp.tril_indices(n, k, m)
+    expected = numpy.tril_indices(n, k, m)
+    numpy.testing.assert_array_equal(expected, result)
+
+
+@pytest.mark.parametrize("k",
+                         [0, 1, 2, 3, 4, 5],
+                         ids=['0', '1', '2', '3', '4', '5'])
+@pytest.mark.parametrize("array",
+                         [[[0, 0], [0, 0]],
+                          [[1, 2], [1, 2]],
+                          [[1, 2], [3, 4]],],
+                         ids=['[[0, 0], [0, 0]]',
+                              '[[1, 2], [1, 2]]',
+                              '[[1, 2], [3, 4]]'])
+def test_tril_indices_from(array, k):
+    a = numpy.array(array)
+    ia = dpnp.array(a)
+    result = dpnp.tril_indices_from(ia, k)
+    expected = numpy.tril_indices_from(a, k)
+    numpy.testing.assert_array_equal(expected, result)
