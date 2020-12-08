579e2466961d37468863ae83e042c1de55db5421
sergey shalnov <sergey.shalnov@intel.com>
2020-12-08 11:10:35 -0600

    change comment

--- scripts/install_system_deps.sh
+++ scripts/install_system_deps.sh
@@ -11,24 +11,24 @@ rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 sudo add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"
 sudo apt-get update
 
-echo ========================= install Intel OneAPI ===============================
+echo ========================= install Intel OneAPI ===========================
 sudo apt-get install intel-oneapi-mkl       \
                      intel-oneapi-mkl-devel \
                      intel-oneapi-dpcpp-cpp-compiler
 
-echo ========================= list /opt/intel/oneapi/ ===============================
+echo ========================= list /opt/intel/oneapi/ ========================
 ls -l /opt/intel/oneapi/
 
-echo ========================= install Python 3.8 ===============================
+echo ========================= Change python3 to python executanble name ======
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
-echo ========================= Python version ===============================
+echo ========================= Python version =================================
 python --version
 python3 --version
 
 echo ========================= install valgrind ===============================
 sudo apt-get install valgrind
 
-echo ========================= install/delete libstdc++-dev ===============================
+echo ========================= install/delete libstdc++-dev ===================
 sudo apt remove -y gcc-7 g++-7 gcc-8 g++-8 gcc-10 g++-10
 # oneapi beta 10 can not work with libstdc++-10-dev 
 sudo apt remove -y libstdc++-10-dev
@@ -36,7 +36,7 @@ sudo apt autoremove
 sudo apt install --reinstall -y gcc-9 g++-9 libstdc++-9-dev
 
 
-echo ========================= SW versions ===============================
+echo ========================= SW versions ====================================
 sudo apt list --installed
 
 gcc --version
