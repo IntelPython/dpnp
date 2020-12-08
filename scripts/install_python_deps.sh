a68e97f742516466c4456ef530e02f7a61584f87
sergey shalnov <sergey.shalnov@intel.com>
2020-12-08 11:29:20 -0600

    get_include() info check added

--- scripts/install_python_deps.sh
+++ scripts/install_python_deps.sh
@@ -16,6 +16,6 @@ conda list
 python --version
 which python
 
-python -c "import numpy as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\")"
-python -c "import dpctl as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\")"
-python -c "import dpnp as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\")"
+python -c "import numpy as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\nsw.get_include={sw.get_include()}\")"
+python -c "import dpctl as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\nsw.get_include={sw.get_include()}\")"
+python -c "import dpnp as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\nsw.get_include={sw.get_include()}\")"
