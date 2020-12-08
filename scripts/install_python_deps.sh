49a8bff0148c83b295cab5af18c483ec53fa795b
sergey shalnov <sergey.shalnov@intel.com>
2020-12-08 11:05:28 -0600

    split installing prerequisites

---
+++ scripts/install_python_deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
+
+echo +++++++++++++++++++++++++ Python prerequisites +++++++++++++++++++++++++++
+
+echo ========================= PIP3: install prerequisites ===============================
+pip3 install numpy cython pytest pytest-valgrind
+
+echo ========================= Conda: install prerequisites ===============================
+conda install conda-build
+
+echo ========================= SW versions ===============================
+conda list
+
+python --version
+which python
+
+python -c "import numpy as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\")"
+python -c "import dpctl as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\")"
+python -c "import dpnp as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\")"
---
+++ scripts/install_system_deps.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
+
+echo +++++++++++++++++++++++++ System prerequisites +++++++++++++++++++++++++++
+
+echo ========================= install Intel repository =======================
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+sudo add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"
+sudo apt-get update
+
+echo ========================= install Intel OneAPI ===============================
+sudo apt-get install intel-oneapi-mkl       \
+                     intel-oneapi-mkl-devel \
+                     intel-oneapi-dpcpp-cpp-compiler
+
+echo ========================= list /opt/intel/oneapi/ ===============================
+ls -l /opt/intel/oneapi/
+
+echo ========================= install Python 3.8 ===============================
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+echo ========================= Python version ===============================
+python --version
+python3 --version
+
+echo ========================= install valgrind ===============================
+sudo apt-get install valgrind
+
+echo ========================= install/delete libstdc++-dev ===============================
+sudo apt remove -y gcc-7 g++-7 gcc-8 g++-8 gcc-10 g++-10
+# oneapi beta 10 can not work with libstdc++-10-dev 
+sudo apt remove -y libstdc++-10-dev
+sudo apt autoremove
+sudo apt install --reinstall -y gcc-9 g++-9 libstdc++-9-dev
+
+
+echo ========================= SW versions ===============================
+sudo apt list --installed
+
+gcc --version
+which gcc
+
+clang++ --version
+which clang++
+
+dpcpp --version
+which dpcpp
+
+valgrind --version
+which valgrind
