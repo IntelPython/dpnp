cbd4a844e9ae5695d708560a9012b53d2d3852e9
Rubtsowa <mariyarubtsowa@yandex.ru>
2020-12-09 13:01:03 +0300

    add triu_indices()

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -36,3 +36,18 @@ def test_tril_indices_from(array, k):
     result = dpnp.tril_indices_from(ia, k)
     expected = numpy.tril_indices_from(a, k)
     numpy.testing.assert_array_equal(expected, result)
+
+
+@pytest.mark.parametrize("m",
+                         [None, 0, 1, 2, 3, 4],
+                         ids=['None', '0', '1', '2', '3', '4'])
+@pytest.mark.parametrize("k",
+                         [0, 1, 2, 3, 4, 5],
+                         ids=['0', '1', '2', '3', '4', '5'])
+@pytest.mark.parametrize("n",
+                         [1, 2, 3, 4, 5, 6],
+                         ids=['1', '2', '3', '4', '5', '6'])
+def test_triu_indices(n, k, m):
+    result = dpnp.triu_indices(n, k, m)
+    expected = numpy.triu_indices(n, k, m)
+    numpy.testing.assert_array_equal(expected, result)
