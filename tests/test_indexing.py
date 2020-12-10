cf03b72d568c404c09803aacf1c764e5a9c47240
Rubtsowa <36762665+Rubtsowa@users.noreply.github.com>
2020-12-10 17:50:29 +0300

    Triu indices from (#389)
    * add triu_indices()

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -51,3 +51,21 @@ def test_triu_indices(n, k, m):
     result = dpnp.triu_indices(n, k, m)
     expected = numpy.triu_indices(n, k, m)
     numpy.testing.assert_array_equal(expected, result)
+
+
+@pytest.mark.parametrize("k",
+                         [0, 1, 2, 3, 4, 5],
+                         ids=['0', '1', '2', '3', '4', '5'])
+@pytest.mark.parametrize("array",
+                         [[[0, 0], [0, 0]],
+                          [[1, 2], [1, 2]],
+                          [[1, 2], [3, 4]],],
+                         ids=['[[0, 0], [0, 0]]',
+                              '[[1, 2], [1, 2]]',
+                              '[[1, 2], [3, 4]]'])
+def test_triu_indices_from(array, k):
+    a = numpy.array(array)
+    ia = dpnp.array(a)
+    result = dpnp.triu_indices_from(ia, k)
+    expected = numpy.triu_indices_from(a, k)
+    numpy.testing.assert_array_equal(expected, result)
