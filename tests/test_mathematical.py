367b7eea0faecbac09f8125e6ba1f70a44aaab2d
Rubtsowa <mariyarubtsowa@yandex.ru>
2020-12-11 21:06:34 +0300

    add diff

---
+++ tests/test_mathematical.py
@@ -0,0 +1,26 @@
+import pytest
+
+import dpnp as inp
+
+import numpy
+
+
+@pytest.mark.parametrize("array",
+                         [[[0, 0], [0, 0]],
+                          [[1, 2], [1, 2]],
+                          [[1, 2], [3, 4]],
+                          [[[1, 2], [3, 4]], [[1, 2], [2, 1]], [[1, 3], [3, 1]]],
+                          [[[[1, 2], [3, 4]], [[1, 2], [2, 1]]], [[[1, 3], [3, 1]], [[0, 1], [1, 3]]]]
+                          ],
+                         ids=['[[0, 0], [0, 0]]',
+                              '[[1, 2], [1, 2]]',
+                              '[[1, 2], [3, 4]]',
+                              '[[[1, 2], [3, 4]], [[1, 2], [2, 1]], [[1, 3], [3, 1]]]',
+                              '[[[[1, 2], [3, 4]], [[1, 2], [2, 1]]], [[[1, 3], [3, 1]], [[0, 1], [1, 3]]]]'
+                              ])
+def test_diff(array):
+    a = numpy.array(array)
+    ia = inp.array(a)
+    result = inp.linalg.det(ia)
+    expected = numpy.linalg.det(a)
+    numpy.testing.assert_allclose(expected, result)
