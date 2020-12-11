d00bdd4f948a9cfa642a34285a204f7050b4e49a
Alexander-Makaryev <alexander.makaryev@gmail.com>
2020-12-11 04:13:47 -0600

    build scripts refactoring

--- 0.env.sh
+++ 0.env.sh
@@ -5,12 +5,14 @@ THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 # We can not use common setup script because
 # using Intel Python brakes build and run procedure
 export ONEAPI_ROOT=/opt/intel/oneapi
-# Temporary use MKLVARS due to MKLD-10520
-export MKLVARS=${ONEAPI_ROOT}/mkl/latest/env
-. ${MKLVARS}/vars.sh
+. ${ONEAPI_ROOT}/mkl/latest/env/vars.sh
 . ${ONEAPI_ROOT}/compiler/latest/env/vars.sh
-. ${ONEAPI_ROOT}/dpl/latest/env/vars.sh
 . ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
+
+# TODO: remove when math lib vars.sh script will work properly
+# MKLD-10520
+export MKLROOT=${ONEAPI_ROOT}/mkl/latest
+export LD_LIBRARY_PATH=${MKLROOT}/lib/intel64:$LD_LIBRARY_PATH
 
 export DPCPPROOT=${ONEAPI_ROOT}/compiler/latest
 
