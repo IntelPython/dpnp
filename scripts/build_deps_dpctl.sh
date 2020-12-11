d23fb6467262aa9603602525a43cd79d8acfa85d
sergey shalnov <sergey.shalnov@intel.com>
2020-12-11 17:44:42 -0600

    add rpath to dpctl

--- scripts/build_deps_dpctl.sh
+++ scripts/build_deps_dpctl.sh
@@ -2,8 +2,8 @@
 
 THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
-echo +++++++++++++++++++++++++ Build DPCTL +++++++++++++++++++++++++++
-git clone https://github.com/IntelPython/dpctl.git
+echo +++++++++++++++++++++++++ Build DPCTL 0.5.0rc2 +++++++++++++++++++++++++++
+git clone --branch 0.5.0rc2 https://github.com/IntelPython/dpctl.git 
 
 cd dpctl
 
@@ -15,7 +15,16 @@ git tag 5.0
 
 conda build conda-recipe/ --no-test
 
-ls -lR /opt/intel/oneapi/intelpython/latest/conda-bld
+# ls -lR /opt/intel/oneapi/intelpython/latest/conda-bld
 
 conda install /opt/intel/oneapi/intelpython/latest/conda-bld/linux-64/dpctl*
+
+echo ========================= DPCTL version ==================================
+python -c "import dpctl as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\nsw.get_include={sw.get_include()}\")"
+
+echo ========================= where DPCTL ===============================
+find /opt/intel -name libDPCTLSyclInterface.so
+echo ========================= where mkl_sycl ===============================
+find /opt/intel -name libmkl_sycl.so
+
 cd ..
