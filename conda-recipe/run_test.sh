dce3a56ff4043b8f352dcf4a45f8efba5326e227
Sergey Shalnov <shssf@users.noreply.github.com>
2020-12-14 18:23:48 -0600

    Update run_test.sh

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # if ONEAPI_ROOT is specified (use all from it)
-if [ -n "${ONEAPI_ROOT}" ]; then
-    export DPCPPROOT=${ONEAPI_ROOT}/compiler/latest
-    # TODO uncomment when CI will be changed
-    # export MKLROOT=${ONEAPI_ROOT}/mkl/latest
-    export TBBROOT=${ONEAPI_ROOT}/tbb/latest
-fi
+# if [ -n "${ONEAPI_ROOT}" ]; then
+#     export DPCPPROOT=${ONEAPI_ROOT}/compiler/latest
+#     # TODO uncomment when CI will be changed
+#     # export MKLROOT=${ONEAPI_ROOT}/mkl/latest
+#     export TBBROOT=${ONEAPI_ROOT}/tbb/latest
+# fi
 
 # if DPCPPROOT is specified (work with custom DPCPP)
-if [ -n "${DPCPPROOT}" ]; then
-    . ${DPCPPROOT}/env/vars.sh
-fi
+# if [ -n "${DPCPPROOT}" ]; then
+#     . ${DPCPPROOT}/env/vars.sh
+# fi
 
 # if MKLROOT is specified (work with custom math library)
-if [ -n "${MKLROOT}" ]; then
-    . ${MKLROOT}/env/vars.sh
-    conda remove mkl --force -y || true
-fi
+# if [ -n "${MKLROOT}" ]; then
+#     . ${MKLROOT}/env/vars.sh
+#     conda remove mkl --force -y || true
+# fi
 
 # have to activate while SYCL CPU device/driver needs paths
 # if TBBROOT is specified
-if [ -n "${TBBROOT}" ]; then
-    . ${TBBROOT}/env/vars.sh
-fi
+# if [ -n "${TBBROOT}" ]; then
+#     . ${TBBROOT}/env/vars.sh
+# fi
