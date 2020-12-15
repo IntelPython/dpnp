110ebd1f0068db376aa94078e982f862ac6ec408
Sergey Shalnov <shssf@users.noreply.github.com>
2020-12-14 18:23:15 -0600

    Update activate_env.sh

--- conda-recipe/activate_env.sh
+++ conda-recipe/activate_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -z "${ONEAPI_ROOT}" ]; then
-    . ${ONEAPI_ROOT}/mkl/latest/env/vars.sh
-    . ${ONEAPI_ROOT}/compiler/2021.1-beta09/env/vars.sh
-    . ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
-fi
+# if [ ! -z "${ONEAPI_ROOT}" ]; then
+#     . ${ONEAPI_ROOT}/mkl/latest/env/vars.sh
+#     . ${ONEAPI_ROOT}/compiler/2021.1-beta09/env/vars.sh
+#     . ${ONEAPI_ROOT}/tbb/latest/env/vars.sh
+# fi
