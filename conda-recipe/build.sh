253a08cae856ebf262ea26dc5ef8e7e371703b39
Alexander-Makaryev <40917969+Alexander-Makaryev@users.noreply.github.com>
2020-12-15 20:27:36 +0300

    Gold/2021 (#425)
    * Add wheels build

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -27,3 +27,9 @@ fi
 
 $PYTHON setup.py build_clib
 $PYTHON setup.py build_ext install
+
+# Build wheel package
+if [ -n "${WHEELS_OUTPUT_FOLDER}" ]; then
+    $PYTHON setup.py bdist_wheel -p manylinux1_x86_64
+    cp dist/dpnp*.whl ${WHEELS_OUTPUT_FOLDER}
+fi
