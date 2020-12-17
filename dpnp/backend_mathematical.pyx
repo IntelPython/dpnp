d0ceb2b36c9b65e9eadc7a2dc2de3c30838c3451
Alexander-Makaryev <40917969+Alexander-Makaryev@users.noreply.github.com>
2020-12-17 18:48:22 +0300

    round funcs (#428)
    * around, round_ impl

--- dpnp/backend_mathematical.pyx
+++ dpnp/backend_mathematical.pyx
@@ -42,7 +42,8 @@ cimport numpy
 __all__ += [
     "dpnp_absolute",
     "dpnp_add",
-    'dpnp_arctan2',
+    "dpnp_arctan2",
+    "dpnp_around",
     "dpnp_ceil",
     "dpnp_copysign",
     "dpnp_cumprod",
@@ -70,8 +71,8 @@ __all__ += [
 ]
 
 
-ctypedef void(*fptr_custom_elemwise_absolute_1in_1out_t)(void *, void * , size_t)
-ctypedef void(*fptr_1in_2out_t)(void *, void * , void * , size_t)
+ctypedef void(*fptr_custom_elemwise_absolute_1in_1out_t)(void * , void * , size_t)
+ctypedef void(*fptr_1in_2out_t)(void * , void * , void * , size_t)
 
 
 cpdef dparray dpnp_absolute(dparray input):
@@ -84,7 +85,7 @@ cpdef dparray dpnp_absolute(dparray input):
     # get the FPTR data structure
     cdef DPNPFuncData kernel_data = get_dpnp_function_ptr(DPNP_FN_ABSOLUTE, param1_type, param1_type)
 
-    result_type = dpnp_DPNPFuncType_to_dtype(< size_t > kernel_data.return_type)
+    result_type = dpnp_DPNPFuncType_to_dtype( < size_t > kernel_data.return_type)
     # ceate result array with type given by FPTR data
     cdef dparray result = dparray(input_shape, dtype=result_type)
 
@@ -101,6 +102,20 @@ cpdef dparray dpnp_add(dparray x1, dparray x2):
 
 cpdef dparray dpnp_arctan2(dparray x1, dparray x2):
     return call_fptr_2in_1out(DPNP_FN_ARCTAN2, x1, x2, x1.shape)
+
+
+cpdef dpnp_around(dparray a, decimals, out):
+    cdef dparray result
+
+    if out is None:
+        result = dparray(a.shape, dtype=a.dtype)
+    else:
+        result = out
+
+    for i in range(result.size):
+        result[i] = round(a[i], decimals)
+
+    return result
 
 
 cpdef dparray dpnp_ceil(dparray x1):
@@ -149,7 +164,7 @@ cpdef dparray dpnp_cumsum(dparray x1):
     cur_res = x1[0]
     result._setitem_scalar(0, cur_res)
     for i in range(1, result.size):
-        cur_res += x1[i] 
+        cur_res += x1[i]
         result._setitem_scalar(i, cur_res)
 
     return result
@@ -221,7 +236,7 @@ cpdef tuple dpnp_modf(dparray x1):
     """ get the FPTR data structure """
     cdef DPNPFuncData kernel_data = get_dpnp_function_ptr(DPNP_FN_MODF, param1_type, DPNP_FT_NONE)
 
-    result_type = dpnp_DPNPFuncType_to_dtype(< size_t > kernel_data.return_type)
+    result_type = dpnp_DPNPFuncType_to_dtype( < size_t > kernel_data.return_type)
     """ Create result arrays with type given by FPTR data """
     cdef dparray result1 = dparray(x1.shape, dtype=result_type)
     cdef dparray result2 = dparray(x1.shape, dtype=result_type)
