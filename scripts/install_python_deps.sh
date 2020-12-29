8480d6950abf2dabb03e471d31f7c120cbe50536
sergey shalnov <sergey.shalnov@intel.com>
2020-12-29 10:51:43 -0600

    small refactor in CI scripts

--- scripts/install_python_deps.sh
+++ scripts/install_python_deps.sh
@@ -4,18 +4,18 @@ THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
 echo +++++++++++++++++++++++++ Python prerequisites +++++++++++++++++++++++++++
 
-echo ========================= PIP3: install prerequisites ===============================
+echo ========================= PIP3: install prerequisites ====================
 pip3 install numpy cython pytest pytest-valgrind
 
-echo ========================= Conda: install prerequisites ===============================
+echo ========================= Conda: install prerequisites ===================
 conda install -y conda-build numpy cython pytest
 
-echo ========================= SW versions ===============================
+echo ========================= SW versions ====================================
 conda list
 
 python --version
 which python
 
-python -c "import numpy as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\nsw.get_include={sw.get_include()}\")"
-python -c "import dpctl as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\nsw.get_include={sw.get_include()}\")"
-python -c "import dpnp as sw; print(f\"sw.version={sw.version}\nsw.version.version={sw.version.version}\nsw.get_include={sw.get_include()}\")"
+python -c "import numpy as sw; print(f\"sw.__version__={sw.__version__}\nsw.get_include={sw.get_include()}\")"
+python -c "import dpctl as sw; print(f\"sw.__version__={sw.__version__}\nsw.get_include={sw.get_include()}\")"
+python -c "import dpnp as sw; print(f\"sw.__version__={sw.__version__}\nsw.get_include={sw.get_include()}\")"
