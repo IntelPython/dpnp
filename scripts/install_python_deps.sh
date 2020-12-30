790d64b02008b43576347c7f183d08fd953b05e1
densmirn <53897164+densmirn@users.noreply.github.com>
2020-12-30 20:10:29 +0300

    TESTS enable run of numpy tests in external CI (#438)
    Co-authored-by: Sergey Shalnov <shssf@users.noreply.github.com>

--- scripts/install_python_deps.sh
+++ scripts/install_python_deps.sh
@@ -4,11 +4,11 @@ THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
 echo +++++++++++++++++++++++++ Python prerequisites +++++++++++++++++++++++++++
 
-echo ========================= PIP3: install prerequisites ====================
-pip3 install numpy cython pytest pytest-valgrind
+echo ========================= PIP3: install prerequisites ===============================
+pip3 install numpy cython pytest pytest-valgrind hypothesis
 
-echo ========================= Conda: install prerequisites ===================
-conda install -y conda-build numpy cython pytest
+echo ========================= Conda: install prerequisites ===============================
+conda install -y conda-build numpy cython pytest hypothesis
 
 echo ========================= SW versions ====================================
 conda list
