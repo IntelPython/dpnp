e02bd94e435341c2f7904d68fa54b40a0a543765
Samir Nasibli <samir.nasibli@intel.com>
2021-01-16 20:24:57 +0300

    ENH: dpnp backend fallback to classic MKL; update `random.beta` (#493)
    * ENH: dpnp backend fallback to classic MKL; update random.beta

--- dpnp/backend/kernels/dpnp_krnl_random.cpp
+++ dpnp/backend/kernels/dpnp_krnl_random.cpp
@@ -23,15 +23,50 @@
 // THE POSSIBILITY OF SUCH DAMAGE.
 //*****************************************************************************
 
+#include <mkl_vsl.h>
+#include <stdexcept>
+#include <vector>
+
 #include <dpnp_iface.hpp>
+
 #include "dpnp_fptr.hpp"
 #include "dpnp_utils.hpp"
 #include "queue_sycl.hpp"
 
-#include <vector>
-
 namespace mkl_rng = oneapi::mkl::rng;
 namespace mkl_blas = oneapi::mkl::blas;
+
+/**
+ * Use get/set functions to access/modify this variable
+ */
+static VSLStreamStatePtr rng_stream = nullptr;
+
+static void set_rng_stream(size_t seed = 1)
+{
+    if (rng_stream)
+    {
+        vslDeleteStream(&rng_stream);
+        rng_stream = nullptr;
+    }
+
+    vslNewStream(&rng_stream, VSL_BRNG_MT19937, seed);
+}
+
+static VSLStreamStatePtr get_rng_stream()
+{
+    if (!rng_stream)
+    {
+        set_rng_stream();
+    }
+
+    return rng_stream;
+}
+
+void dpnp_srand_c(size_t seed)
+{
+    backend_sycl::backend_sycl_rng_engine_init(seed);
+    set_rng_stream(seed);
+}
 
 template <typename _DataType>
 void dpnp_rng_beta_c(void* result, _DataType a, _DataType b, size_t size)
@@ -47,10 +82,24 @@ void dpnp_rng_beta_c(void* result, _DataType a, _DataType b, size_t size)
 
     _DataType* result1 = reinterpret_cast<_DataType*>(result);
 
-    mkl_rng::beta<_DataType> distribution(a, b, displacement, scalefactor);
-    // perform generation
-    auto event_out = mkl_rng::generate(distribution, DPNP_RNG_ENGINE, size, result1);
-    event_out.wait();
+    if (dpnp_queue_is_cpu_c())
+    {
+        mkl_rng::beta<_DataType> distribution(a, b, displacement, scalefactor);
+        // perform generation
+        auto event_out = mkl_rng::generate(distribution, DPNP_RNG_ENGINE, size, result1);
+        event_out.wait();
+    }
+    else
+    {
+        int errcode =
+            vdRngBeta(VSL_RNG_METHOD_BETA_CJA, get_rng_stream(), size, result1, a, b, displacement, scalefactor);
+        if (errcode != VSL_STATUS_OK)
+        {
+            throw std::runtime_error("DPNP RNG Error: dpnp_rng_beta_c() failed.");
+        }
+    }
+
+    return;
 }
 
 template <typename _DataType>
@@ -439,7 +488,6 @@ void dpnp_rng_weibull_c(void* result, double alpha, size_t size)
 void func_map_init_random(func_map_t& fmap)
 {
     fmap[DPNPFuncName::DPNP_FN_RNG_BETA][eft_DBL][eft_DBL] = {eft_DBL, (void*)dpnp_rng_beta_c<double>};
-    fmap[DPNPFuncName::DPNP_FN_RNG_BETA][eft_FLT][eft_FLT] = {eft_FLT, (void*)dpnp_rng_beta_c<float>};
 
     fmap[DPNPFuncName::DPNP_FN_RNG_BINOMIAL][eft_INT][eft_INT] = {eft_INT, (void*)dpnp_rng_binomial_c<int>};
 
