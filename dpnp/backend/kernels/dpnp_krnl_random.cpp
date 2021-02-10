8e1af5dd7fb37dc9cf0c19e93732bd7b9f6460ed
Samir Nasibli <samir.nasibli@intel.com>
2021-02-10 06:44:13 -0600

    optimization

--- dpnp/backend/kernels/dpnp_krnl_random.cpp
+++ dpnp/backend/kernels/dpnp_krnl_random.cpp
@@ -1139,6 +1139,12 @@ void dpnp_rng_uniform_c(void* result, const long low, const long high, const siz
     event_out.wait();
 }
 
+template <typename _KernelNameSpecialization>
+class dpnp_rng_wald_acceptance_kernel1;
+
+template <typename _KernelNameSpecialization>
+class dpnp_rng_wald_acceptance_kernel2;
+
 template <typename _DataType>
 void dpnp_rng_wald_c(void* result, const _DataType mean, const _DataType scale, const size_t size)
 {
@@ -1146,7 +1152,6 @@ void dpnp_rng_wald_c(void* result, const _DataType mean, const _DataType scale, 
     {
         return;
     }
-    cl::sycl::vector_class<cl::sycl::event> no_deps;
 
     _DataType* result1 = reinterpret_cast<_DataType*>(result);
     _DataType* uvec = nullptr;
@@ -1157,45 +1162,48 @@ void dpnp_rng_wald_c(void* result, const _DataType mean, const _DataType scale, 
 
     mkl_rng::gaussian<_DataType> gaussian_distribution(d_zero, gsc);
 
-    auto event_out = mkl_rng::generate(gaussian_distribution, DPNP_RNG_ENGINE, size, result1);
-    event_out.wait();
-
-    // TODO
-    // change no_deps
+    auto gaussian_dstr_event = mkl_rng::generate(gaussian_distribution, DPNP_RNG_ENGINE, size, result1);
 
     /* Y = mean/(2 scale) * Z^2 */
-    event_out = mkl_vm::sqr(DPNP_QUEUE, size, result1, result1, no_deps, mkl_vm::mode::ha);
-    event_out.wait();
+    auto sqr_event = mkl_vm::sqr(DPNP_QUEUE, size, result1, result1, {gaussian_dstr_event}, mkl_vm::mode::ha);
 
-    for (size_t i = 0; i < size; i++)
-    {
+    cl::sycl::range<1> gws(size);
+    auto acceptance_kernel1 = [=](cl::sycl::id<1> global_id) {
+        size_t i = global_id[0];
         if (result1[i] <= 2.0)
         {
-            result1[i] = 1.0 + result1[i] + sqrt(result1[i] * (result1[i] + 2.0));
+            result1[i] = 1.0 + result1[i] + cl::sycl::sqrt(result1[i] * (result1[i] + 2.0));
         }
         else
         {
-            result1[i] = 1.0 + result1[i] * (1.0 + sqrt(1.0 + 2.0 / result1[i]));
+            result1[i] = 1.0 + result1[i] * (1.0 + cl::sycl::sqrt(1.0 + 2.0 / result1[i]));
         }
-    }
+    };
+    auto parallel_for_acceptance1 = [&](cl::sycl::handler& cgh) {
+        cgh.depends_on({sqr_event});
+        cgh.parallel_for<class dpnp_rng_wald_acceptance_kernel1<_DataType>>(gws, acceptance_kernel1);
+    };
+    auto event_ration_acceptance = DPNP_QUEUE.submit(parallel_for_acceptance1);
 
     uvec = reinterpret_cast<_DataType*>(dpnp_memory_alloc_c(size * sizeof(_DataType)));
-    if (uvec == nullptr)
-    {
-        throw std::runtime_error("DPNP RNG Error: dpnp_rng_wald_c() failed.");
-    }
 
     mkl_rng::uniform<_DataType> uniform_distribution(d_zero, d_one);
-    event_out = mkl_rng::generate(uniform_distribution, DPNP_RNG_ENGINE, size, uvec);
-    event_out.wait();
+    auto uniform_distr_event = mkl_rng::generate(uniform_distribution, DPNP_RNG_ENGINE, size, uvec);
 
-    for (size_t i = 0; i < size; i++)
-    {
+    auto acceptance_kernel2 = [=](cl::sycl::id<1> global_id) {
+        size_t i = global_id[0];
         if (uvec[i] * (1.0 + result1[i]) <= result1[i])
             result1[i] = mean / result1[i];
         else
             result1[i] = mean * result1[i];
-    }
+    };
+    auto parallel_for_acceptance2 = [&](cl::sycl::handler& cgh) {
+        cgh.depends_on({event_ration_acceptance, uniform_distr_event});
+        cgh.parallel_for<class dpnp_rng_wald_acceptance_kernel2<_DataType>>(gws, acceptance_kernel2);
+    };
+    auto event_out = DPNP_QUEUE.submit(parallel_for_acceptance2);
+    event_out.wait();
+
     dpnp_memory_free_c(uvec);
 }
 
