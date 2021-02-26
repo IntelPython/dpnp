d94b753308fd240621e53f01c53d8c67023d96cb
Rubtsowa <36762665+Rubtsowa@users.noreply.github.com>
2021-02-26 19:47:39 -0300

    native version function diag (#621)
    * native version function diag

--- dpnp/backend/include/dpnp_iface.hpp
+++ dpnp/backend/include/dpnp_iface.hpp
@@ -377,6 +377,21 @@ INP_DLLEXPORT void dpnp_det_c(void* array1_in, void* result1, size_t* shape, siz
 
 /**
  * @ingroup BACKEND_API
+ * @brief math library implementation of det function
+ *
+ * @param [in]  array          Input array with data.
+ * @param [out] result         Output array.
+ * @param [in]  k              Diagonal in question.
+ * @param [in]  shape          Shape of input array.
+ * @param [in]  shape_result   Shape of result array.
+ * @param [in]  ndim           Number of elements in shape.
+ */
+template <typename _DataType>
+INP_DLLEXPORT void dpnp_diag_c(
+    void* array, void* result, const int k, size_t* shape, size_t* res_shape, const size_t ndim, const size_t res_ndim);
+
+/**
+ * @ingroup BACKEND_API
  * @brief math library implementation of diagonal function
  *
  * @param [in]  array   Input array with data.
