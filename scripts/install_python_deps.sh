5a92addb8355ad7ae04514fe97053c505e4ad88e
Denis Smirnov <denis.smirnov@intel.com>
2021-04-06 18:29:14 +0300

    AZURE use conda env to build DPNP part2

--- scripts/install_python_deps.sh
+++ scripts/install_python_deps.sh
@@ -2,18 +2,27 @@
 
 THEDIR=$(dirname $(readlink -e ${BASH_SOURCE[0]}))
 
-echo +++++++++++++++++++++++++ Python prerequisites +++++++++++++++++++++++++++
+echo +++++++++++++++++++++++++ Python prerequisites +++++++++++++++++++++++++++++++++
 
-echo ========================= Conda: install prerequisites ===============================
-conda install -y conda-build numpy cython pytest hypothesis
+echo ========================= Conda: create environment ============================
+PYTHON_VERSION=${1:-3.*}
+ENV_NAME=dpnp$PYTHON_VERSION
+conda create -q -y -n $ENV_NAME python=$PYTHON_VERSION
+. /usr/share/miniconda/etc/profile.d/conda.sh
+conda activate $ENV_NAME
 
-echo ========================= Conda: remove mkl ===============================
+echo ========================= Conda: install prerequisites =========================
+# explicitly install mkl blas instead of openblas
+# because numpy is installed with openblas for Python 3.9 by default
+conda install -y conda-build numpy blas=*=mkl cython pytest hypothesis
+
+echo ========================= Conda: remove mkl ====================================
 conda remove mkl --force -y || true
 
-echo ========================= PIP3: install prerequisites ===============================
+echo ========================= PIP3: install prerequisites ==========================
 pip3 install pytest-valgrind
 
-echo ========================= SW versions ====================================
+echo ========================= SW versions ==========================================
 conda list
 
 python --version
