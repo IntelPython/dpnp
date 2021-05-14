6e5429f4af47436ad5c7e188a7249dace4ef1605
Sergey Shalnov <shssf@users.noreply.github.com>
2021-05-14 09:40:04 -0500

    Update 0.build.sh

--- 0.build.sh
+++ 0.build.sh
@@ -20,6 +20,15 @@ echo =========example3==============
 clang++ -g -fPIC dpnp/backend/examples/example3.cpp -Idpnp -Idpnp/backend/include -Ldpnp -Wl,-rpath='$ORIGIN'/dpnp -ldpnp_backend_c -o example3
 ./example3
 
+echo =========ldd example3==============
+ldd ./example3
+echo =========readelf example3==============
+readelf -d ./example3
+echo =========ldd dpnp/libdpnp_backend_c.so==============
+ldd ./dpnp/libdpnp_backend_c.so
+echo =========readelf dpnp/libdpnp_backend_c.so==============
+readelf -d ./dpnp/libdpnp_backend_c.so
+
 echo
 echo =========example1==============
 python examples/example1.py
