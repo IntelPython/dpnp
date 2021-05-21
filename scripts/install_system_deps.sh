76ae15587382283f43ce65fdf8ae2ae4da4244df
Sergey Shalnov <shssf@users.noreply.github.com>
2021-05-21 08:47:12 -0500

    Update install_system_deps.sh

--- scripts/install_system_deps.sh
+++ scripts/install_system_deps.sh
@@ -10,6 +10,8 @@ sudo apt-get install -f
 sudo apt-get clean
 sudo apt-get autoclean
 
+sudo apt-get install -y aptitude
+
 echo ========================= install Intel repository =======================
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
@@ -18,11 +20,14 @@ sudo add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"
 sudo apt-get update
 
 echo ========================= make same libstdc++ ===========================
-sudo apt-get install -y aptitude
-echo ========================= remove GCC ===========================
-sudo apt remove -y gcc g++ gcc-6 g++-6 gcc-7 g++-7 gcc-8 g++-8 gcc-10 g++-10 gcc-11 g++-11 libstdc++6 libstdc++-7-dev libstdc++-8-dev libstdc++-10-dev libstdc++-11-dev
+# echo ========================= remove GCC ===========================
+# sudo apt remove -y gcc g++ gcc-6 g++-6 gcc-7 g++-7 gcc-8 g++-8 gcc-10 g++-10 gcc-11 g++-11 libstdc++6 libstdc++-7-dev libstdc++-8-dev libstdc++-10-dev libstdc++-11-dev
 echo ========================= remove by aptitude =========
-sudo aptitude remove -y gcc g++ gcc-6 g++-6 gcc-7 g++-7 gcc-8 g++-8 gcc-10 g++-10 gcc-11 g++-11 libstdc++6 libstdc++-7-dev libstdc++-8-dev libstdc++-10-dev libstdc++-11-dev
+sudo aptitude remove -y gcc g++ gfortran
+sudo aptitude remove -y lib32stdc++6
+sudo aptitude remove -y gcc-6 g++-6 gcc-6-base gcc-7 g++-7 gcc-7-base gcc-8 g++-8 gcc-8-base gcc-10 g++-10 gcc-10-base gcc-11 g++-11 gcc-11-base
+sudo aptitude remove -y libstdc++6 libstdc++-7-dev libstdc++-8-dev libstdc++-10-dev libstdc++-11-dev
+sudo aptitude remove -y llvm-8 llvm-9 llvm-10 llvm-11 llvm-12
 
 echo ========================= auto-remove GCC ===========================
 sudo apt autoremove
