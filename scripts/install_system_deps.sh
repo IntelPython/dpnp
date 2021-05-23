a91abc746062ed089b2083b2603444b2917ddbad
Sergey Shalnov <shssf@users.noreply.github.com>
2021-05-23 16:27:35 -0500

    Update install_system_deps.sh

--- scripts/install_system_deps.sh
+++ scripts/install_system_deps.sh
@@ -23,6 +23,7 @@ echo ========================= make same libstdc++ ===========================
 # echo ========================= remove GCC ===========================
 # sudo apt remove -y gcc g++ gcc-6 g++-6 gcc-7 g++-7 gcc-8 g++-8 gcc-10 g++-10 gcc-11 g++-11 libstdc++6 libstdc++-7-dev libstdc++-8-dev libstdc++-10-dev libstdc++-11-dev
 echo ========================= remove by aptitude =========
+sudo aptitude remove -y build-essential
 sudo aptitude remove -y gcc g++ gfortran
 sudo aptitude remove -y lib32stdc++6
 sudo aptitude remove -y gcc-6 g++-6 gcc-6-base gcc-7 g++-7 gcc-7-base gcc-8 g++-8 gcc-8-base gcc-10 g++-10 gcc-10-base gcc-11 g++-11 gcc-11-base
