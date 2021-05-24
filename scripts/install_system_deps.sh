f5c7c94aae386304f5dad75fa2fd06aca005c356
Sergey Shalnov <shssf@users.noreply.github.com>
2021-05-24 15:51:22 -0500

    Update install_system_deps.sh

--- scripts/install_system_deps.sh
+++ scripts/install_system_deps.sh
@@ -19,14 +19,23 @@ rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 sudo add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"
 sudo apt-get update
 
+echo ========================= alternatives status =======================
+update-alternatives --get-selections
+echo ========================= alternatives gcc =======================
+sudo update-alternatives --query gcc
+echo ========================= alternatives g++ =======================
+sudo update-alternatives --query g++
+echo ========================= delete alternatives =======================
+sudo update-alternatives --remove-all gcc
+sudo update-alternatives --remove-all g++
+
+
 echo ========================= make same libstdc++ ===========================
-# echo ========================= remove GCC ===========================
-# sudo apt remove -y gcc g++ gcc-6 g++-6 gcc-7 g++-7 gcc-8 g++-8 gcc-10 g++-10 gcc-11 g++-11 libstdc++6 libstdc++-7-dev libstdc++-8-dev libstdc++-10-dev libstdc++-11-dev
 echo ========================= remove by aptitude =========
 sudo aptitude remove -y build-essential
 sudo aptitude remove -y gcc g++ gfortran
 sudo aptitude remove -y lib32stdc++6
-sudo aptitude remove -y gcc-6 g++-6 gcc-6-base gcc-7 g++-7 gcc-7-base gcc-8 g++-8 gcc-8-base gcc-10 g++-10 gcc-10-base gcc-11 g++-11 gcc-11-base
+sudo aptitude remove -y gcc-6 g++-6 gcc-6-base gcc-7 g++-7 gcc-7-base gcc-8 g++-8 gcc-8-base gcc-9 g++-9 gcc-9-base gcc-10 g++-10 gcc-10-base gcc-11 g++-11 gcc-11-base
 sudo aptitude remove -y libstdc++6 libstdc++-7-dev libstdc++-8-dev libstdc++-9-dev libstdc++-10-dev libstdc++-11-dev
 sudo aptitude remove -y llvm-8 llvm-9 llvm-10 llvm-11 llvm-12
 
@@ -38,8 +47,9 @@ sudo aptitude install -y gcc-9 g++-9 libstdc++-9-dev
 echo ========================= install Intel OneAPI ===========================
 sudo aptitude install -y intel-oneapi-mkl                \
                          intel-oneapi-mkl-devel          \
-                         intel-oneapi-compiler-dpcpp-cpp \
-                         intel-oneapi-python
+                         intel-oneapi-compiler-dpcpp-cpp
+
+#intel-oneapi-python
 
 echo ========================= list /opt/intel/oneapi/ ========================
 ls -l /opt/intel/oneapi/
