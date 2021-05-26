df3160f49748c0a85042dbb0cfbfcaa9901be721
Samir Nasibli <samir.nasibli@intel.com>
2021-05-26 06:17:52 -0500

    tmp solution

--- dpnp/backend/kernels/dpnp_krnl_random.cpp
+++ dpnp/backend/kernels/dpnp_krnl_random.cpp
@@ -1275,21 +1275,18 @@ void dpnp_rng_vonmises_large_kappa_c(void* result, const _DataType mu, const _Da
     Uvec = reinterpret_cast<_DataType*>(dpnp_memory_alloc_c(size * sizeof(_DataType)));
     Vvec = reinterpret_cast<_DataType*>(dpnp_memory_alloc_c(size * sizeof(_DataType)));
     n = reinterpret_cast<size_t*>(dpnp_memory_alloc_c(sizeof(size_t)));
-    for (n[0] = 0; n[0] < size;)
+    for (*n = 0; *n < size;)
     {
-        size_t diff_size = size - n[0];
+        size_t diff_size = size - *n;
         mkl_rng::uniform<_DataType> uniform_distribution_u(d_zero, 0.5 * M_PI);
         auto uniform_distr_u_event = mkl_rng::generate(uniform_distribution_u, DPNP_RNG_ENGINE, diff_size, Uvec);
         mkl_rng::uniform<_DataType> uniform_distribution_v(d_zero, d_one);
         auto uniform_distr_v_event = mkl_rng::generate(uniform_distribution_v, DPNP_RNG_ENGINE, diff_size, Vvec);
 
-        cl::sycl::range<1> diff_gws(diff_size);
-
-        auto paral_kernel_some = [&](cl::sycl::handler& cgh) {
-            cgh.depends_on({uniform_distr_u_event, uniform_distr_v_event});
-            cgh.parallel_for(diff_gws, [=](cl::sycl::id<1> global_id) {
-                size_t i = global_id[0];
-
+        if (dpnp_queue_is_cpu_c())
+        {
+            for (size_t i = 0; i < diff_size; i++)
+            {
                 _DataType sn, cn, sn2, cn2;
                 _DataType neg_W_minus_one, V, Y;
 
@@ -1309,13 +1306,49 @@ void dpnp_rng_vonmises_large_kappa_c(void* result, const _DataType mu, const _Da
                         Y = 0.0;
                     else if (Y > 1.0)
                         Y = 1.0;
-                    n[0] = n[0] + 1;
-                    result1[n[0]] = cl::sycl::asin(cl::sycl::sqrt(Y));
+
+                    *n = *n + 1;
+                    result1[*n] = cl::sycl::asin(sqrt(Y));
                 }
-            });
-        };
-        auto some_event = DPNP_QUEUE.submit(paral_kernel_some);
-        some_event.wait();
+            }
+        }
+        else
+        {
+            // TODO
+            // Failed tests for checking the same seed on CPU
+            cl::sycl::range<1> diff_gws(diff_size);
+            auto paral_kernel_some = [&](cl::sycl::handler& cgh) {
+                cgh.depends_on({uniform_distr_u_event, uniform_distr_v_event});
+                cgh.parallel_for(diff_gws, [=](cl::sycl::id<1> global_id) {
+                    size_t i = global_id[0];
+
+                    _DataType sn, cn, sn2, cn2;
+                    _DataType neg_W_minus_one, V, Y;
+
+                    sn = cl::sycl::sin(Uvec[i]);
+                    cn = cl::sycl::cos(Uvec[i]);
+                    V = Vvec[i];
+                    sn2 = sn * sn;
+                    cn2 = cn * cn;
+
+                    neg_W_minus_one = s_minus_one * sn2 / (0.5 * s_minus_one + cn2);
+                    Y = kappa * (s_minus_one + neg_W_minus_one);
+
+                    if ((Y * (2 - Y) >= V) || (cl::sycl::log(Y / V) + 1 >= Y))
+                    {
+                        Y = neg_W_minus_one * (2 - neg_W_minus_one);
+                        if (Y < 0)
+                            Y = 0.0;
+                        else if (Y > 1.0)
+                            Y = 1.0;
+                        *n = *n + 1;
+                        result1[*n] = cl::sycl::asin(cl::sycl::sqrt(Y));
+                    }
+                });
+            };
+            auto some_event = DPNP_QUEUE.submit(paral_kernel_some);
+            some_event.wait();
+        }
     }
     dpnp_memory_free_c(Uvec);
     dpnp_memory_free_c(n);
@@ -1372,21 +1405,21 @@ void dpnp_rng_vonmises_small_kappa_c(void* result, const _DataType mu, const _Da
     Vvec = reinterpret_cast<_DataType*>(dpnp_memory_alloc_c(size * sizeof(_DataType)));
     n = reinterpret_cast<size_t*>(dpnp_memory_alloc_c(sizeof(size_t)));
 
-    for (n[0] = 0; n[0] < size;)
+    for (*n = 0; *n < size;)
     {
-        size_t diff_size = size - n[0];
+        size_t diff_size = size - *n;
         mkl_rng::uniform<_DataType> uniform_distribution_u(d_zero, M_PI);
         auto uniform_distr_u_event = mkl_rng::generate(uniform_distribution_u, DPNP_RNG_ENGINE, diff_size, Uvec);
         mkl_rng::uniform<_DataType> uniform_distribution_v(d_zero, d_one);
         auto uniform_distr_v_event = mkl_rng::generate(uniform_distribution_v, DPNP_RNG_ENGINE, diff_size, Vvec);
 
-        cl::sycl::range<1> diff_gws(diff_size);
+        if (dpnp_queue_is_cpu_c())
+        {
+            uniform_distr_u_event.wait();
+            uniform_distr_v_event.wait();
 
-        auto paral_kernel_some = [&](cl::sycl::handler& cgh) {
-            cgh.depends_on({uniform_distr_u_event, uniform_distr_v_event});
-            cgh.parallel_for(diff_gws, [=](cl::sycl::id<1> global_id) {
-                size_t i = global_id[0];
-
+            for (size_t i = 0; i < diff_size; i++)
+            {
                 _DataType Z, W, Y, V;
                 Z = cl::sycl::cos(Uvec[i]);
                 V = Vvec[i];
@@ -1394,13 +1427,36 @@ void dpnp_rng_vonmises_small_kappa_c(void* result, const _DataType mu, const _Da
                 Y = s_kappa - kappa * W;
                 if ((Y * (2 - Y) >= V) || (cl::sycl::log(Y / V) + 1 >= Y))
                 {
-                    n[0] = n[0] + 1;
-                    result1[n[0]] = cl::sycl::acos(W);
+                    *n = *n + 1;
+                    result1[*n] = cl::sycl::acos(W);
                 }
-            });
-        };
+            }
+        }
+        else
+        {
+            cl::sycl::range<1> diff_gws((diff_size));
+
+            // TODO
+            // Failed tests for checking the same seed on CPU
+            auto paral_kernel_some = [&](cl::sycl::handler& cgh) {
+                cgh.depends_on({uniform_distr_u_event, uniform_distr_v_event});
+                cgh.parallel_for(diff_gws, [=](cl::sycl::id<1> global_id) {
+                    size_t i = global_id[0];
+                    _DataType Z, W, Y, V;
+                    Z = cl::sycl::cos(Uvec[i]);
+                    V = Vvec[i];
+                    W = (kappa + s_kappa * Z) / (s_kappa + kappa * Z);
+                    Y = s_kappa - kappa * W;
+                    if ((Y * (2 - Y) >= V) || (cl::sycl::log(Y / V) + 1 >= Y))
+                    {
+                        *n = *n + 1;
+                        result1[*n] = cl::sycl::acos(W);
+                    }
+                });
+            };
         auto some_event = DPNP_QUEUE.submit(paral_kernel_some);
         some_event.wait();
+        }
     }
     dpnp_memory_free_c(Uvec);
     dpnp_memory_free_c(n);
