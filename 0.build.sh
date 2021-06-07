b40c0a681117c2eb21aad5163e9f114b1587882a
Sergey Shalnov <shssf@users.noreply.github.com>
2021-06-07 11:18:35 -0500

    Update 0.build.sh

--- 0.build.sh
+++ 0.build.sh
@@ -21,46 +21,44 @@ dpcpp -g -fPIC dpnp/backend/examples/example3.cpp -Idpnp -Idpnp/backend/include 
 # LD_DEBUG=libs,bindings,symbols ./example3
 ./example3
 
-gcc --version
+# gcc --version
+# echo =========LD_LIBRARY_PATH==============
+# echo $LD_LIBRARY_PATH
+
+# echo =========ldd example3==============
+# ldd ./example3
+# echo =========readelf example3==============
+# readelf -d ./example3
+# echo =========ldd dpnp/libdpnp_backend_c.so==============
+# ldd ./dpnp/libdpnp_backend_c.so
+# echo =========readelf dpnp/libdpnp_backend_c.so==============
+# readelf -d ./dpnp/libdpnp_backend_c.so
+
+# echo ========= libstdc++.so ==============
+# ls -l /usr/share/miniconda/envs/dpnp*/lib/libstdc++.so
+# strings /usr/share/miniconda/envs/dpnp*/lib/libstdc++.so | grep GLIBCXX | sort -n
 
 
-echo =========LD_LIBRARY_PATH==============
-echo $LD_LIBRARY_PATH
-
-echo =========ldd example3==============
-ldd ./example3
-echo =========readelf example3==============
-readelf -d ./example3
-echo =========ldd dpnp/libdpnp_backend_c.so==============
-ldd ./dpnp/libdpnp_backend_c.so
-echo =========readelf dpnp/libdpnp_backend_c.so==============
-readelf -d ./dpnp/libdpnp_backend_c.so
-
-echo ========= libstdc++.so ==============
-ls -l /usr/share/miniconda/envs/dpnp*/lib/libstdc++.so
-strings /usr/share/miniconda/envs/dpnp*/lib/libstdc++.so | grep GLIBCXX | sort -n
-
-
-echo
+# echo
 echo =========example1==============
 # LD_DEBUG=libs,bindings,symbols python examples/example1.py
 # LD_DEBUG=libs python examples/example1.py
 python examples/example1.py
 
-echo ========= find /opt ==============
-find /opt -name libstdc++.so*
-echo ========= find anaconda ==============
-find /usr/share/miniconda -name libstdc++.so*
-echo ========= dpkg-query -L libstdc++6 ==============
-dpkg-query -L libstdc++6
-echo ========= ls -l /lib/x86_64-linux-gnu/libstdc* ==============
-ls -l /lib/x86_64-linux-gnu/libstdc*
+# echo ========= find /opt ==============
+# find /opt -name libstdc++.so*
+# echo ========= find anaconda ==============
+# find /usr/share/miniconda -name libstdc++.so*
+# echo ========= dpkg-query -L libstdc++6 ==============
+# dpkg-query -L libstdc++6
+# echo ========= ls -l /lib/x86_64-linux-gnu/libstdc* ==============
+# ls -l /lib/x86_64-linux-gnu/libstdc*
 
-gcc --version
-g++ --version
-dpcpp --version
+# gcc --version
+# g++ --version
+# dpcpp --version
 
-echo ========= APT ==============
-apt list --installed
-echo ========= conda ==============
-conda list
+# echo ========= APT ==============
+# apt list --installed
+# echo ========= conda ==============
+# conda list
