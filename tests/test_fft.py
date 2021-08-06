9c81b0e20e7552f9ea7e23c9c9a876a35f014db9
Samir Nasibli <samir.nasibli@intel.com>
2021-08-06 01:42:28 -0500

    update

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -4,19 +4,17 @@ import dpnp
 
 import numpy
 
-@pytest.mark.parametrize("type",
-                         [numpy.complex128, numpy.complex64, numpy.float32, numpy.float64, numpy.int32, numpy.int64],
-                         ids=['complex128', 'complex64', 'float32', 'float64', 'int32', 'int64'])
+@pytest.mark.parametrize("type", ['complex128', 'complex64', 'float32', 'float64', 'int32', 'int64'])
 def test_fft(type):
     # 1 dim array
-    data = numpy.arange(100, dtype=type)
+    data = numpy.arange(100, dtype=numpy.dtype(type))
     # TODO:
     # doesn't work correct with `complex64` (not supported)
-    # dpnp_data = dpnp.arange(100, dtype=type)
+    # dpnp_data = dpnp.arange(100, dtype=dpnp.dtype(type))
     dpnp_data = dpnp.array(data)
 
     np_res = numpy.fft.fft(data)
     dpnp_res = dpnp.fft.fft(dpnp_data)
 
-    numpy.testing.assert_array_equal(dpnp_res, np_res)
+    numpy.testing.assert_allclose(dpnp_res, np_res, rtol=1e-4, atol=1e-7)
     assert dpnp_res.dtype == np_res.dtype
