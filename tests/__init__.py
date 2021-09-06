3caf35f28d6b2a6fadb365d434c08807e2746f64
denis.smirnov <denis.smirnov@intel.com>
2021-09-06 05:26:05 -0500

    Add assert_equal to assert usm_ndarray - fix

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,4 @@ from tests import testing
 
 numpy.testing.assert_allclose = testing.assert_allclose
 numpy.testing.assert_array_equal = testing.assert_array_equal
+numpy.testing.assert_equal = testing.assert_equal
