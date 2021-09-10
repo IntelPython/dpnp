55305f0d26532d1db5c54a941711939c70454dd6
Alexander-Makaryev <alexander.makaryev@gmail.com>
2021-09-10 17:17:53 +0300

    Update tests/__init__.py
    Co-authored-by: Sergey Shalnov <shssf@users.noreply.github.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,7 +12,7 @@ numpy.testing.assert_equal = testing.assert_equal
 # necessary because new data container does not support these functions yet
 from tests.third_party.cupy import testing as cupy_testing
 orig_shaped_arange = cupy_testing.shaped_arange
-def _shaped_arange(shape, xp=dpnp, dtype=numpy.float64, order='C'):
+def _shaped_arange(shape, xp=dpnp, dtype=dpnp.float64, order='C'):
     res = xp.array(orig_shaped_arange(shape, xp=numpy, dtype=dtype, order=order))
     return res
 cupy_testing.shaped_arange = _shaped_arange
