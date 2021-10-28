66d8f8761dc0be8863c72fba7084c331e74dad2d
Samir Nasibli <samir.nasibli@intel.com>
2021-10-28 10:24:16 -0500

    for DPNPC added ndim case

--- dpnp/backend/examples/example11.cpp
+++ dpnp/backend/examples/example11.cpp
@@ -1,5 +1,32 @@
-/*
- * dpcpp -g -fPIC -fsycl dpnp/backend/examples/example11.cpp  -o example11 -lmkl_sycl -lmkl_intel_ilp64 -lmkl_tbb_thread -lmkl_core
+//*****************************************************************************
+// Copyright (c) 2016-2020, Intel Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//*****************************************************************************
+
+/**
+ * Example 11.
+ *
+ * Possible compile line:
  * dpcpp -g -fPIC   -W -Wextra -Wshadow -Wall -Wstrict-prototypes -Wformat -Wformat-security -fsycl -fsycl-device-code-split=per_kernel -O0 -fno-delete-null-pointer-checks -fstack-protector-strong -fno-strict-overflow -std=gnu++17 dpnp/backend/examples/example11.cpp -Idpnp -Idpnp/backend/include -Ldpnp -Wl,-rpath='$ORIGIN'/dpnp -ldpnp_backend_c -o example11 -lmkl_sycl -lmkl_intel_ilp64 -lmkl_core
  */
 #include <iostream>
@@ -35,7 +62,8 @@ void init(T* x, size_t size)
     }
 }
 
-// For 1dim array
+#if 0
+/* For 1dim array */
 template <typename _DataType>
 void shuffle_c(
     void* result, const size_t itemsize, const size_t ndim, const size_t high_dim_size, const size_t size, cl::sycl::queue& queue, size_t seed)
@@ -98,47 +126,59 @@ void shuffle_c(
     free(Uvec, queue);
     return;
 }
+#endif
 
 int main(int, char**)
 {
-    cl::sycl::queue queue{cl::sycl::gpu_selector()};
+    // Two cases:
+    // 1) array size = 100, ndim = 1, high_dim_size = 10 (aka ndarray with shape (100,) )
+    // 2) array size = 100, ndim = 2, high_dim_size = 20 (aka ndarray with shape (20, 5) and len(array) = 20 )
+    const size_t ndim_cases = 2;
     const size_t itemsize = sizeof(double);
-    const size_t ndim = 1;
-    const size_t high_dim_size = 100;
+    const size_t ndim[ndim_cases] = {1, 2};
+    const size_t high_dim_size[ndim_cases] = {100, 20};
     const size_t size = 100;
     const size_t seed = 1234;
 
-    // reproducer for shuffling array (as is in dpnp_rng_shuffle_c)
-    // using mathlib interface
+#if 0
+    /*
+      reproducer for shuffling array (as is in dpnp_rng_shuffle_c)
+      for 1dim array
+      the same logic for ndim arrays
+      using mathlib interface
+    */
+    std::cout << "\n///////////////////////////////////////////////\n";
+    cl::sycl::queue queue{cl::sycl::gpu_selector()};
     std::cout << "\nREPRODUCE: logic as is in DPNPC dpnp_rng_shuffle_c:";
+    std::cout << "\nDIMS: " << ndim[0] <<std::endl;
     double* array_1 = reinterpret_cast<double*>(malloc_shared(size * sizeof(double), queue));
     std::cout << "\nINPUT array 1:";
     init<double>(array_1, size);
     print_dpnp_array(array_1, size);
-    shuffle_c<double>(array_1, itemsize, ndim, high_dim_size, size, queue, seed);
+    shuffle_c<double>(array_1, itemsize, ndim[0], high_dim_size[0], size, queue, seed);
     std::cout << "\nSHUFFLE INPUT array:";
     print_dpnp_array(array_1, size);
     free(array_1, queue);
+#endif
 
     std::cout << "\n///////////////////////////////////////////////\n";
-
     // DPNPC dpnp_rng_shuffle_c
     // DPNPC interface
-    std::cout << "\nREPRODUCE: DPNPC dpnp_rng_shuffle_c:";
     double* array_2 = reinterpret_cast<double*>(dpnp_memory_alloc_c(size * sizeof(double)));
-    // init array
-    init<double>(array_2, size);
-    // print before shuffle
-    std::cout << "\nINPUT array 2:";
-    print_dpnp_array(array_2, size);
-
-    dpnp_rng_srand_c(seed);
-
-    dpnp_rng_shuffle_c<double>(array_2, itemsize, ndim, high_dim_size, size);
-
-    // print shuffle result
-    std::cout << "\nSHUFFLE INPUT array 2:";
-    print_dpnp_array(array_2, size);
-
+    for (size_t i = 0; i < ndim_cases; i++)
+    {
+        std::cout << "\nREPRODUCE: DPNPC dpnp_rng_shuffle_c:";
+        std::cout << "\nDIMS: " << ndim[i] <<std::endl;
+        // init array 0, 1, 2, 3, 4, 5, 6, ....
+        init<double>(array_2, size);
+        // print before shuffle
+        std::cout << "\nINPUT array 2:";
+        print_dpnp_array(array_2, size);
+        dpnp_rng_srand_c(seed);
+        dpnp_rng_shuffle_c<double>(array_2, itemsize, ndim[i], high_dim_size[i], size);
+        // print shuffle result
+        std::cout << "\nSHUFFLE INPUT array 2:";
+        print_dpnp_array(array_2, size); 
+    }
     dpnp_memory_free_c(array_2);
 }
