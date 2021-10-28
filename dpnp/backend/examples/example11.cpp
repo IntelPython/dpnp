42486a8c53456e2474f57f36052fbdbb8b0ade58
Samir Nasibli <samir.nasibli@intel.com>
2021-10-28 09:44:33 -0500

    update: DPNPC interface

--- dpnp/backend/examples/example11.cpp
+++ dpnp/backend/examples/example11.cpp
@@ -1,10 +1,12 @@
 /*
  * dpcpp -g -fPIC -fsycl dpnp/backend/examples/example11.cpp  -o example11 -lmkl_sycl -lmkl_intel_ilp64 -lmkl_tbb_thread -lmkl_core
+ * dpcpp -g -fPIC   -W -Wextra -Wshadow -Wall -Wstrict-prototypes -Wformat -Wformat-security -fsycl -fsycl-device-code-split=per_kernel -O0 -fno-delete-null-pointer-checks -fstack-protector-strong -fno-strict-overflow -std=gnu++17 dpnp/backend/examples/example11.cpp -Idpnp -Idpnp/backend/include -Ldpnp -Wl,-rpath='$ORIGIN'/dpnp -ldpnp_backend_c -o example11 -lmkl_sycl -lmkl_intel_ilp64 -lmkl_core
  */
 #include <iostream>
 #include <time.h>
 
-// #include <dpnp_iface.hpp>
+#include <dpnp_iface.hpp>
+#include <dpnp_iface_fptr.hpp>
 
 #include <CL/sycl.hpp>
 #include <oneapi/mkl.hpp>
@@ -36,7 +38,7 @@ void init(T* x, size_t size)
 // For 1dim array
 template <typename _DataType>
 void shuffle_c(
-    void* result, const size_t itemsize, const size_t ndim, const size_t high_dim_size, const size_t size, cl::sycl::queue& queue)
+    void* result, const size_t itemsize, const size_t ndim, const size_t high_dim_size, const size_t size, cl::sycl::queue& queue, size_t seed)
 {
     if (!result)
     {
@@ -49,7 +51,6 @@ void shuffle_c(
     }
 
     // cl::sycl::queue queue{cl::sycl::gpu_selector()};
-    size_t seed = 1234;
 
     mkl_rng::mt19937 rng_engine = mkl_rng::mt19937(queue, seed);
 
@@ -100,22 +101,44 @@ void shuffle_c(
 
 int main(int, char**)
 {
-    // reproducer for shuffling array (as is in dpnp_rng_shuffle_c)
     cl::sycl::queue queue{cl::sycl::gpu_selector()};
     const size_t itemsize = sizeof(double);
     const size_t ndim = 1;
     const size_t high_dim_size = 100;
     const size_t size = 100;
+    const size_t seed = 1234;
+
+    // reproducer for shuffling array (as is in dpnp_rng_shuffle_c)
+    // using mathlib interface
+    std::cout << "\nREPRODUCE: logic as is in DPNPC dpnp_rng_shuffle_c:";
     double* array_1 = reinterpret_cast<double*>(malloc_shared(size * sizeof(double), queue));
-    std::cout << "\nINPUT array:"; // << std::endl;
+    std::cout << "\nINPUT array 1:";
     init<double>(array_1, size);
     print_dpnp_array(array_1, size);
-    shuffle_c<double>(array_1, itemsize, ndim, high_dim_size, size, queue);
-    std::cout << "\nSHUFFLE INPUT array:"; // << std::endl;
+    shuffle_c<double>(array_1, itemsize, ndim, high_dim_size, size, queue, seed);
+    std::cout << "\nSHUFFLE INPUT array:";
     print_dpnp_array(array_1, size);
-
     free(array_1, queue);
 
-    // TODO
-    // add using dpnpc dpnp_rng_shuffle_c
+    std::cout << "\n///////////////////////////////////////////////\n";
+
+    // DPNPC dpnp_rng_shuffle_c
+    // DPNPC interface
+    std::cout << "\nREPRODUCE: DPNPC dpnp_rng_shuffle_c:";
+    double* array_2 = reinterpret_cast<double*>(dpnp_memory_alloc_c(size * sizeof(double)));
+    // init array
+    init<double>(array_2, size);
+    // print before shuffle
+    std::cout << "\nINPUT array 2:";
+    print_dpnp_array(array_2, size);
+
+    dpnp_rng_srand_c(seed);
+
+    dpnp_rng_shuffle_c<double>(array_2, itemsize, ndim, high_dim_size, size);
+
+    // print shuffle result
+    std::cout << "\nSHUFFLE INPUT array 2:";
+    print_dpnp_array(array_2, size);
+
+    dpnp_memory_free_c(array_2);
 }
