cb37aaebeac5e2cde23fecb45980b8a8a365a4a0
Alexander-Makaryev <Alexander-Makaryev@users.noreply.github.com>
2022-02-18 14:30:13 +0000

    deploy: 7d8f2a09046959de87efa60799573c77369db101

--- backend_doc/group___b_a_c_k_e_n_d___f_f_t___a_p_i.js
+++ backend_doc/group___b_a_c_k_e_n_d___f_f_t___a_p_i.js
@@ -1,4 +1,4 @@
 var group___b_a_c_k_e_n_d___f_f_t___a_p_i =
 [
-    [ "dpnp_fft_fft_c", "group___b_a_c_k_e_n_d___f_f_t___a_p_i.html#ga89c66f7027d40a49c0d3d3d8665cbbdb", null ]
+    [ "dpnp_fft_fft_c", "group___b_a_c_k_e_n_d___f_f_t___a_p_i.html#gaba833cf617f755b2112b025000f7d239", null ]
 ];
