5d65a330c15fc775bfde6e21fe4ab1c2e5276aea
Alexander-Makaryev <alexander.makaryev@gmail.com>
2022-02-25 20:32:26 +0300

    Warning about Numpy import removed, cleanup in __init__.py (#1135)

--- dpnp/__init__.py
+++ dpnp/__init__.py
@@ -24,23 +24,6 @@
 # THE POSSIBILITY OF SUCH DAMAGE.
 # *****************************************************************************
 
-import sys
-import warnings
-
-# import os
-# myvar=os.environ
-
-# import pprint
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(myvar)
-
-# os.system("ldd ./dpnp/libdpnp_backend_c.so")
-# subprocess.call("echo Hello 123", shell=True)
-
-if "numpy" in sys.modules:
-    warnings.warn("\nDPNP: Module NumPy found. Please load DPNP module before NumPy.\n")
-
-
 import os
 mypath = os.path.dirname(os.path.realpath(__file__))
 
@@ -50,7 +33,6 @@ dpctlpath = os.path.dirname(dpctl.__file__)
 os.environ["PATH"] += os.pathsep + mypath + os.pathsep + dpctlpath
 
 
-# from dpnp.dparray import dparray as ndarray
 from dpnp.dpnp_array import dpnp_array as ndarray
 from dpnp.dpnp_flatiter import flatiter as flatiter
 
