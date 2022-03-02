56fc910051ddf329c0058e15d1a0f5a7e91395b0
Alexander-Makaryev <alexander.makaryev@gmail.com>
2022-03-03 01:45:39 +0300

    Updating version to 0.10.0 (#1133)

--- doc/conf.py
+++ doc/conf.py
@@ -31,9 +31,9 @@ copyright = '2020, Intel'
 author = 'Intel'
 
 # The short X.Y version
-version = '0.9'
+version = '0.10'
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.10.0'
 
 
 # -- General configuration ---------------------------------------------------
