43e8b80d65186f562ab229afe123a5bc97fc3af0
Alexander-Makaryev <Alexander-Makaryev@users.noreply.github.com>
2022-03-11 13:00:12 +0000

    deploy: 67ac3c69d8e1bf54fe1ce1d35d5896df7c82b456

--- backend_doc/group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.js
+++ backend_doc/group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.js
@@ -115,6 +115,8 @@ var group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i =
       [ "DPNPFuncName::DPNP_FN_FABS_EXT", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#gga8b20590525bff7c7ce4f728d2021b558a18e2b9cfe653d72b43f41581c75f2f6e", null ],
       [ "DPNPFuncName::DPNP_FN_FFT_FFT", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#gga8b20590525bff7c7ce4f728d2021b558a07975db9b1b940b6d733b592af110e1a", null ],
       [ "DPNPFuncName::DPNP_FN_FFT_FFT_EXT", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#gga8b20590525bff7c7ce4f728d2021b558acb2053b1a76c7b007f2aadfeb5f9dca7", null ],
+      [ "DPNPFuncName::DPNP_FN_FFT_RFFT", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#gga8b20590525bff7c7ce4f728d2021b558ad92488ea0c21568b202a717d59ff6439", null ],
+      [ "DPNPFuncName::DPNP_FN_FFT_RFFT_EXT", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#gga8b20590525bff7c7ce4f728d2021b558aa2d2bcea47c4e52a80ffbab34aa3eb68", null ],
       [ "DPNPFuncName::DPNP_FN_FILL_DIAGONAL", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#gga8b20590525bff7c7ce4f728d2021b558a4f6034abd1dbffe17b749af2d3484f1e", null ],
       [ "DPNPFuncName::DPNP_FN_FILL_DIAGONAL_EXT", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#gga8b20590525bff7c7ce4f728d2021b558a0acab283177974a15dcb741dbe06293d", null ],
       [ "DPNPFuncName::DPNP_FN_FLATTEN", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#gga8b20590525bff7c7ce4f728d2021b558a6497e5c3a97f99555841d09106f66b6d", null ],
