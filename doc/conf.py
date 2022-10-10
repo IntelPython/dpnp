95d47fda0300ef87eacf64070fbf81631390815b
Anton <100830759+antonwolfy@users.noreply.github.com>
2022-10-10 16:56:57 +0200

    Setting version to 0.10.2 (#1196)

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,7 @@ author = 'Intel'
 # The short X.Y version
 version = '0.10'
 # The full version, including alpha/beta/rc tags
-release = '0.10.1'
+release = '0.10.2'
 
 
 # -- General configuration ---------------------------------------------------
