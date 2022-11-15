85d38b15816180487c885cfdd67b995812b6fb4a
Anton Volkov <antonwolfy@gmail.com>
2022-11-15 07:24:32 -0600

    Use std::vector() in constructors with old OneMKL

--- dpnp/backend/kernels/dpnp_krnl_random.cpp
+++ dpnp/backend/kernels/dpnp_krnl_random.cpp
@@ -973,11 +973,11 @@ DPCTLSyclEventRef dpnp_rng_multinomial_c(DPCTLSyclQueueRef q_ref,
     else
     {
         DPNPC_ptr_adapter<double> p_ptr(q_ref, p_in, p_size, true);
-        double* p = p_ptr.get_ptr();
+        double* p_data = p_ptr.get_ptr();
 
         // size = size
         // `result` is a array for random numbers
-        // `size` is a `result`'s len. `size = n * p.size()`
+        // `size` is a `result`'s len. `size = n * p_size`
         // `n` is a number of random values to be generated.
         size_t n = size / p_size;
 
@@ -990,8 +990,12 @@ DPCTLSyclEventRef dpnp_rng_multinomial_c(DPCTLSyclQueueRef q_ref,
             DPNPC_ptr_adapter<_DataType> result_ptr(q_ref, result, size, true, true);
             _DataType* result1 = result_ptr.get_ptr();
 
-            auto p_span = sycl::span<double>{p, p_size};
-            mkl_rng::multinomial<_DataType> distribution(ntrial, p_span);
+#if (INTEL_MKL_VERSION < __INTEL_MKL_2023_SWITCHOVER)
+            std::vector<double> p(p_data, p_data + p_size);
+#else
+            auto p = sycl::span<double>{p_data, p_size};
+#endif
+            mkl_rng::multinomial<_DataType> distribution(ntrial, p);
 
             // perform generation
             event_out = mkl_rng::generate(distribution, DPNP_RNG_ENGINE, n, result1);
@@ -1005,7 +1009,7 @@ DPCTLSyclEventRef dpnp_rng_multinomial_c(DPCTLSyclQueueRef q_ref,
             DPNPC_ptr_adapter<_DataType> result_ptr(q_ref, result, size, true, true);
             _DataType* result1 = result_ptr.get_ptr();
             int errcode = viRngMultinomial(
-                VSL_RNG_METHOD_MULTINOMIAL_MULTPOISSON, get_rng_stream(), n, result1, ntrial, p_size, p);
+                VSL_RNG_METHOD_MULTINOMIAL_MULTPOISSON, get_rng_stream(), n, result1, ntrial, p_size, p_data);
             if (errcode != VSL_STATUS_OK)
             {
                 throw std::runtime_error("DPNP RNG Error: dpnp_rng_multinomial_c() failed.");
@@ -1072,21 +1076,26 @@ DPCTLSyclEventRef dpnp_rng_multivariate_normal_c(DPCTLSyclQueueRef q_ref,
     sycl::queue q = *(reinterpret_cast<sycl::queue*>(q_ref));
 
     DPNPC_ptr_adapter<double> mean_ptr(q_ref, mean_in, mean_size, true);
-    double* mean = mean_ptr.get_ptr();
+    double* mean_data = mean_ptr.get_ptr();
     DPNPC_ptr_adapter<double> cov_ptr(q_ref, cov_in, cov_size, true);
-    double* cov = cov_ptr.get_ptr();
+    double* cov_data = cov_ptr.get_ptr();
 
     _DataType* result1 = static_cast<_DataType *>(result);
 
-    auto mean_span = sycl::span<double>{mean, mean_size};
-    auto cov_span = sycl::span<double>{cov, cov_size};
+#if (INTEL_MKL_VERSION < __INTEL_MKL_2023_SWITCHOVER)
+    std::vector<double> mean(mean_data, mean_data + mean_size);
+    std::vector<double> cov(cov_data, cov_data + cov_size);
+#else
+    auto mean = sycl::span<double>{mean_data, mean_size};
+    auto cov = sycl::span<double>{cov_data, cov_size};
+#endif
 
     // `result` is a array for random numbers
     // `size` is a `result`'s len.
     // `size1` is a number of random values to be generated for each dimension.
     size_t size1 = size / dimen;
 
-    mkl_rng::gaussian_mv<_DataType> distribution(dimen, mean_span, cov_span);
+    mkl_rng::gaussian_mv<_DataType> distribution(dimen, mean, cov);
     auto event_out = mkl_rng::generate(distribution, DPNP_RNG_ENGINE, size1, result1);
     event_ref = reinterpret_cast<DPCTLSyclEventRef>(&event_out);
 
