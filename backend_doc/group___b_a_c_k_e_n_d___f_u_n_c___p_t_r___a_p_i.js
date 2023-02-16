03da4575ed7f5f97d29e5d0d4382e847610bad57
antonwolfy <antonwolfy@users.noreply.github.com>
2023-02-16 22:06:46 +0000

    deploy: 19715dedcaa04ece03c2ee5183fe8ce24ffe484d

--- backend_doc/group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.js
+++ backend_doc/group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.js
@@ -2,7 +2,9 @@ var group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i =
 [
     [ "DPNPFuncData", "struct_d_p_n_p_func_data.html", [
       [ "ptr", "struct_d_p_n_p_func_data.html#af40fd22f5954384788121f817fb02f0d", null ],
-      [ "return_type", "struct_d_p_n_p_func_data.html#a59c07232bc3830e89d5abe5447a24816", null ]
+      [ "ptr_no_fp64", "struct_d_p_n_p_func_data.html#aa88f90ce507ebffe7462bba4d2df6c76", null ],
+      [ "return_type", "struct_d_p_n_p_func_data.html#a59c07232bc3830e89d5abe5447a24816", null ],
+      [ "return_type_no_fp64", "struct_d_p_n_p_func_data.html#a591831d25d776b9ce6ef0a328bda9ddc", null ]
     ] ],
     [ "DPNPFuncData_t", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#ga52f631e01afd73cc5573f9e8f61bd78f", null ],
     [ "DPNPFuncName", "group___b_a_c_k_e_n_d___f_u_n_c___p_t_r___a_p_i.html#ga8b20590525bff7c7ce4f728d2021b558", [
