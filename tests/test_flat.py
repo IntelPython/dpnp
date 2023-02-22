e0de6d3df4fc2a86af274d91f927691b4ed1a3ef
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2023-02-22 20:41:24 +0100

    Update test_flat

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -5,11 +5,9 @@ import dpnp as inp
 import numpy
 
 
-@pytest.mark.parametrize("type",
-                         [numpy.int64],
-                         ids=['int64'])
-def test_flat(type):
-    a = numpy.array([1, 0, 2, -3, -1, 2, 21, -9])
+@pytest.mark.parametrize("dtype", [numpy.int64], ids=["int64"])
+def test_flat(dtype):
+    a = numpy.array([1, 0, 2, -3, -1, 2, 21, -9], dtype=dtype)
     ia = inp.array(a)
 
     result = ia.flat[0]
@@ -17,11 +15,9 @@ def test_flat(type):
     numpy.testing.assert_array_equal(expected, result)
 
 
-@pytest.mark.parametrize("type",
-                         [numpy.int64],
-                         ids=['int64'])
-def test_flat2(type):
-    a = numpy.arange(1, 7).reshape(2, 3)
+@pytest.mark.parametrize("dtype", [numpy.int64], ids=["int64"])
+def test_flat2(dtype):
+    a = numpy.arange(1, 7, dtype=dtype).reshape(2, 3)
     ia = inp.array(a)
 
     result = ia.flat[3]
@@ -29,11 +25,9 @@ def test_flat2(type):
     numpy.testing.assert_array_equal(expected, result)
 
 
-@pytest.mark.parametrize("type",
-                         [numpy.int64],
-                         ids=['int64'])
-def test_flat3(type):
-    a = numpy.arange(1, 7).reshape(2, 3).T
+@pytest.mark.parametrize("dtype", [numpy.int64], ids=["int64"])
+def test_flat3(dtype):
+    a = numpy.arange(1, 7, dtype=dtype).reshape(2, 3).T
     ia = inp.array(a)
 
     result = ia.flat[3]
