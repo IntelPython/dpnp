b18d35f938bc2e381dd919c9db0b43ce17cb9065
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2023-02-22 20:06:08 +0100

    Update test_absolute

--- tests/test_absolute.py
+++ tests/test_absolute.py
@@ -1,15 +1,14 @@
 import pytest
+from .helper import get_all_dtypes
 
 import dpnp as inp
 
 import numpy
 
 
-@pytest.mark.parametrize("type",
-                         [numpy.int64],
-                         ids=['int64'])
-def test_abs_int(type):
-    a = numpy.array([1, 0, 2, -3, -1, 2, 21, -9])
+@pytest.mark.parametrize("dtype", get_all_dtypes(no_bool=True, no_complex=True))
+def test_abs(dtype):
+    a = numpy.array([1, 0, 2, -3, -1, 2, 21, -9], dtype=dtype)
     ia = inp.array(a)
 
     result = inp.abs(ia)
@@ -17,11 +16,11 @@ def test_abs_int(type):
     numpy.testing.assert_array_equal(expected, result)
 
 
-@pytest.mark.parametrize("type",
-                         [numpy.int64],
-                         ids=['int64'])
-def test_absolute_int(type):
-    a = numpy.array([1, 0, 2, -3, -1, 2, 21, -9])
+@pytest.mark.parametrize(
+    "dtype", get_all_dtypes(no_bool=True, no_none=True, no_complex=True)
+)
+def test_absolute(dtype):
+    a = numpy.array([[-2.0, 3.0, 9.1], [-2.0, 5.0, -2], [1.0, -2.0, 5.0]], dtype=dtype)
     ia = inp.array(a)
 
     result = inp.absolute(ia)
@@ -29,23 +28,18 @@ def test_absolute_int(type):
     numpy.testing.assert_array_equal(expected, result)
 
 
-@pytest.mark.parametrize("type",
-                         [numpy.float64],
-                         ids=['float64'])
-def test_absolute_float(type):
-    a = numpy.array([[-2., 3., 9.1], [-2., 5.0, -2], [1.0, -2., 5.0]])
-    ia = inp.array(a)
-
-    result = inp.absolute(ia)
-    expected = numpy.absolute(a)
-    numpy.testing.assert_array_equal(expected, result)
-
-
-@pytest.mark.parametrize("type",
-                         [numpy.float64],
-                         ids=['float64'])
-def test_absolute_float_3(type):
-    a = numpy.array([[[-2., 3.], [9.1, 0.2]], [[-2., 5.0], [-2, -1.2]], [[1.0, -2.], [5.0, -1.1]]])
+@pytest.mark.parametrize(
+    "dtype", get_all_dtypes(no_bool=True, no_none=True, no_complex=True)
+)
+def test_absolute_float_3d(dtype):
+    a = numpy.array(
+        [
+            [[-2.0, 3.0], [9.1, 0.2]],
+            [[-2.0, 5.0], [-2, -1.2]],
+            [[1.0, -2.0], [5.0, -1.1]],
+        ],
+        dtype=dtype,
+    )
     ia = inp.array(a)
 
     result = inp.absolute(ia)
