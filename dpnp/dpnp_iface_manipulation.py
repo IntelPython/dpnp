3975e9b1610f440725283034e5aac3f4b7bc2ffb
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2023-02-28 12:11:51 +0100

    Update copyright

--- dpnp/dpnp_iface_manipulation.py
+++ dpnp/dpnp_iface_manipulation.py
@@ -2,7 +2,7 @@
 # distutils: language = c++
 # -*- coding: utf-8 -*-
 # *****************************************************************************
-# Copyright (c) 2016-2022, Intel Corporation
+# Copyright (c) 2016-2023, Intel Corporation
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
