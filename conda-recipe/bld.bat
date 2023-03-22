61c027eed3be41391f4110d433c6b20617f7c725
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2023-03-22 11:35:05 -0500

    Updated conda-recipe build scipts per changes in build system

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -9,10 +9,43 @@ REM @TODO: remove the setting, once transition to build backend on Windows
 REM to cmake is complete.
 SET "SETUPTOOLS_USE_DISTUTILS=stdlib"
 
-IF DEFINED DPLROOT (
-    ECHO "Sourcing DPLROOT"
-    SET "INCLUDE=%DPLROOT%\include;%INCLUDE%"
+"%PYTHON%" setup.py clean --all
+set "SKBUILD_ARGS=-G Ninja -- -DCMAKE_C_COMPILER:PATH=icx -DCMAKE_CXX_COMPILER:PATH=icx -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+
+FOR %%V IN (14.0.0 14 15.0.0 15 16.0.0 16) DO @(
+  REM set DIR_HINT if directory exists
+  IF EXIST "%BUILD_PREFIX%\Library\lib\clang\%%V\" (
+     SET "SYCL_INCLUDE_DIR_HINT=%BUILD_PREFIX%\Library\lib\clang\%%V"
+  )
 )
 
-%PYTHON% setup.py build_clib
-%PYTHON% setup.py build_ext install
+set "PLATFORM_DIR=%PREFIX%\Library\share\cmake-3.22\Modules\Platform"
+set "FN=Windows-IntelLLVM.cmake"
+
+rem Save the original file, and copy patched file to
+rem fix the issue with IntelLLVM integration with cmake on Windows
+if EXIST "%PLATFORM_DIR%" (
+  dir "%PLATFORM_DIR%\%FN%"
+  copy /Y "%PLATFORM_DIR%\%FN%" .
+  if errorlevel 1 exit 1
+  copy /Y .github\workflows\Windows-IntelLLVM.cmake "%PLATFORM_DIR%"
+  if errorlevel 1 exit 1
+)
+
+if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
+    rem Install and assemble wheel package from the build bits
+    "%PYTHON%" setup.py install bdist_wheel %SKBUILD_ARGS%
+    if errorlevel 1 exit 1
+    copy dist\dpctl*.whl %WHEELS_OUTPUT_FOLDER%
+    if errorlevel 1 exit 1
+) ELSE (
+    rem Only install
+    "%PYTHON%" setup.py install %SKBUILD_ARGS%
+    if errorlevel 1 exit 1
+)
+
+rem copy back
+if EXIST "%PLATFORM_DIR%" (
+   copy /Y "%FN%" "%PLATFORM_DIR%"
+   if errorlevel 1 exit 1
+)
