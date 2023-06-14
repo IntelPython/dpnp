2963b9f785300407eab8aff18dad7f93482072d3
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2023-06-14 11:53:10 +0200

    Additional tests for dpnp.mean

--- tests/test_mathematical.py
+++ tests/test_mathematical.py
@@ -956,3 +956,44 @@ def test_sum(shape, dtype_in, dtype_out):
             numpy_res = a_np.sum(axis=axis, dtype=dtype_out)
             dpnp_res = a.sum(axis=axis, dtype=dtype_out)
             assert_array_equal(numpy_res, dpnp_res.asnumpy())
+
+class TestMean:
+
+    @pytest.mark.parametrize("dtype", get_all_dtypes())
+    def test_mean_axis_tuple(self, dtype):
+        dp_array = dpnp.array([[0,1,2],[3,4,0]], dtype=dtype)
+        np_array = dpnp.asnumpy(dp_array)
+
+        result = dpnp.mean(dp_array, axis=(0,1))
+        expected = numpy.mean(np_array, axis=(0,1))
+        assert_allclose(expected, result)
+
+
+    def test_mean_axis_zero_size(self):
+        dp_array = dpnp.array([], dtype='int64')
+        np_array = dpnp.asnumpy(dp_array)
+
+        result = dpnp.mean(dp_array)
+        expected = numpy.mean(np_array)
+        assert_allclose(expected, result)
+
+
+    def test_mean_strided(self):
+        dp_array = dpnp.array([-2,-1,0,1,0,2], dtype='f4')
+        np_array = dpnp.asnumpy(dp_array)
+
+        result = dpnp.mean(dp_array[::-1])
+        expected = numpy.mean(np_array[::-1])
+        assert_allclose(expected, result)
+
+        result = dpnp.mean(dp_array[::2])
+        expected = numpy.mean(np_array[::2])
+        assert_allclose(expected, result)
+
+    def test_mean_scalar(self):
+        dp_array = dpnp.array(5)
+        np_array = dpnp.asnumpy(dp_array)
+
+        result = dp_array.mean()
+        expected = np_array.mean()
+        assert_allclose(expected, result)
