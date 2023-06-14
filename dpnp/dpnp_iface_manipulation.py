068de4c660d037a38e220fcdd62e3b9d21d2fadc
Anton <100830759+antonwolfy@users.noreply.github.com>
2023-06-14 18:44:48 +0200

    Update dpnp/dpnp_iface_manipulation.py
    Co-authored-by: Natalia Polina <natalia.polina@intel.com>

--- dpnp/dpnp_iface_manipulation.py
+++ dpnp/dpnp_iface_manipulation.py
@@ -617,7 +617,7 @@ def result_type(*arrays_and_dtypes):
     """
 
     usm_arrays_and_dtypes = [
-        dpnp.get_usm_ndarray(X) if isinstance(X, (dpnp_array, dpt.usm_ndarray)) else X
+        X.dtype if isinstance(X, (dpnp_array, dpt.usm_ndarray)) else X
         for X in arrays_and_dtypes
     ]
     return dpt.result_type(*usm_arrays_and_dtypes)
