b79acb126aa6b9b82578bbde47fa669177e8ae6d
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2023-07-21 20:37:31 +0200

    Update test_statistics

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,5 +1,6 @@
 import numpy
 import pytest
+from numpy.testing import assert_allclose
 
 import dpnp
 
@@ -7,31 +8,30 @@ from .helper import get_all_dtypes
 
 
 @pytest.mark.parametrize(
-    "type",
-    [numpy.float64, numpy.float32, numpy.int64, numpy.int32],
-    ids=["float64", "float32", "int64", "int32"],
+    "dtype", get_all_dtypes(no_none=True, no_bool=True, no_complex=True)
 )
 @pytest.mark.parametrize("size", [2, 4, 8, 16, 3, 9, 27, 81])
-def test_median(type, size):
-    a = numpy.arange(size, dtype=type)
+def test_median(dtype, size):
+    a = numpy.arange(size, dtype=dtype)
     ia = dpnp.array(a)
 
     np_res = numpy.median(a)
     dpnp_res = dpnp.median(ia)
 
-    numpy.testing.assert_allclose(dpnp_res, np_res)
+    assert_allclose(dpnp_res, np_res)
 
 
 @pytest.mark.usefixtures("allow_fall_back_on_numpy")
 @pytest.mark.parametrize("axis", [0, 1, -1, 2, -2, (1, 2), (0, -2)])
 def test_max(axis):
-    a = numpy.arange(768, dtype=numpy.float64).reshape((4, 4, 6, 8))
+    dtype = dpnp.default_float_type()
+    a = numpy.arange(768, dtype=dtype).reshape((4, 4, 6, 8))
     ia = dpnp.array(a)
 
     np_res = numpy.max(a, axis=axis)
     dpnp_res = dpnp.max(ia, axis=axis)
 
-    numpy.testing.assert_allclose(dpnp_res, np_res)
+    assert_allclose(dpnp_res, np_res)
 
 
 @pytest.mark.usefixtures("allow_fall_back_on_numpy")
@@ -71,16 +71,17 @@ def test_max(axis):
     ],
 )
 def test_nanvar(array):
-    a = numpy.array(array)
+    dtype = dpnp.default_float_type()
+    a = numpy.array(array, dtype=dtype)
     ia = dpnp.array(a)
     for ddof in range(a.ndim):
         expected = numpy.nanvar(a, ddof=ddof)
         result = dpnp.nanvar(ia, ddof=ddof)
-        numpy.testing.assert_array_equal(expected, result)
+        assert_allclose(expected, result, rtol=1e-06)
 
     expected = numpy.nanvar(a, axis=None, ddof=0)
     result = dpnp.nanvar(ia, axis=None, ddof=0)
-    numpy.testing.assert_array_equal(expected, result)
+    assert_allclose(expected, result, rtol=1e-06)
 
 
 @pytest.mark.usefixtures("allow_fall_back_on_numpy")
@@ -99,7 +100,7 @@ class TestBincount:
 
         expected = numpy.bincount(np_a, minlength=minlength)
         result = dpnp.bincount(dpnp_a, minlength=minlength)
-        numpy.testing.assert_array_equal(expected, result)
+        assert_allclose(expected, result)
 
     @pytest.mark.parametrize(
         "array", [[1, 2, 2, 1, 2, 4]], ids=["[1, 2, 2, 1, 2, 4]"]
@@ -115,7 +116,7 @@ class TestBincount:
 
         expected = numpy.bincount(np_a, weights=weights)
         result = dpnp.bincount(dpnp_a, weights=weights)
-        numpy.testing.assert_array_equal(expected, result)
+        assert_allclose(expected, result)
 
 
 @pytest.mark.parametrize(
