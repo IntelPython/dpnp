b0237dc0549ad663dae002efc63875c1ad0f3b9a
Anton Volkov <antonwolfy@gmail.com>
2023-08-11 09:24:14 +0200

    Removed unnecessary import

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,6 @@
 import numpy
 
 from tests import testing
-from tests.third_party.cupy import testing as cupy_testing
 
 numpy.testing.assert_allclose = testing.assert_allclose
 numpy.testing.assert_array_equal = testing.assert_array_equal
