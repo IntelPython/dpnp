239cca738818a0066b30afe85b9d1a58be8a4853
Anton <100830759+antonwolfy@users.noreply.github.com>
2023-09-07 12:42:18 +0200

    Unmuted tests for dpnp.sum (#1552)

--- tests/test_sum.py
+++ tests/test_sum.py
@@ -1,12 +1,19 @@
 import numpy
 import pytest
+from numpy.testing import (
+    assert_array_equal,
+)
 
 import dpnp
-from tests.helper import get_float_dtypes, has_support_aspect64
+from tests.helper import (
+    assert_dtype_allclose,
+    get_float_dtypes,
+    has_support_aspect64,
+)
 
 
-# Note: numpy.sum() always upcast integers to (u)int64 and float32 to
-# float64 for dtype=None. `np.sum` does that too for integers, but not for
+# Note: dpnp.sum() always upcast integers to (u)int64 and float32 to
+# float64 for dtype=None. `numpy.sum()` does that too for integers, but not for
 # float32, so we need to special-case it for these tests
 @pytest.mark.parametrize("dtype", get_float_dtypes())
 def test_sum_float(dtype):
@@ -22,11 +29,8 @@ def test_sum_float(dtype):
 
     for axis in range(len(a)):
         result = dpnp.sum(ia, axis=axis)
-        if dtype == dpnp.float32 and has_support_aspect64():
-            expected = numpy.sum(a, axis=axis, dtype=numpy.float64)
-        else:
-            expected = numpy.sum(a, axis=axis)
-        numpy.testing.assert_array_equal(expected, result)
+        expected = numpy.sum(a, axis=axis)
+        assert_dtype_allclose(result, expected)
 
 
 def test_sum_int():
@@ -35,7 +39,7 @@ def test_sum_int():
 
     result = dpnp.sum(ia)
     expected = numpy.sum(a)
-    numpy.testing.assert_array_equal(expected, result)
+    assert_array_equal(expected, result)
 
 
 def test_sum_axis():
@@ -54,4 +58,4 @@ def test_sum_axis():
         expected = numpy.sum(a, axis=1, dtype=numpy.float64)
     else:
         expected = numpy.sum(a, axis=1)
-    numpy.testing.assert_array_equal(expected, result)
+    assert_array_equal(expected, result)
