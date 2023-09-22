ad7ea5e9de577456caf3dbfe590e839df21eccd2
vlad-perevezentsev <vlad-perevezentsev@users.noreply.github.com>
2023-09-22 14:16:38 +0000

    deploy: 6ada5a7013711d47a9afdf8667f7c0873c4ee611

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.1',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.13.0rc1+1.g6ada5a701',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- pull/1558/backend_doc/search/classes_4.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['elements_5fcount_0',['elements_count',['../structdpnp_1_1backend_1_1ext_1_1sycl__ext_1_1elements__count.html',1,'dpnp::backend::ext::sycl_ext']]],
-  ['elements_5fcount_3c_20std_3a_3atuple_3c_20t_20_3e_20_3e_1',['elements_count&lt; std::tuple&lt; T &gt; &gt;',['../structdpnp_1_1backend_1_1ext_1_1sycl__ext_1_1elements__count_3_01std_1_1tuple_3_01_t_01_4_01_4.html',1,'dpnp::backend::ext::sycl_ext']]],
-  ['elements_5fcount_3c_20std_3a_3atuple_3c_20t_2c_20axis_2e_2e_2e_20_3e_20_3e_2',['elements_count&lt; std::tuple&lt; T, Axis... &gt; &gt;',['../structdpnp_1_1backend_1_1ext_1_1sycl__ext_1_1elements__count_3_01std_1_1tuple_3_01_t_00_01_axis_8_8_8_01_4_01_4.html',1,'dpnp::backend::ext::sycl_ext']]],
-  ['engine_5fstruct_3',['engine_struct',['../structengine__struct.html',1,'']]],
-  ['extend_5ftuple_5ftype_4',['extend_tuple_type',['../structdpnp_1_1backend_1_1ext_1_1sycl__ext_1_1extend__tuple__type.html',1,'dpnp::backend::ext::sycl_ext']]],
-  ['extend_5ftuple_5ftype_3c_20std_3a_3atuple_3c_20args_2e_2e_2e_20_3e_2c_20t_20_3e_5',['extend_tuple_type&lt; std::tuple&lt; Args... &gt;, T &gt;',['../structdpnp_1_1backend_1_1ext_1_1sycl__ext_1_1extend__tuple__type_3_01std_1_1tuple_3_01_args_8_8_8_01_4_00_01_t_01_4.html',1,'dpnp::backend::ext::sycl_ext']]]
-];
