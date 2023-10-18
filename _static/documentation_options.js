d9b555efc013da94c26334ae82e4db1805868127
antonwolfy <antonwolfy@users.noreply.github.com>
2023-10-18 19:35:27 +0000

    deploy: 3976ea65c6443ac290465bb86d087ce23229fd7c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.13.1dev0+42.g2aeb33324',
+    VERSION: '0.13.1dev0+43.g3976ea65c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
