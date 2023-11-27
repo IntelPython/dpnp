6c20debd83f12e469461887b170e635162652da9
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2023-11-27 13:00:27 +0100

    Add new cupy tests for dpnp.linalg.det

--- tests/third_party/cupy/linalg_tests/test_norms.py
+++ tests/third_party/cupy/linalg_tests/test_norms.py
@@ -7,6 +7,82 @@ import dpnp as cupy
 from tests.third_party.cupy import testing
 
 
+class TestDet(unittest.TestCase):
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_det(self, xp, dtype):
+        a = testing.shaped_arange((2, 2), xp, dtype) + 1
+        return xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_det_3(self, xp, dtype):
+        a = testing.shaped_arange((2, 2, 2), xp, dtype) + 1
+        return xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_det_4(self, xp, dtype):
+        a = testing.shaped_arange((2, 2, 2, 2), xp, dtype) + 1
+        return xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_det_empty_batch(self, xp, dtype):
+        a = xp.empty((2, 0, 3, 3), dtype)
+        return xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_det_empty_matrix(self, xp, dtype):
+        a = xp.empty((0, 0), dtype)
+        return xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_det_empty_matrices(self, xp, dtype):
+        a = xp.empty((2, 3, 0, 0), dtype)
+        return xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    def test_det_different_last_two_dims(self, dtype):
+        for xp in (numpy, cupy):
+            a = testing.shaped_arange((2, 3, 2), xp, dtype)
+            # TODO: replace ValueError with dpnp.linalg.LinAlgError
+            with pytest.raises((numpy.linalg.LinAlgError, ValueError)):
+                xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    def test_det_different_last_two_dims_empty_batch(self, dtype):
+        for xp in (numpy, cupy):
+            a = xp.empty((0, 3, 2), dtype=dtype)
+            # TODO: replace ValueError with dpnp.linalg.LinAlgError
+            with pytest.raises((numpy.linalg.LinAlgError, ValueError)):
+                xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    def test_det_one_dim(self, dtype):
+        for xp in (numpy, cupy):
+            a = testing.shaped_arange((2,), xp, dtype)
+            # TODO: replace ValueError with dpnp.linalg.LinAlgError
+            with pytest.raises((numpy.linalg.LinAlgError, ValueError)):
+                xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    def test_det_zero_dim(self, dtype):
+        for xp in (numpy, cupy):
+            a = testing.shaped_arange((), xp, dtype)
+            # TODO: replace ValueError with dpnp.linalg.LinAlgError
+            with pytest.raises((numpy.linalg.LinAlgError, ValueError)):
+                xp.linalg.det(a)
+
+    @testing.for_float_dtypes(no_float16=True)
+    @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
+    def test_det_singular(self, xp, dtype):
+        a = xp.zeros((2, 3, 3), dtype=dtype)
+        return xp.linalg.det(a)
+
+
 class TestSlogdet(unittest.TestCase):
     @testing.for_dtypes("fd")
     @testing.numpy_cupy_allclose(rtol=1e-3, atol=1e-4)
