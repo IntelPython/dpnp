5a4721f255b3f9ba47ede4d23765dad5a1fa8996
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2024-01-22 14:43:19 +0100

    Tune atol for TestSvd

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -919,7 +919,7 @@ class TestSvd:
                 reconstructed = inp.dot(dp_u, inp.dot(dpnp_diag_s, dp_vt))
             # TODO: use assert dpnp.allclose() inside check_decomposition()
             # when it will support complex dtypes
-            assert_allclose(dp_a, reconstructed, rtol=tol, atol=tol)
+            assert_allclose(dp_a, reconstructed, rtol=tol, atol=1e-4)
 
         assert_allclose(dp_s, np_s, rtol=tol, atol=1e-03)
 
