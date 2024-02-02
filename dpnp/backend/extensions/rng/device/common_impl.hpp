eda96edee5679e52ffb6a57f24ffc41043eb10dd
Anton Volkov <antonwolfy@gmail.com>
2024-02-02 17:31:01 +0100

    Add pybind class for engines

--- dpnp/backend/extensions/rng/device/common_impl.hpp
+++ dpnp/backend/extensions/rng/device/common_impl.hpp
@@ -50,41 +50,46 @@ namespace py = pybind11;
 namespace mkl_rng_dev = oneapi::mkl::rng::device;
 
 /*! @brief Functor for unary function evaluation on contiguous array */
-template <typename DataT,
+template <typename EngineDistrT,
+          typename DataT,
           typename GaussianDistrT,
-          typename ResT = DataT,
-          typename UnaryOperatorT = ResT,
-          unsigned int vec_sz = 8,
           unsigned int items_per_wi = 4,
           bool enable_sg_load = true>
 struct RngContigFunctor
 {
 private:
-    const std::uint32_t seed_;
+    // const std::uint32_t seed_;
+    EngineDistrT engine_;
     GaussianDistrT distr_;
-    ResT * const res_ = nullptr;
+    DataT * const res_ = nullptr;
     const size_t nelems_;
 
 public:
 
-    RngContigFunctor(const std::uint32_t seed, GaussianDistrT& distr, ResT *res, const size_t n_elems)
-        : seed_(seed), distr_(distr), res_(res), nelems_(n_elems)
+    RngContigFunctor(EngineDistrT& engine, GaussianDistrT& distr, DataT *res, const size_t n_elems)
+        : engine_(engine), distr_(distr), res_(res), nelems_(n_elems)
     {
     }
 
     void operator()(sycl::nd_item<1> nd_it) const
     {
-        auto global_id = nd_it.get_global_id();
+        // auto global_id = nd_it.get_global_id();
+
+        // constexpr std::size_t vec_sz = EngineT::vec_size;
         
         auto sg = nd_it.get_sub_group();
         const std::uint8_t sg_size = sg.get_local_range()[0];
         const std::uint8_t max_sg_size = sg.get_max_local_range()[0];
 
-        using EngineT = typename mkl_rng_dev::mrg32k3a<vec_sz>;
-        auto engine = EngineT(seed_, nelems_ * global_id);  // offset is questionable...
+        // auto engine = EngineT(seed_, nelems_ * global_id);  // offset is questionable...
+
+        using EngineT = typename EngineDistrT::engine_type;
+        EngineT engine = engine_();
 
         using DistrT = typename GaussianDistrT::distr_type;
         DistrT distr = distr_();
+
+        constexpr std::size_t vec_sz = EngineT::vec_size;
 
         if constexpr (enable_sg_load) {
             const size_t base = items_per_wi * vec_sz * (nd_it.get_group(0) * nd_it.get_local_range(0) + sg.get_group_id()[0] * max_sg_size);
