16a8b344ee3bef55e33666d438060bfd28b44227
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2024-02-16 14:08:49 +0100

    Remove old impl of dpnp_matrix_rank

--- dpnp/backend/kernels/dpnp_krnl_linalg.cpp
+++ dpnp/backend/kernels/dpnp_krnl_linalg.cpp
@@ -969,15 +969,6 @@ void func_map_init_linalg_func(func_map_t &fmap)
     fmap[DPNPFuncName::DPNP_FN_MATRIX_RANK][eft_DBL][eft_DBL] = {
         eft_DBL, (void *)dpnp_matrix_rank_default_c<double>};
 
-    fmap[DPNPFuncName::DPNP_FN_MATRIX_RANK_EXT][eft_INT][eft_INT] = {
-        eft_INT, (void *)dpnp_matrix_rank_ext_c<int32_t>};
-    fmap[DPNPFuncName::DPNP_FN_MATRIX_RANK_EXT][eft_LNG][eft_LNG] = {
-        eft_LNG, (void *)dpnp_matrix_rank_ext_c<int64_t>};
-    fmap[DPNPFuncName::DPNP_FN_MATRIX_RANK_EXT][eft_FLT][eft_FLT] = {
-        eft_FLT, (void *)dpnp_matrix_rank_ext_c<float>};
-    fmap[DPNPFuncName::DPNP_FN_MATRIX_RANK_EXT][eft_DBL][eft_DBL] = {
-        eft_DBL, (void *)dpnp_matrix_rank_ext_c<double>};
-
     fmap[DPNPFuncName::DPNP_FN_QR][eft_INT][eft_INT] = {
         eft_DBL, (void *)dpnp_qr_default_c<int32_t, double>};
     fmap[DPNPFuncName::DPNP_FN_QR][eft_LNG][eft_LNG] = {
