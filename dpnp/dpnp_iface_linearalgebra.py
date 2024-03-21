a152467495cef998708c9f043c09fecc751b3392
Vahid Tavanashad <vahid.tavanashad@intel.com>
2024-03-21 15:11:30 -0500

    implement dpnp.einsum

--- dpnp/dpnp_iface_linearalgebra.py
+++ dpnp/dpnp_iface_linearalgebra.py
@@ -49,6 +49,7 @@ from .dpnp_utils import (
 )
 from .dpnp_utils.dpnp_utils_linearalgebra import (
     dpnp_dot,
+    dpnp_einsum,
     dpnp_kron,
     dpnp_matmul,
 )
@@ -166,7 +167,7 @@ def dot(a, b, out=None):
     return dpnp.get_result_array(result, out, casting="no")
 
 
-def einsum(*args, **kwargs):
+def einsum(*operands, out=None, optimize=False, **kwargs):
     """
     Evaluates the Einstein summation convention on the operands.
 
@@ -184,12 +185,30 @@ def einsum(*args, **kwargs):
     :obj:`dpnp.inner` : Returns the inner product of two arrays.
     :obj:`dpnp.outer` : Returns the outer product of two arrays.
 
+    Examples
+    --------
+    >>> import dpnp as np
+    >>> a = np.arange(25).reshape(5,5)
+    >>> b = np.arange(5)
+    >>> c = np.arange(6).reshape(2,3)
+
+    Trace of a matrix:
+
+    >>> np.einsum('ii', a)
+    60
+    >>> np.einsum(a, [0,0])
+    60
+    >>> np.trace(a)
+    60
+
+    Extract the diagonal (requires explicit form):
+
     """
 
-    return call_origin(numpy.einsum, *args, **kwargs)
+    return dpnp_einsum(*operands, out=out, optimize=optimize, **kwargs)
 
 
-def einsum_path(*args, **kwargs):
+def einsum_path(*operands, optimize="greedy", einsum_call=False):
     """
     einsum_path(subscripts, *operands, optimize='greedy')
 
@@ -198,21 +217,123 @@ def einsum_path(*args, **kwargs):
 
     For full documentation refer to :obj:`numpy.einsum_path`.
 
-    Limitations
-    -----------
-    Function is executed sequentially on CPU.
+    Parameters
+    ----------
+    subscripts : str
+        Specifies the subscripts for summation.
+    *operands : sequence of arrays
+        These are the arrays for the operation in any form that can be
+        converted to an array. This includes scalars, lists, lists of
+        tuples, tuples, tuples of tuples, tuples of lists, and ndarrays.
+    optimize : {bool, list, tuple, None, "greedy", "optimal"}
+        Choose the type of path. If a tuple is provided, the second argument is
+        assumed to be the maximum intermediate size created. If only a single
+        argument is provided the largest input or output array size is used
+        as a maximum intermediate size.
+
+        * if a list is given that starts with ``einsum_path``, uses this as the
+          contraction path
+        * if ``False`` or ``None`` no optimization is taken
+        * if ``True`` defaults to the "greedy" algorithm
+        * "optimal" is an algorithm that combinatorially explores all possible
+          ways of contracting the listed tensors and chooses the least costly
+          path. Scales exponentially with the number of terms in the
+          contraction.
+        * "greedy" is an algorithm that chooses the best pair contraction
+          at each step. Effectively, this algorithm searches the largest inner,
+          Hadamard, and then outer products at each step. Scales cubically with
+          the number of terms in the contraction. Equivalent to the "optimal"
+          path for most contractions.
+
+        Default is "greedy".
+
+    Returns
+    -------
+    path : list of tuples
+        A list representation of the einsum path.
+    string_repr : str
+        A printable representation of the einsum path.
+
+    Notes
+    -----
+    The resulting path indicates which terms of the input contraction should be
+    contracted first, the result of this contraction is then appended to the
+    end of the contraction list. This list can then be iterated over until all
+    intermediate contractions are complete.
 
     See Also
     --------
     :obj:`dpnp.einsum` : Evaluates the Einstein summation convention
                          on the operands.
+    :obj:`dpnp.linalg.multi_dot` : Chained dot product.
     :obj:`dpnp.dot` : Returns the dot product of two arrays.
     :obj:`dpnp.inner` : Returns the inner product of two arrays.
     :obj:`dpnp.outer` : Returns the outer product of two arrays.
 
+    Examples
+    --------
+    We can begin with a chain dot example. In this case, it is optimal to
+    contract the ``b`` and ``c`` tensors first as represented by the first
+    element of the path ``(1, 2)``. The resulting tensor is added to the end
+    of the contraction and the remaining contraction ``(0, 1)`` is then
+    completed.
+
+    >>> import dpnp as np
+    >>> np.random.seed(123)
+    >>> a = np.random.rand(2, 2)
+    >>> b = np.random.rand(2, 5)
+    >>> c = np.random.rand(5, 2)
+    >>> path_info = np.einsum_path('ij,jk,kl->il', a, b, c, optimize="greedy")
+    >>> print(path_info[0])
+    ['einsum_path', (1, 2), (0, 1)]
+    >>> print(path_info[1])
+      Complete contraction:  ij,jk,kl->il # may vary
+             Naive scaling:  4
+         Optimized scaling:  3
+          Naive FLOP count:  1.600e+02
+      Optimized FLOP count:  5.600e+01
+       Theoretical speedup:  2.857
+      Largest intermediate:  4.000e+00 elements
+    -------------------------------------------------------------------------
+    scaling                  current                                remaining
+    -------------------------------------------------------------------------
+       3                   kl,jk->jl                                ij,jl->il
+       3                   jl,ij->il                                   il->il
+
+    A more complex index transformation example.
+
+    >>> I = np.random.rand(10, 10, 10, 10)
+    >>> C = np.random.rand(10, 10)
+    >>> path_info = np.einsum_path(
+            "ea,fb,abcd,gc,hd->efgh", C, C, I, C, C, optimize="greedy"
+        )
+    >>> print(path_info[0])
+    ['einsum_path', (0, 2), (0, 3), (0, 2), (0, 1)]
+    >>> print(path_info[1])
+      Complete contraction:  ea,fb,abcd,gc,hd->efgh # may vary
+             Naive scaling:  8
+         Optimized scaling:  5
+          Naive FLOP count:  8.000e+08
+      Optimized FLOP count:  8.000e+05
+       Theoretical speedup:  1000.000
+      Largest intermediate:  1.000e+04 elements
+    --------------------------------------------------------------------------
+    scaling                  current                                remaining
+    --------------------------------------------------------------------------
+       5               abcd,ea->bcde                      fb,gc,hd,bcde->efgh
+       5               bcde,fb->cdef                         gc,hd,cdef->efgh
+       5               cdef,gc->defg                            hd,defg->efgh
+       5               defg,hd->efgh                               efgh->efgh
+
     """
 
-    return call_origin(numpy.einsum_path, *args, **kwargs)
+    return call_origin(
+        numpy.einsum_path,
+        *operands,
+        optimize=optimize,
+        einsum_call=einsum_call,
+        allow_fallback=True,
+    )
 
 
 def inner(a, b):
