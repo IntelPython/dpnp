841664c9fa0f46df227020bb6192e58c51ac404d
Anton <100830759+antonwolfy@users.noreply.github.com>
2024-05-31 11:33:59 +0200

    Implement `dpnp.gradient` function (#1859)
    * Implement dpnp.gradient function
    
    * Resolve pre-commit issues
    
    * Update dpnp/dpnp_iface_mathematical.py
    
    Co-authored-by: vtavana <120411540+vtavana@users.noreply.github.com>
    
    * Update dpnp/dpnp_iface_mathematical.py
    
    Co-authored-by: vtavana <120411540+vtavana@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: vtavana <120411540+vtavana@users.noreply.github.com>

--- tests/test_mathematical.py
+++ tests/test_mathematical.py
@@ -9,6 +9,7 @@ from numpy.testing import (
     assert_array_equal,
     assert_equal,
     assert_raises,
+    assert_raises_regex,
 )
 
 import dpnp
@@ -23,7 +24,6 @@ from .helper import (
     get_float_dtypes,
     get_integer_dtypes,
     has_support_aspect64,
-    is_cpu_device,
 )
 from .test_umath import (
     _get_numpy_arrays_1in_1out,
@@ -71,6 +71,35 @@ class TestAngle:
         result = dpnp.angle(dp_a, deg=deg)
 
         assert_dtype_allclose(result, expected)
+
+
+@pytest.mark.usefixtures("allow_fall_back_on_numpy")
+class TestConvolve:
+    def test_object(self):
+        d = [1.0] * 100
+        k = [1.0] * 3
+        assert_array_almost_equal(dpnp.convolve(d, k)[2:-2], dpnp.full(98, 3))
+
+    def test_no_overwrite(self):
+        d = dpnp.ones(100)
+        k = dpnp.ones(3)
+        dpnp.convolve(d, k)
+        assert_array_equal(d, dpnp.ones(100))
+        assert_array_equal(k, dpnp.ones(3))
+
+    def test_mode(self):
+        d = dpnp.ones(100)
+        k = dpnp.ones(3)
+        default_mode = dpnp.convolve(d, k, mode="full")
+        full_mode = dpnp.convolve(d, k, mode="f")
+        assert_array_equal(full_mode, default_mode)
+        # integer mode
+        with assert_raises(ValueError):
+            dpnp.convolve(d, k, mode=-1)
+        assert_array_equal(dpnp.convolve(d, k, mode=2), full_mode)
+        # illegal arguments
+        with assert_raises(TypeError):
+            dpnp.convolve(d, k, mode=None)
 
 
 class TestClip:
@@ -582,33 +611,347 @@ class TestDiff:
         assert_raises(numpy.AxisError, xp.diff, a, axis=3, append=0)
 
 
-@pytest.mark.usefixtures("allow_fall_back_on_numpy")
-class TestConvolve:
-    def test_object(self):
-        d = [1.0] * 100
-        k = [1.0] * 3
-        assert_array_almost_equal(dpnp.convolve(d, k)[2:-2], dpnp.full(98, 3))
+class TestGradient:
+    @pytest.mark.parametrize("dt", get_all_dtypes(no_none=True, no_bool=True))
+    def test_basic(self, dt):
+        x = numpy.array([[1, 1], [3, 4]], dtype=dt)
+        ix = dpnp.array(x)
 
-    def test_no_overwrite(self):
-        d = dpnp.ones(100)
-        k = dpnp.ones(3)
-        dpnp.convolve(d, k)
-        assert_array_equal(d, dpnp.ones(100))
-        assert_array_equal(k, dpnp.ones(3))
+        expected = numpy.gradient(x)
+        result = dpnp.gradient(ix)
+        assert_array_equal(result, expected)
 
-    def test_mode(self):
-        d = dpnp.ones(100)
-        k = dpnp.ones(3)
-        default_mode = dpnp.convolve(d, k, mode="full")
-        full_mode = dpnp.convolve(d, k, mode="f")
-        assert_array_equal(full_mode, default_mode)
-        # integer mode
-        with assert_raises(ValueError):
-            dpnp.convolve(d, k, mode=-1)
-        assert_array_equal(dpnp.convolve(d, k, mode=2), full_mode)
-        # illegal arguments
-        with assert_raises(TypeError):
-            dpnp.convolve(d, k, mode=None)
+    @pytest.mark.parametrize(
+        "args",
+        [3.0, numpy.array(3.0), numpy.cumsum(numpy.ones(5))],
+        ids=["scalar", "array", "cumsum"],
+    )
+    @pytest.mark.parametrize("dt", get_all_dtypes(no_none=True, no_bool=True))
+    def test_args_1d(self, args, dt):
+        x = numpy.arange(5, dtype=dt)
+        ix = dpnp.array(x)
+
+        if numpy.isscalar(args):
+            iargs = args
+        else:
+            iargs = dpnp.array(args)
+
+        expected = numpy.gradient(x, args)
+        result = dpnp.gradient(ix, iargs)
+        assert_dtype_allclose(result, expected)
+
+    @pytest.mark.parametrize(
+        "args", [1.5, numpy.array(1.5)], ids=["scalar", "array"]
+    )
+    @pytest.mark.parametrize("dt", get_all_dtypes(no_none=True, no_bool=True))
+    def test_args_2d(self, args, dt):
+        x = numpy.arange(25, dtype=dt).reshape(5, 5)
+        ix = dpnp.array(x)
+
+        if numpy.isscalar(args):
+            iargs = args
+        else:
+            iargs = dpnp.array(args)
+
+        expected = numpy.gradient(x, args)
+        result = dpnp.gradient(ix, iargs)
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("dt", get_all_dtypes(no_none=True, no_bool=True))
+    def test_args_2d_uneven(self, dt):
+        x = numpy.arange(25, dtype=dt).reshape(5, 5)
+        ix = dpnp.array(x)
+
+        dx = numpy.array([1.0, 2.0, 5.0, 9.0, 11.0])
+        idx = dpnp.array(dx)
+
+        expected = numpy.gradient(x, dx, dx)
+        result = dpnp.gradient(ix, idx, idx)
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("dt", get_all_dtypes(no_none=True, no_bool=True))
+    def test_args_2d_mix_with_scalar(self, dt):
+        x = numpy.arange(25, dtype=dt).reshape(5, 5)
+        ix = dpnp.array(x)
+
+        dx = numpy.cumsum(numpy.ones(5))
+        idx = dpnp.array(dx)
+
+        expected = numpy.gradient(x, dx, 2)
+        result = dpnp.gradient(ix, idx, 2)
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("dt", get_all_dtypes(no_none=True, no_bool=True))
+    def test_axis_args_2d(self, dt):
+        x = numpy.arange(25, dtype=dt).reshape(5, 5)
+        ix = dpnp.array(x)
+
+        dx = numpy.cumsum(numpy.ones(5))
+        idx = dpnp.array(dx)
+
+        expected = numpy.gradient(x, dx, axis=1)
+        result = dpnp.gradient(ix, idx, axis=1)
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("xp", [numpy, dpnp])
+    def test_args_2d_error(self, xp):
+        x = xp.arange(25).reshape(5, 5)
+        dx = xp.cumsum(xp.ones(5))
+        assert_raises_regex(
+            ValueError,
+            ".*scalars or 1d",
+            xp.gradient,
+            x,
+            xp.stack([dx] * 2, axis=-1),
+            1,
+        )
+
+    @pytest.mark.parametrize("xp", [numpy, dpnp])
+    def test_badargs(self, xp):
+        x = xp.arange(25).reshape(5, 5)
+        dx = xp.cumsum(xp.ones(5))
+
+        # wrong sizes
+        assert_raises(ValueError, xp.gradient, x, x, xp.ones(2))
+        assert_raises(ValueError, xp.gradient, x, 1, xp.ones(2))
+        assert_raises(ValueError, xp.gradient, x, xp.ones(2), xp.ones(2))
+        # wrong number of arguments
+        assert_raises(TypeError, xp.gradient, x, x)
+        assert_raises(TypeError, xp.gradient, x, dx, axis=(0, 1))
+        assert_raises(TypeError, xp.gradient, x, dx, dx, dx)
+        assert_raises(TypeError, xp.gradient, x, 1, 1, 1)
+        assert_raises(TypeError, xp.gradient, x, dx, dx, axis=1)
+        assert_raises(TypeError, xp.gradient, x, 1, 1, axis=1)
+
+    @pytest.mark.parametrize(
+        "x",
+        [
+            numpy.linspace(0, 1, 10),
+            numpy.sort(numpy.random.RandomState(0).random(10)),
+        ],
+        ids=["linspace", "random_sorted"],
+    )
+    @pytest.mark.parametrize("dt", get_float_dtypes())
+    # testing that the relative numerical error is close to numpy
+    def test_second_order_accurate(self, x, dt):
+        x = x.astype(dt)
+        dx = x[1] - x[0]
+        y = 2 * x**3 + 4 * x**2 + 2 * x
+
+        iy = dpnp.array(y)
+        idx = dpnp.array(dx)
+
+        expected = numpy.gradient(y, dx, edge_order=2)
+        result = dpnp.gradient(iy, idx, edge_order=2)
+        assert_dtype_allclose(result, expected)
+
+    @pytest.mark.parametrize("edge_order", [1, 2])
+    @pytest.mark.parametrize("axis", [0, 1, (0, 1)])
+    @pytest.mark.parametrize("dt", get_float_dtypes())
+    def test_spacing_axis_scalar(self, edge_order, axis, dt):
+        x = numpy.array([0, 2.0, 3.0, 4.0, 5.0, 5.0], dtype=dt)
+        x = numpy.tile(x, (6, 1)) + x.reshape(-1, 1)
+        ix = dpnp.array(x)
+
+        expected = numpy.gradient(x, 1.0, axis=axis, edge_order=edge_order)
+        result = dpnp.gradient(ix, 1.0, axis=axis, edge_order=edge_order)
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("edge_order", [1, 2])
+    @pytest.mark.parametrize("axis", [(0, 1), None])
+    @pytest.mark.parametrize("dt", get_float_dtypes())
+    @pytest.mark.parametrize(
+        "dx",
+        [numpy.arange(6.0), numpy.array([0.0, 0.5, 1.0, 3.0, 5.0, 7.0])],
+        ids=["even", "uneven"],
+    )
+    def test_spacing_axis_two_args(self, edge_order, axis, dt, dx):
+        x = numpy.array([0, 2.0, 3.0, 4.0, 5.0, 5.0], dtype=dt)
+        x = numpy.tile(x, (6, 1)) + x.reshape(-1, 1)
+
+        ix = dpnp.array(x)
+        idx = dpnp.array(dx)
+
+        expected = numpy.gradient(x, dx, dx, axis=axis, edge_order=edge_order)
+        result = dpnp.gradient(ix, idx, idx, axis=axis, edge_order=edge_order)
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("edge_order", [1, 2])
+    @pytest.mark.parametrize("axis", [0, 1])
+    @pytest.mark.parametrize("dt", get_float_dtypes())
+    @pytest.mark.parametrize(
+        "dx",
+        [numpy.arange(6.0), numpy.array([0.0, 0.5, 1.0, 3.0, 5.0, 7.0])],
+        ids=["even", "uneven"],
+    )
+    def test_spacing_axis_args(self, edge_order, axis, dt, dx):
+        x = numpy.array([0, 2.0, 3.0, 4.0, 5.0, 5.0], dtype=dt)
+        x = numpy.tile(x, (6, 1)) + x.reshape(-1, 1)
+
+        ix = dpnp.array(x)
+        idx = dpnp.array(dx)
+
+        expected = numpy.gradient(x, dx, axis=axis, edge_order=edge_order)
+        result = dpnp.gradient(ix, idx, axis=axis, edge_order=edge_order)
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("edge_order", [1, 2])
+    @pytest.mark.parametrize("dt", get_float_dtypes())
+    def test_spacing_mix_args(self, edge_order, dt):
+        x = numpy.array([0, 2.0, 3.0, 4.0, 5.0, 5.0], dtype=dt)
+        x = numpy.tile(x, (6, 1)) + x.reshape(-1, 1)
+        x_uneven = numpy.array([0.0, 0.5, 1.0, 3.0, 5.0, 7.0])
+        x_even = numpy.arange(6.0)
+
+        ix = dpnp.array(x)
+        ix_uneven = dpnp.array(x_uneven)
+        ix_even = dpnp.array(x_even)
+
+        expected = numpy.gradient(
+            x, x_even, x_uneven, axis=(0, 1), edge_order=edge_order
+        )
+        result = dpnp.gradient(
+            ix, ix_even, ix_uneven, axis=(0, 1), edge_order=edge_order
+        )
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+        expected = numpy.gradient(
+            x, x_uneven, x_even, axis=(1, 0), edge_order=edge_order
+        )
+        result = dpnp.gradient(
+            ix, ix_uneven, ix_even, axis=(1, 0), edge_order=edge_order
+        )
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("axis", [0, 1, -1, (1, 0), None])
+    def test_specific_axes(self, axis):
+        x = numpy.array([[1, 1], [3, 4]])
+        ix = dpnp.array(x)
+
+        expected = numpy.gradient(x, axis=axis)
+        result = dpnp.gradient(ix, axis=axis)
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    def test_axis_scalar_args(self):
+        x = numpy.array([[1, 1], [3, 4]])
+        ix = dpnp.array(x)
+
+        expected = numpy.gradient(x, 2, 3, axis=(1, 0))
+        result = dpnp.gradient(ix, 2, 3, axis=(1, 0))
+        for gr, igr in zip(expected, result):
+            assert_dtype_allclose(igr, gr)
+
+    @pytest.mark.parametrize("xp", [numpy, dpnp])
+    def test_wrong_number_of_args(self, xp):
+        x = xp.array([[1, 1], [3, 4]])
+        assert_raises(TypeError, xp.gradient, x, 1, 2, axis=1)
+
+    @pytest.mark.parametrize("xp", [numpy, dpnp])
+    def test_wrong_axis(self, xp):
+        x = xp.array([[1, 1], [3, 4]])
+        assert_raises(numpy.AxisError, xp.gradient, x, axis=3)
+
+    @pytest.mark.parametrize(
+        "size, edge_order",
+        [
+            pytest.param(2, 1),
+            pytest.param(3, 2),
+        ],
+    )
+    def test_min_size_with_edge_order(self, size, edge_order):
+        x = numpy.arange(size)
+        ix = dpnp.array(x)
+
+        expected = numpy.gradient(x, edge_order=edge_order)
+        result = dpnp.gradient(ix, edge_order=edge_order)
+        assert_dtype_allclose(result, expected)
+
+    @pytest.mark.parametrize(
+        "size, edge_order",
+        [
+            pytest.param(0, 1),
+            pytest.param(0, 2),
+            pytest.param(1, 1),
+            pytest.param(1, 2),
+            pytest.param(2, 2),
+        ],
+    )
+    @pytest.mark.parametrize("xp", [numpy, dpnp])
+    def test_wrong_size_with_edge_order(self, size, edge_order, xp):
+        assert_raises(
+            ValueError, xp.gradient, xp.arange(size), edge_order=edge_order
+        )
+
+    @pytest.mark.parametrize(
+        "dt", [numpy.uint8, numpy.uint16, numpy.uint32, numpy.uint64]
+    )
+    def test_f_decreasing_unsigned_int(self, dt):
+        x = numpy.array([5, 4, 3, 2, 1], dtype=dt)
+        ix = dpnp.array(x)
+
+        expected = numpy.gradient(x)
+        result = dpnp.gradient(ix)
+        assert_array_equal(result, expected)
+
+    @pytest.mark.parametrize(
+        "dt", [numpy.int8, numpy.int16, numpy.int32, numpy.int64]
+    )
+    def test_f_signed_int_big_jump(self, dt):
+        maxint = numpy.iinfo(dt).max
+        x = numpy.array([-1, maxint], dtype=dt)
+        dx = numpy.array([1, 3])
+
+        ix = dpnp.array(x)
+        idx = dpnp.array(dx)
+
+        expected = numpy.gradient(x, dx)
+        result = dpnp.gradient(ix, idx)
+        assert_array_equal(result, expected)
+
+    @pytest.mark.parametrize(
+        "dt", [numpy.uint8, numpy.uint16, numpy.uint32, numpy.uint64]
+    )
+    def test_x_decreasing_unsigned(self, dt):
+        x = numpy.array([3, 2, 1], dtype=dt)
+        f = numpy.array([0, 2, 4])
+
+        dp_x = dpnp.array(x)
+        dp_f = dpnp.array(f)
+
+        expected = numpy.gradient(f, x)
+        result = dpnp.gradient(dp_f, dp_x)
+        assert_array_equal(result, expected)
+
+    @pytest.mark.parametrize(
+        "dt", [numpy.int8, numpy.int16, numpy.int32, numpy.int64]
+    )
+    def test_x_signed_int_big_jump(self, dt):
+        minint = numpy.iinfo(dt).min
+        maxint = numpy.iinfo(dt).max
+        x = numpy.array([-1, maxint], dtype=dt)
+        f = numpy.array([minint // 2, 0])
+
+        dp_x = dpnp.array(x)
+        dp_f = dpnp.array(f)
+
+        expected = numpy.gradient(f, x)
+        result = dpnp.gradient(dp_f, dp_x)
+        assert_array_equal(result, expected)
+
+    def test_return_type(self):
+        x = dpnp.array([[1, 2], [2, 3]])
+        res = dpnp.gradient(x)
+        assert type(res) is tuple
 
 
 @pytest.mark.parametrize("dtype1", get_all_dtypes())
@@ -1381,32 +1724,6 @@ class TestTrapz:
 
         result = dpnp.trapz(dpnp_y, dx=dx)
         expected = numpy.trapz(np_y, dx=dx)
-        assert_array_equal(expected, result)
-
-
-class TestGradient:
-    @pytest.mark.parametrize(
-        "array", [[2, 3, 6, 8, 4, 9], [3.0, 4.0, 7.5, 9.0], [2, 6, 8, 10]]
-    )
-    def test_gradient_y1(self, array):
-        np_y = numpy.array(array)
-        dpnp_y = dpnp.array(array)
-
-        result = dpnp.gradient(dpnp_y)
-        expected = numpy.gradient(np_y)
-        assert_array_equal(expected, result)
-
-    @pytest.mark.usefixtures("allow_fall_back_on_numpy")
-    @pytest.mark.parametrize(
-        "array", [[2, 3, 6, 8, 4, 9], [3.0, 4.0, 7.5, 9.0], [2, 6, 8, 10]]
-    )
-    @pytest.mark.parametrize("dx", [2, 3.5])
-    def test_gradient_y1_dx(self, array, dx):
-        np_y = numpy.array(array)
-        dpnp_y = dpnp.array(array)
-
-        result = dpnp.gradient(dpnp_y, dx)
-        expected = numpy.gradient(np_y, dx)
         assert_array_equal(expected, result)
 
 
