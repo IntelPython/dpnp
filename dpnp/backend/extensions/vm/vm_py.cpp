d2f31e599500f7d7ffb72af4457ec17bab854801
Anton Volkov <antonwolfy@gmail.com>
2024-06-05 16:57:57 +0200

    Separated implementation of asinh function

--- dpnp/backend/extensions/vm/vm_py.cpp
+++ dpnp/backend/extensions/vm/vm_py.cpp
@@ -75,7 +75,6 @@ namespace vm_ns = dpnp::extensions::vm;
 using vm_ext::binary_impl_fn_ptr_t;
 using vm_ext::unary_impl_fn_ptr_t;
 
-static unary_impl_fn_ptr_t asinh_dispatch_vector[dpctl_td_ns::num_types];
 static unary_impl_fn_ptr_t atan_dispatch_vector[dpctl_td_ns::num_types];
 static binary_impl_fn_ptr_t atan2_dispatch_vector[dpctl_td_ns::num_types];
 static unary_impl_fn_ptr_t atanh_dispatch_vector[dpctl_td_ns::num_types];
@@ -116,34 +115,7 @@ PYBIND11_MODULE(_vm_impl, m)
     vm_ns::init_acosh(m);
     vm_ns::init_add(m);
     vm_ns::init_asin(m);
-
-    // UnaryUfunc: ==== Asinh(x) ====
-    {
-        vm_ext::init_ufunc_dispatch_vector<unary_impl_fn_ptr_t,
-                                           vm_ext::AsinhContigFactory>(
-            asinh_dispatch_vector);
-
-        auto asinh_pyapi = [&](sycl::queue exec_q, arrayT src, arrayT dst,
-                               const event_vecT &depends = {}) {
-            return vm_ext::unary_ufunc(exec_q, src, dst, depends,
-                                       asinh_dispatch_vector);
-        };
-        m.def("_asinh", asinh_pyapi,
-              "Call `asinh` function from OneMKL VM library to compute "
-              "inverse cosine of vector elements",
-              py::arg("sycl_queue"), py::arg("src"), py::arg("dst"),
-              py::arg("depends") = py::list());
-
-        auto asinh_need_to_call_pyapi = [&](sycl::queue exec_q, arrayT src,
-                                            arrayT dst) {
-            return vm_ext::need_to_call_unary_ufunc(exec_q, src, dst,
-                                                    asinh_dispatch_vector);
-        };
-        m.def("_mkl_asinh_to_call", asinh_need_to_call_pyapi,
-              "Check input arguments to answer if `asinh` function from "
-              "OneMKL VM library can be used",
-              py::arg("sycl_queue"), py::arg("src"), py::arg("dst"));
-    }
+    vm_ns::init_asinh(m);
 
     // UnaryUfunc: ==== Atan(x) ====
     {
