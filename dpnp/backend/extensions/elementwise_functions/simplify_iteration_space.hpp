d553611d9670fb1ee50e6b293ffea2853d0f5655
Anton <100830759+antonwolfy@users.noreply.github.com>
2024-06-12 21:26:04 +0200

    Preparation to reuse future common dpctl f/w in functions from `vm` extension (#1868)
    * Preparation to reuse common dpctl f/w for VM functions
    
    * PoC to decouple abs implementation to separate source file
    
    * Reuse typedef for function poiter from dpctl.tensor
    
    * Define populating vectors by a separate macro
    
    * Move implementation of utility functions from headers to source to resolve link issues
    
    * Separated implementation of acos function
    
    * Separated implementation of acosh function
    
    * Use function to simplify strides from dpctl tensor headers
    
    * PoC to decouple add implementation to separate source file
    
    * Separated implementation of asin function
    
    * Separated implementation of asinh function
    
    * Separated implementation of atan, atan2, atanh functions
    
    * Resolve issue with calling MKL function for undefined types
    
    * Separated implementation of cbrt, ceil, conj, cos and cosh functions
    
    * Separated implementation of div, exp, exp2, expm1, floor and hypot functions
    
    * Separated implementation of ln, log1p, log2 and log10 functions
    
    * Separated implementation of mul, pow, rint, sin and sinh functions
    
    * Separated implementation of sqr, sqrt, sub, tan, tanh and trunc functions
    
    * Removed unused header with types matrix
    
    * Remove unused functions
    
    * Use passing by reference in unary and binary funcs

---
+++ dpnp/backend/extensions/elementwise_functions/simplify_iteration_space.hpp
@@ -0,0 +1,61 @@
+//*****************************************************************************
+// Copyright (c) 2024, Intel Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//*****************************************************************************
+
+#pragma once
+
+#include <pybind11/pybind11.h>
+#include <vector>
+
+namespace dpnp::extensions::py_internal
+{
+namespace py = pybind11;
+
+void simplify_iteration_space(int &,
+                              const py::ssize_t *const &,
+                              std::vector<py::ssize_t> const &,
+                              std::vector<py::ssize_t> const &,
+                              std::vector<py::ssize_t> &,
+                              std::vector<py::ssize_t> &,
+                              std::vector<py::ssize_t> &,
+                              py::ssize_t &,
+                              py::ssize_t &);
+
+void simplify_iteration_space_3(int &,
+                                const py::ssize_t *const &,
+                                // src1
+                                std::vector<py::ssize_t> const &,
+                                // src2
+                                std::vector<py::ssize_t> const &,
+                                // dst
+                                std::vector<py::ssize_t> const &,
+                                // output
+                                std::vector<py::ssize_t> &,
+                                std::vector<py::ssize_t> &,
+                                std::vector<py::ssize_t> &,
+                                std::vector<py::ssize_t> &,
+                                py::ssize_t &,
+                                py::ssize_t &,
+                                py::ssize_t &);
+} // namespace dpnp::extensions::py_internal
