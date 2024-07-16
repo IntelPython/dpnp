aff1dfafb607e6572b924895815c1db3753dd697
Anton Volkov <antonwolfy@gmail.com>
2024-07-16 18:39:21 +0200

    Unmuted tests where fixes are expected to be available

--- tests/third_party/cupy/linalg_tests/test_decomposition.py
+++ tests/third_party/cupy/linalg_tests/test_decomposition.py
@@ -156,14 +156,6 @@ class TestCholeskyInvalid(unittest.TestCase):
 )
 @testing.fix_random()
 class TestSVD(unittest.TestCase):
-    # TODO: New packages that fix issue CMPLRLLVM-53771 are only available in internal CI.
-    # Skip the tests on cpu until these packages are available for the external CI.
-    # Specifically dpcpp_linux-64>=2024.1.0
-    @classmethod
-    def setUpClass(cls):
-        if is_cpu_device():
-            raise unittest.SkipTest("CMPLRLLVM-53771")
-
     def setUp(self):
         self.seed = testing.generate_seed()
 
@@ -410,10 +402,6 @@ class TestQRDecomposition(unittest.TestCase):
             assert result_cpu.dtype == result_gpu.dtype
             testing.assert_allclose(result_cpu, result_gpu, atol=1e-4)
 
-    # TODO: New packages that fix issue CMPLRLLVM-53771 are only available in internal CI.
-    # Skip the tests on cpu until these packages are available for the external CI.
-    # Specifically dpcpp_linux-64>=2024.1.0
-    @pytest.mark.skipif(is_cpu_device(), reason="CMPLRLLVM-53771")
     @testing.fix_random()
     @_condition.repeat(3, 10)
     def test_mode(self):
@@ -421,7 +409,6 @@ class TestQRDecomposition(unittest.TestCase):
         self.check_mode(numpy.random.randn(3, 3), mode=self.mode)
         self.check_mode(numpy.random.randn(5, 4), mode=self.mode)
 
-    @pytest.mark.skipif(is_cpu_device(), reason="CMPLRLLVM-53771")
     @testing.with_requires("numpy>=1.22")
     @testing.fix_random()
     def test_mode_rank3(self):
@@ -429,7 +416,6 @@ class TestQRDecomposition(unittest.TestCase):
         self.check_mode(numpy.random.randn(4, 3, 3), mode=self.mode)
         self.check_mode(numpy.random.randn(2, 5, 4), mode=self.mode)
 
-    @pytest.mark.skipif(is_cpu_device(), reason="CMPLRLLVM-53771")
     @testing.with_requires("numpy>=1.22")
     @testing.fix_random()
     def test_mode_rank4(self):
