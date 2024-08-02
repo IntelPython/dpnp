9e274fd7279f10769244f6f76789c2120181ca9f
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2024-08-02 21:02:33 +0200

    Update cupy tests for nan_to_num()

--- tests/third_party/cupy/math_tests/test_misc.py
+++ tests/third_party/cupy/math_tests/test_misc.py
@@ -260,14 +260,14 @@ class TestMisc:
 
     @testing.numpy_cupy_array_equal()
     def test_nan_to_num_copy(self, xp):
-        x = xp.asarray([0, 1, xp.nan, 4], dtype=xp.float64)
+        x = xp.asarray([0, 1, xp.nan, 4], dtype=cupy.default_float_type())
         y = xp.nan_to_num(x, copy=True)
         assert x is not y
         return y
 
     @testing.numpy_cupy_array_equal()
     def test_nan_to_num_inplace(self, xp):
-        x = xp.asarray([0, 1, xp.nan, 4], dtype=xp.float64)
+        x = xp.asarray([0, 1, xp.nan, 4], dtype=cupy.default_float_type())
         y = xp.nan_to_num(x, copy=False)
         assert x is y
         return y
@@ -275,12 +275,15 @@ class TestMisc:
     @pytest.mark.parametrize("kwarg", ["nan", "posinf", "neginf"])
     def test_nan_to_num_broadcast(self, kwarg):
         for xp in (numpy, cupy):
-            x = xp.asarray([0, 1, xp.nan, 4], dtype=xp.float64)
-            y = xp.zeros((2, 4), dtype=xp.float64)
+            x = xp.asarray([0, 1, xp.nan, 4], dtype=cupy.default_float_type())
+            y = xp.zeros((2, 4), dtype=cupy.default_float_type())
             with pytest.raises(ValueError):
                 xp.nan_to_num(x, **{kwarg: y})
+            # dpnp.nan_to_num() doesn`t support a scalar as an input
+            # convert 0.0 to 0-ndim array
             with pytest.raises(ValueError):
-                xp.nan_to_num(0.0, **{kwarg: y})
+                x_ndim_0 = xp.array(0.0)
+                xp.nan_to_num(x_ndim_0, **{kwarg: y})
 
     @testing.for_all_dtypes(no_bool=True, no_complex=True)
     @testing.numpy_cupy_array_equal()
