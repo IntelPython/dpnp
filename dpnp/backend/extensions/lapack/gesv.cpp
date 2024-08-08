1e5ba88acc6589e04c7f2a6fa7ba071c8a564a56
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2024-08-08 10:36:09 +0200

    Implement gesv_batch via gesv call (#1877)
    * First working version with transpose and C contig
    
    * Second working version with moveaxis, transpose and F contig
    
    * Add more shape checks
    
    * Pass sycl::queue by reference for gesv/gesv_batch
    
    * Update _batched_solve implementation
    
    * Remove old impl in _batched_solve
    
    * Use py::gil_scoped_release before gesv call
    
    * Move gesv_batch to gesv_batch.cpp
    
    * Improve gesv_batch with independent linear streams
    
    * Extend checks for gesv/gesv_batch
    
    * Add common_gesv_checks
    
    * Release GIL in gesv_batch_impl
    
    * Remove host_task_events from gesv
    
    * Use check_zeros_shape in gesv and gesv_batch
    
    * Add additional checks for gesv_impl
    
    * Move alloc_scratchpad to common_helpers.hpp
    
    * Use helper::alloc_scratchpad in gesv_batch_impl
    
    * Remove current_scratch_gesv check
    
    * Remove lda, ldb pass to gesv_batch_impl, gesv_impl
    
    * Use const and constexpr in gesv/gesv_batch
    
    * Use dpnp.reshape in _batched_solve
    
    * Implement alloc_ipiv in common_helpers.hpp
    
    * Add gesv_common_utils.hpp
    
    * Implement handle_lapack_exc function
    
    * Use try/catch for scratchpad/ipiv allocation
    
    * Update alloc_scratchpad/alloc_ipiv
    
    * gesv_scratchpad_size can be 0
    
    * Implement help functions alloc_ipiv/alloc_scratchpad
    
    * Reuse alloc_scratchpad/ipiv in batch versions

--- dpnp/backend/extensions/lapack/gesv.cpp
+++ dpnp/backend/extensions/lapack/gesv.cpp
@@ -26,15 +26,12 @@
 #include <pybind11/pybind11.h>
 
 // dpctl tensor headers
-#include "utils/memory_overlap.hpp"
 #include "utils/type_utils.hpp"
 
 #include "common_helpers.hpp"
 #include "gesv.hpp"
-#include "linalg_exceptions.hpp"
+#include "gesv_common_utils.hpp"
 #include "types_matrix.hpp"
-
-#include "dpnp_utils.hpp"
 
 namespace dpnp::extensions::lapack
 {
@@ -42,27 +39,21 @@ namespace mkl_lapack = oneapi::mkl::lapack;
 namespace py = pybind11;
 namespace type_utils = dpctl::tensor::type_utils;
 
-typedef sycl::event (*gesv_impl_fn_ptr_t)(sycl::queue,
+typedef sycl::event (*gesv_impl_fn_ptr_t)(sycl::queue &,
                                           const std::int64_t,
                                           const std::int64_t,
                                           char *,
-                                          std::int64_t,
                                           char *,
-                                          std::int64_t,
-                                          std::vector<sycl::event> &,
                                           const std::vector<sycl::event> &);
 
 static gesv_impl_fn_ptr_t gesv_dispatch_vector[dpctl_td_ns::num_types];
 
 template <typename T>
-static sycl::event gesv_impl(sycl::queue exec_q,
+static sycl::event gesv_impl(sycl::queue &exec_q,
                              const std::int64_t n,
                              const std::int64_t nrhs,
                              char *in_a,
-                             std::int64_t lda,
                              char *in_b,
-                             std::int64_t ldb,
-                             std::vector<sycl::event> &host_task_events,
                              const std::vector<sycl::event> &depends)
 {
     type_utils::validate_type_for_device<T>(exec_q);
@@ -70,21 +61,28 @@ static sycl::event gesv_impl(sycl::queue exec_q,
     T *a = reinterpret_cast<T *>(in_a);
     T *b = reinterpret_cast<T *>(in_b);
 
+    const std::int64_t lda = std::max<size_t>(1UL, n);
+    const std::int64_t ldb = std::max<size_t>(1UL, n);
+
     const std::int64_t scratchpad_size =
         mkl_lapack::gesv_scratchpad_size<T>(exec_q, n, nrhs, lda, ldb);
-    T *scratchpad = nullptr;
+
+    T *scratchpad = helper::alloc_scratchpad<T>(scratchpad_size, exec_q);
 
     std::int64_t *ipiv = nullptr;
+    try {
+        ipiv = helper::alloc_ipiv(n, exec_q);
+    } catch (const std::exception &e) {
+        if (scratchpad != nullptr)
+            sycl::free(scratchpad, exec_q);
+        throw;
+    }
 
     std::stringstream error_msg;
-    std::int64_t info = 0;
     bool is_exception_caught = false;
 
     sycl::event gesv_event;
     try {
-        scratchpad = sycl::malloc_device<T>(scratchpad_size, exec_q);
-        ipiv = sycl::malloc_device<std::int64_t>(n, exec_q);
-
         gesv_event = mkl_lapack::gesv(
             exec_q,
             n,    // The order of the square matrix A
@@ -104,41 +102,8 @@ static sycl::event gesv_impl(sycl::queue exec_q,
             scratchpad_size, depends);
     } catch (mkl_lapack::exception const &e) {
         is_exception_caught = true;
-        info = e.info();
-
-        if (info < 0) {
-            error_msg << "Parameter number " << -info
-                      << " had an illegal value.";
-        }
-        else if (info == scratchpad_size && e.detail() != 0) {
-            error_msg
-                << "Insufficient scratchpad size. Required size is at least "
-                << e.detail();
-        }
-        else if (info > 0) {
-            T host_U;
-            exec_q.memcpy(&host_U, &a[(info - 1) * lda + info - 1], sizeof(T))
-                .wait();
-
-            using ThresholdType = typename helper::value_type_of<T>::type;
-
-            const auto threshold =
-                std::numeric_limits<ThresholdType>::epsilon() * 100;
-            if (std::abs(host_U) < threshold) {
-                sycl::free(scratchpad, exec_q);
-                throw LinAlgError("The input coefficient matrix is singular.");
-            }
-            else {
-                error_msg << "Unexpected MKL exception caught during gesv() "
-                             "call:\nreason: "
-                          << e.what() << "\ninfo: " << e.info();
-            }
-        }
-        else {
-            error_msg << "Unexpected MKL exception caught during gesv() "
-                         "call:\nreason: "
-                      << e.what() << "\ninfo: " << e.info();
-        }
+        gesv_utils::handle_lapack_exc(exec_q, lda, a, scratchpad_size,
+                                      scratchpad, ipiv, e, error_msg);
     } catch (sycl::exception const &e) {
         is_exception_caught = true;
         error_msg << "Unexpected SYCL exception caught during gesv() call:\n"
@@ -147,16 +112,14 @@ static sycl::event gesv_impl(sycl::queue exec_q,
 
     if (is_exception_caught) // an unexpected error occurs
     {
-        if (scratchpad != nullptr) {
+        if (scratchpad != nullptr)
             sycl::free(scratchpad, exec_q);
-        }
-        if (ipiv != nullptr) {
+        if (ipiv != nullptr)
             sycl::free(ipiv, exec_q);
-        }
         throw std::runtime_error(error_msg.str());
     }
 
-    sycl::event clean_up_event = exec_q.submit([&](sycl::handler &cgh) {
+    sycl::event ht_ev = exec_q.submit([&](sycl::handler &cgh) {
         cgh.depends_on(gesv_event);
         auto ctx = exec_q.get_context();
         cgh.host_task([ctx, scratchpad, ipiv]() {
@@ -164,13 +127,12 @@ static sycl::event gesv_impl(sycl::queue exec_q,
             sycl::free(ipiv, ctx);
         });
     });
-    host_task_events.push_back(clean_up_event);
 
-    return gesv_event;
+    return ht_ev;
 }
 
 std::pair<sycl::event, sycl::event>
-    gesv(sycl::queue exec_q,
+    gesv(sycl::queue &exec_q,
          dpctl::tensor::usm_ndarray coeff_matrix,
          dpctl::tensor::usm_ndarray dependent_vals,
          const std::vector<sycl::event> &depends)
@@ -178,66 +140,30 @@ std::pair<sycl::event, sycl::event>
     const int coeff_matrix_nd = coeff_matrix.get_ndim();
     const int dependent_vals_nd = dependent_vals.get_ndim();
 
-    if (coeff_matrix_nd != 2) {
-        throw py::value_error("The coefficient matrix has ndim=" +
-                              std::to_string(coeff_matrix_nd) +
-                              ", but a 2-dimensional array is expected.");
-    }
-
-    if (dependent_vals_nd > 2) {
-        throw py::value_error(
-            "The dependent values array has ndim=" +
-            std::to_string(dependent_vals_nd) +
-            ", but a 1-dimensional or a 2-dimensional array is expected.");
-    }
-
     const py::ssize_t *coeff_matrix_shape = coeff_matrix.get_shape_raw();
     const py::ssize_t *dependent_vals_shape = dependent_vals.get_shape_raw();
 
-    if (coeff_matrix_shape[0] != coeff_matrix_shape[1]) {
-        throw py::value_error("The coefficient matrix must be square,"
-                              " but got a shape of (" +
-                              std::to_string(coeff_matrix_shape[0]) + ", " +
-                              std::to_string(coeff_matrix_shape[1]) + ").");
-    }
+    constexpr int expected_coeff_matrix_ndim = 2;
+    constexpr int min_dependent_vals_ndim = 1;
+    constexpr int max_dependent_vals_ndim = 2;
 
-    // check compatibility of execution queue and allocation queue
-    if (!dpctl::utils::queues_are_compatible(exec_q,
-                                             {coeff_matrix, dependent_vals}))
+    gesv_utils::common_gesv_checks(
+        exec_q, coeff_matrix, dependent_vals, coeff_matrix_shape,
+        dependent_vals_shape, expected_coeff_matrix_ndim,
+        min_dependent_vals_ndim, max_dependent_vals_ndim);
+
+    // Ensure `batch_size`, `n` and 'nrhs' are non-zero, otherwise return empty
+    // events
+    if (helper::check_zeros_shape(coeff_matrix_nd, coeff_matrix_shape) ||
+        helper::check_zeros_shape(dependent_vals_nd, dependent_vals_shape))
     {
-        throw py::value_error(
-            "Execution queue is not compatible with allocation queues");
-    }
-
-    auto const &overlap = dpctl::tensor::overlap::MemoryOverlap();
-    if (overlap(coeff_matrix, dependent_vals)) {
-        throw py::value_error(
-            "The arrays of coefficients and dependent variables "
-            "are overlapping segments of memory");
-    }
-
-    bool is_coeff_matrix_f_contig = coeff_matrix.is_f_contiguous();
-    if (!is_coeff_matrix_f_contig) {
-        throw py::value_error("The coefficient matrix "
-                              "must be F-contiguous");
-    }
-
-    bool is_dependent_vals_f_contig = dependent_vals.is_f_contiguous();
-    if (!is_dependent_vals_f_contig) {
-        throw py::value_error("The array of dependent variables "
-                              "must be F-contiguous");
+        // nothing to do
+        return std::make_pair(sycl::event(), sycl::event());
     }
 
     auto array_types = dpctl_td_ns::usm_ndarray_types();
-    int coeff_matrix_type_id =
+    const int coeff_matrix_type_id =
         array_types.typenum_to_lookup_id(coeff_matrix.get_typenum());
-    int dependent_vals_type_id =
-        array_types.typenum_to_lookup_id(dependent_vals.get_typenum());
-
-    if (coeff_matrix_type_id != dependent_vals_type_id) {
-        throw py::value_error("The types of the coefficient matrix and "
-                              "dependent variables are mismatched");
-    }
 
     gesv_impl_fn_ptr_t gesv_fn = gesv_dispatch_vector[coeff_matrix_type_id];
     if (gesv_fn == nullptr) {
@@ -253,18 +179,13 @@ std::pair<sycl::event, sycl::event>
     const std::int64_t nrhs =
         (dependent_vals_nd > 1) ? dependent_vals_shape[1] : 1;
 
-    const std::int64_t lda = std::max<size_t>(1UL, n);
-    const std::int64_t ldb = std::max<size_t>(1UL, n);
+    sycl::event gesv_ev = gesv_fn(exec_q, n, nrhs, coeff_matrix_data,
+                                  dependent_vals_data, depends);
 
-    std::vector<sycl::event> host_task_events;
-    sycl::event gesv_ev =
-        gesv_fn(exec_q, n, nrhs, coeff_matrix_data, lda, dependent_vals_data,
-                ldb, host_task_events, depends);
+    sycl::event ht_ev = dpctl::utils::keep_args_alive(
+        exec_q, {coeff_matrix, dependent_vals}, {gesv_ev});
 
-    sycl::event args_ev = dpctl::utils::keep_args_alive(
-        exec_q, {coeff_matrix, dependent_vals}, host_task_events);
-
-    return std::make_pair(args_ev, gesv_ev);
+    return std::make_pair(ht_ev, gesv_ev);
 }
 
 template <typename fnT, typename T>
