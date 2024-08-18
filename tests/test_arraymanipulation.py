fffa78d9ee4504bdf88c87528ef15d902470df3b
Anton Volkov <antonwolfy@gmail.com>
2024-08-18 13:49:08 +0200

    Add skip fixture in test_arraymanipulation.py::test_asfarray2

--- tests/test_arraymanipulation.py
+++ tests/test_arraymanipulation.py
@@ -845,6 +845,7 @@ def test_asfarray(dtype, data):
     assert_array_equal(result, expected)
 
 
+@testing.with_requires("numpy<2.0")
 @pytest.mark.usefixtures("suppress_complex_warning")
 @pytest.mark.parametrize("dtype", get_all_dtypes())
 @pytest.mark.parametrize("data", [[1.0, 2.0, 3.0]], ids=["[1., 2., 3.]"])
