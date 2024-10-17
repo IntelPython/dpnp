82b55685488494c48e7580199f5f2af130c14158
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2024-10-17 14:16:32 +0200

    Add tests folder to conda package

--- setup.py
+++ setup.py
@@ -69,6 +69,10 @@ setup(
     ],
     package_data={
         "dpnp": [
+            "tests/*.*",
+            "tests/testing/*.py",
+            "tests/third_party/cupy/*.py",
+            "tests/third_party/cupy/*/*.py",
             "backend/include/*.hpp",
             "libdpnp_backend_c.so",
             "dpnp_backend_c.lib",
