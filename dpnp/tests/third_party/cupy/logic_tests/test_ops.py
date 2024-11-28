6a275a4858e9d389d5a845770baa20f016a25285
Anton Volkov <antonwolfy@gmail.com>
2024-11-28 11:47:40 +0100

    Update logic_tests/test_type_test.py

--- dpnp/tests/third_party/cupy/logic_tests/test_ops.py
+++ dpnp/tests/third_party/cupy/logic_tests/test_ops.py
@@ -4,6 +4,7 @@ from dpnp.tests.third_party.cupy import testing
 
 
 class TestOps(unittest.TestCase):
+
     @testing.for_all_dtypes(no_complex=True)
     @testing.numpy_cupy_allclose(atol=1e-5)
     def check_unary(self, name, xp, dtype):
