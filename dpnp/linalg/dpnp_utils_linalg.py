7e988dae46d3aace7a033a04fff803710ff964e0
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2025-01-17 05:31:53 -0800

    Do not use event manager for w/a

--- dpnp/linalg/dpnp_utils_linalg.py
+++ dpnp/linalg/dpnp_utils_linalg.py
@@ -397,13 +397,14 @@ def _batched_qr(a, mode="reduced"):
         batch_size,
         depends=[copy_ev],
     )
-    _manager.add_event_pair(ht_ev, geqrf_ev)
 
     # w/a to avoid raice conditional on CUDA during multiple runs
     # TODO: Remove it ones the OneMath issue is resolved
     # https://github.com/uxlfoundation/oneMath/issues/626
     if dpnp.is_cuda_backend(a_sycl_queue):
         ht_ev.wait()
+    else:
+        _manager.add_event_pair(ht_ev, geqrf_ev)
 
     if mode in ["r", "raw"]:
         if mode == "r":
@@ -2474,13 +2475,14 @@ def dpnp_qr(a, mode="reduced"):
     ht_ev, geqrf_ev = li._geqrf(
         a_sycl_queue, a_t.get_array(), tau_h.get_array(), depends=[copy_ev]
     )
-    _manager.add_event_pair(ht_ev, geqrf_ev)
 
     # w/a to avoid raice conditional on CUDA during multiple runs
     # TODO: Remove it ones the OneMath issue is resolved
     # https://github.com/uxlfoundation/oneMath/issues/626
     if dpnp.is_cuda_backend(a_sycl_queue):
         ht_ev.wait()
+    else:
+        _manager.add_event_pair(ht_ev, geqrf_ev)
 
     if mode in ["r", "raw"]:
         if mode == "r":
