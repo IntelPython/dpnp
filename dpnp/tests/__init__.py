ad43560bd1f868fab1ed8294636e4ff6625259fa
Anton Volkov <antonwolfy@gmail.com>
2025-01-19 12:48:29 +0100

    Add proper casting of input arrays passed to assert_almost_equal

--- dpnp/tests/__init__.py
+++ dpnp/tests/__init__.py
@@ -3,5 +3,6 @@ import numpy
 from . import testing
 
 numpy.testing.assert_allclose = testing.assert_allclose
+numpy.testing.assert_almost_equal = testing.assert_almost_equal
 numpy.testing.assert_array_equal = testing.assert_array_equal
 numpy.testing.assert_equal = testing.assert_equal
--- dpnp/tests/testing/__init__.py
+++ dpnp/tests/testing/__init__.py
@@ -1,5 +1,6 @@
 from .array import (
     assert_allclose,
+    assert_almost_equal,
     assert_array_equal,
     assert_equal,
 )
