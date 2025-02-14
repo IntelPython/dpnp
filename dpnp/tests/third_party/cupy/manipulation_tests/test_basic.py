93535bd874ace78e8f9c1ba5873ed79a7367bd8a
Anton <100830759+antonwolfy@users.noreply.github.com>
2025-02-14 13:37:24 +0100

    Update third party tests with the latest changes (#2312)
    The PR proposes to align third party tests with the latest content.

--- dpnp/tests/third_party/cupy/manipulation_tests/test_basic.py
+++ dpnp/tests/third_party/cupy/manipulation_tests/test_basic.py
@@ -199,10 +199,6 @@ class TestBasic:
         testing.assert_array_equal(expected, dst.get())
 
 
-@pytest.mark.skipif(
-    numpy.__version__ < "2",
-    reason="XXX: NP2.0: copyto is in flux in numpy 2.0.0rc2",
-)
 @testing.parameterize(
     *testing.product(
         {
