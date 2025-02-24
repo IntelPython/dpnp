8efd4389a8998ff6f285553457e1cf2bfc0e26e3
vlad-perevezentsev <vladislav.perevezentsev@intel.com>
2025-02-24 20:05:52 +0100

    Update `dpnp.fft` to run on CUDA (#2332)
    This PR suggests fixing the current issues with `dpnp.fft.fftn()` and
    `dpnp.fft.rfftn()` on CUDA and removing the skip tests for them
    
    The `incorrect result` issue for `dpnp.fft.fftn()` on cuda was because
    preparing the input array when `batch_fft=True` could change it to `F
    contiguous` array. cuFFT for correct execution requires `C contiguous`
    array as input.
    
    The issue with raising `Invalid strides` error is a bug in oneMath
    ([631](https://github.com/uxlfoundation/oneMath/issues/631)).
    As a workaround until this issue is solved it is suggested to use swap
    of the last two axes if the last dimension is 1 and there are multiple
    axes.
    In this case the strides inside cuFFT are calculated correctly.
    
    Additionally updated the arguments for `test_erf` in
    `skipped_tests_cuda.tbl` to skip it on cuda.

--- dpnp/fft/dpnp_utils_fft.py
+++ dpnp/fft/dpnp_utils_fft.py
@@ -395,12 +395,32 @@ def _fft(a, norm, out, forward, in_place, c2c, axes, batch_fft=True):
         a = dpnp.reshape(a, local_shape)
         index = 1
 
+        # cuFFT requires input arrays to be C-contiguous (row-major)
+        # for correct execution
+        if (
+            dpnp.is_cuda_backend(a) and not a.flags.c_contiguous
+        ):  # pragma: no cover
+            a = dpnp.ascontiguousarray(a)
+
+    # w/a for cuFFT to avoid "Invalid strides" error when
+    # the last dimension is 1 and there are multiple axes
+    # by swapping the last two axes to correct the input.
+    # TODO: Remove this ones the OneMath issue is resolved
+    # https://github.com/uxlfoundation/oneMath/issues/631
+    cufft_wa = dpnp.is_cuda_backend(a) and a.shape[-1] == 1 and len(axes) > 1
+    if cufft_wa:  # pragma: no cover
+        a = dpnp.moveaxis(a, -1, -2)
+
     a_strides = _standardize_strides_to_nonzero(a.strides, a.shape)
     dsc, out_strides = _commit_descriptor(
         a, forward, in_place, c2c, a_strides, index, batch_fft
     )
     res = _compute_result(dsc, a, out, forward, c2c, out_strides)
     res = _scale_result(res, a.shape, norm, forward, index)
+
+    # Revert swapped axes
+    if cufft_wa:  # pragma: no cover
+        res = dpnp.moveaxis(res, -1, -2)
 
     if batch_fft:
         tmp_shape = a_shape_orig[:-1] + (res.shape[-1],)
