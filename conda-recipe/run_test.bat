f914622f658b0349a1c57a94791825575c527d17
Anton Volkov <antonwolfy@gmail.com>
2025-02-25 11:16:17 +0100

    Reduce test scope to specific class/function in test_usm_type.py

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -42,8 +42,26 @@ if %errorlevel% neq 0 exit 1
 "%PYTHON%" -m dpctl -f
 if %errorlevel% neq 0 exit 1
 
+echo "Test only dpnp.tests.test_usm_type::test_norm"
+
+REM https://cje-fm-owrp-prod04.devtools.intel.com/satg-dap-intelpython/job/intel-packages/job/dpnp/job/dev-windows-py3.12/job/test-stable/137/
+"%PYTHON%" -m pytest --count 100 -ra -v --pyargs dpnp.tests.test_usm_type::test_norm
+if %errorlevel% neq 0 exit 1
+
+echo "Test only dpnp.tests.test_usm_type::TestFft"
+
+REM https://cje-fm-owrp-prod04.devtools.intel.com/satg-dap-intelpython/job/intel-packages/job/dpnp/job/dev-windows-py3.11/job/test/1713/
+REM https://cje-fm-owrp-prod04.devtools.intel.com/satg-dap-intelpython/job/intel-packages/job/dpnp/job/dev-windows-py3.11/job/test-wheel-conda-stable/232/
+REM https://cje-fm-owrp-prod04.devtools.intel.com/satg-dap-intelpython/job/intel-packages/job/dpnp/job/dev-windows-py3.12/job/test-wheel-conda-stable/234/
+"%PYTHON%" -m pytest --count 100 -ra -v --pyargs dpnp.tests.test_usm_type::TestFft
+if %errorlevel% neq 0 exit 1
+
+echo "Test only dpnp.tests.test_usm_type"
+
 "%PYTHON%" -m pytest --count 100 -ra -v --pyargs dpnp.tests.test_usm_type
 if %errorlevel% neq 0 exit 1
+
+echo "Test everything"
 
 "%PYTHON%" -m pytest -ra --pyargs dpnp
 if %errorlevel% neq 0 exit 1
