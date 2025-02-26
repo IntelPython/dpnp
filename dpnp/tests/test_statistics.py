31c63556548635021a337317431d038dcb52f16f
Vahid Tavanashad <vahid.tavanashad@intel.com>
2025-02-26 08:35:56 -0600

    update test_nanfunctions.py

--- dpnp/tests/test_statistics.py
+++ dpnp/tests/test_statistics.py
@@ -602,20 +602,9 @@ class TestCov:
 class TestMaxMin:
     @pytest.mark.parametrize("axis", [None, 0, 1, -1, 2, -2, (1, 2), (0, -2)])
     @pytest.mark.parametrize("keepdims", [False, True])
-    @pytest.mark.parametrize("dtype", get_all_dtypes(no_bool=True))
+    @pytest.mark.parametrize("dtype", get_all_dtypes(no_bool=None))
     def test_func(self, func, axis, keepdims, dtype):
-        a = numpy.arange(768, dtype=dtype).reshape((4, 4, 6, 8))
-        ia = dpnp.array(a)
-
-        expected = getattr(numpy, func)(a, axis=axis, keepdims=keepdims)
-        result = getattr(dpnp, func)(ia, axis=axis, keepdims=keepdims)
-        assert_dtype_allclose(result, expected)
-
-    @pytest.mark.parametrize("axis", [None, 0, 1, -1])
-    @pytest.mark.parametrize("keepdims", [False, True])
-    def test_bool(self, func, axis, keepdims):
-        a = numpy.arange(2, dtype=numpy.bool_)
-        a = numpy.tile(a, (2, 2))
+        a = generate_random_numpy_array((4, 4, 6, 8), dtype=dtype)
         ia = dpnp.array(a)
 
         expected = getattr(numpy, func)(a, axis=axis, keepdims=keepdims)
