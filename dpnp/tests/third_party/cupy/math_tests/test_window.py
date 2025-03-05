aa5ed7e7518ef105aa2ff76ada32d99d4ee61445
Vahid Tavanashad <vahid.tavanashad@intel.com>
2025-03-05 06:47:48 -0800

    implement dpnp.blackman

--- dpnp/tests/third_party/cupy/math_tests/test_window.py
+++ dpnp/tests/third_party/cupy/math_tests/test_window.py
@@ -10,8 +10,8 @@ from dpnp.tests.third_party.cupy import testing
     *testing.product(
         {
             "m": [0, 1, -1, 1024],
-            # TODO: add ["bartlett", "blackman"] when supported
-            "name": ["hamming", "hanning"],
+            # TODO: add ["bartlett"] when supported
+            "name": ["blackman", "hamming", "hanning"],
         }
     )
 )
