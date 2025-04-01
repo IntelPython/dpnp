d2da9863ba82179738d87d91095d05c310e0fd84
Vahid Tavanashad <120411540+vtavana@users.noreply.github.com>
2025-04-01 13:26:05 -0500

    define a new helper function for tests called `get_integer_float_dtypes` (#2402)
    A new helper function is defined for tests, to get integer and float
    dtypes.

--- dpnp/tests/helper.py
+++ dpnp/tests/helper.py
@@ -325,6 +325,36 @@ def get_integer_dtypes(all_int_types=False, no_unsigned=False):
     return dtypes
 
 
+def get_integer_float_dtypes(
+    all_int_types=False,
+    no_unsigned=False,
+    no_float16=True,
+    device=None,
+    xfail_dtypes=None,
+    exclude=None,
+):
+    """
+    Build a list of integer and float types supported by DPNP.
+    """
+    dtypes = get_integer_dtypes(
+        all_int_types=all_int_types, no_unsigned=no_unsigned
+    )
+    dtypes += get_float_dtypes(no_float16=no_float16, device=device)
+
+    def mark_xfail(dtype):
+        if xfail_dtypes is not None and dtype in xfail_dtypes:
+            return pytest.param(dtype, marks=pytest.mark.xfail)
+        return dtype
+
+    def not_excluded(dtype):
+        if exclude is None:
+            return True
+        return dtype not in exclude
+
+    dtypes = [mark_xfail(dtype) for dtype in dtypes if not_excluded(dtype)]
+    return dtypes
+
+
 def has_support_aspect16(device=None):
     """
     Return True if the device supports 16-bit precision floating point operations,
