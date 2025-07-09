f2425e9f8b7897eb821479435767eb8450662ac5
Vahid Tavanashad <vahid.tavanashad@intel.com>
2025-07-09 16:59:53 -0700

    use two copy kernel

--- dpnp/backend/extensions/blas/syrk.cpp
+++ dpnp/backend/extensions/blas/syrk.cpp
@@ -27,6 +27,8 @@
 
 #include <pybind11/pybind11.h>
 
+#include "ext/common.hpp"
+
 // dpctl tensor headers
 #include "utils/memory_overlap.hpp"
 #include "utils/output_validation.hpp"
@@ -36,6 +38,8 @@
 #include "types_matrix.hpp"
 
 #include "dpnp_utils.hpp"
+
+using ext::common::Align;
 
 namespace dpnp::extensions::blas
 {
@@ -59,6 +63,114 @@ typedef sycl::event (*syrk_impl_fn_ptr_t)(sycl::queue &,
 static syrk_impl_fn_ptr_t syrk_dispatch_vector[dpctl_td_ns::num_types];
 
 template <typename T>
+sycl::event launch_CPU_kernel(sycl::queue &exec_q,
+                              T *res,
+                              const std::int64_t ldc,
+                              const std::int64_t n,
+#if !defined(USE_ONEMATH_CUBLAS)
+                              const bool is_row_major,
+#endif // !USE_ONEMATH_CUBLAS
+                              sycl::event depends)
+{
+    static constexpr std::size_t tile_sz = 8;
+    sycl::range<2> global_range(Align(n, tile_sz), Align(n, tile_sz));
+    sycl::range<2> local_range(tile_sz, tile_sz);
+
+    return exec_q.submit([&](sycl::handler &h) {
+        h.depends_on(depends);
+
+        h.parallel_for(sycl::nd_range<2>(global_range, local_range),
+                       [=](sycl::nd_item<2> item) {
+                           int i = item.get_global_id(0);
+                           int j = item.get_global_id(1);
+
+                           if (i < n && j < n && i > j) {
+#if defined(USE_ONEMATH_CUBLAS)
+                               res[j * ldc + i] = res[i * ldc + j];
+#else
+                    if (is_row_major) {
+                        res[i * ldc + j] = res[j * ldc + i];
+                    } else {
+                        res[j * ldc + i] = res[i * ldc + j];
+                    }
+#endif
+                           }
+                       });
+    });
+}
+
+template <typename T>
+sycl::event launch_GPU_kernel(sycl::queue &exec_q,
+                              T *res,
+                              const std::int64_t ldc,
+                              const std::int64_t n,
+#if !defined(USE_ONEMATH_CUBLAS)
+                              const bool is_row_major,
+#endif // !USE_ONEMATH_CUBLAS
+                              sycl::event depends)
+{
+    return exec_q.submit([&](sycl::handler &h) {
+        h.depends_on(depends);
+
+        h.parallel_for(
+            sycl::range<2>{static_cast<size_t>(n), static_cast<size_t>(n)},
+            [=](sycl::id<2> idx) {
+                int i = idx[0];
+                int j = idx[1];
+
+                if (j > i) {
+#if defined(USE_ONEMATH_CUBLAS)
+                    res[i * ldc + j] = res[j * ldc + i];
+#else
+                    // result form row_major::syrk is row major and result form
+                    // column_major::syrk is column major, so copying upper
+                    // triangle to lower triangle is different for each case
+                    if (is_row_major) {
+                        // row-major: res[i][j] = res[i * ldc + j]
+                        res[j * ldc + i] = res[i * ldc + j];
+                    }
+                    else {
+                        // column-major: res[i][j] = res[i + j * ldc]
+                        res[i * ldc + j] = res[j * ldc + i];
+                    }
+                }
+#endif // !USE_ONEMATH_CUBLAS
+                });
+            });
+}
+
+// kernel to copy upper triangle to lower triangle
+template <typename T>
+sycl::event run_copy(sycl::queue &exec_q,
+                     T *res,
+                     const std::int64_t ldc,
+                     const std::int64_t n,
+#if !defined(USE_ONEMATH_CUBLAS)
+                     const bool is_row_major,
+#endif // !USE_ONEMATH_CUBLAS
+                     sycl::event depends)
+{
+        auto dev_type =
+            exec_q.get_device().get_info<sycl::info::device::device_type>();
+
+        // two separate kernels are used to have better performance compared
+        // to gemm on both CPU and GPU
+        sycl::event copy_event;
+        if (dev_type == sycl::info::device_type::gpu) {
+            copy_event =
+                launch_GPU_kernel(exec_q, res, ldc, n, is_row_major, depends);
+        }
+        else if (dev_type == sycl::info::device_type::cpu) {
+            copy_event =
+                launch_CPU_kernel(exec_q, res, ldc, n, is_row_major, depends);
+        }
+        else {
+            assert(false && "Unsupported device type (not CPU or GPU)");
+        }
+        return copy_event;
+}
+
+template <typename T>
 static sycl::event syrk_impl(sycl::queue &exec_q,
                              const oneapi::mkl::transpose transA,
                              const std::int64_t n,
@@ -72,26 +184,27 @@ static sycl::event syrk_impl(sycl::queue &exec_q,
 #endif // !USE_ONEMATH_CUBLAS
                              const std::vector<sycl::event> &depends)
 {
-    type_utils::validate_type_for_device<T>(exec_q);
+        type_utils::validate_type_for_device<T>(exec_q);
 
-    const T *a = reinterpret_cast<const T *>(matrixA);
-    T *res = reinterpret_cast<T *>(resultC);
+        const T *a = reinterpret_cast<const T *>(matrixA);
+        T *res = reinterpret_cast<T *>(resultC);
 
-    std::stringstream error_msg;
-    bool is_exception_caught = false;
+        std::stringstream error_msg;
+        bool is_exception_caught = false;
 
-    sycl::event syrk_event;
-    try {
-        auto syrk_func =
-            [&](sycl::queue &q, oneapi::mkl::uplo upper_lower,
-                oneapi::mkl::transpose transA, const std::int64_t n,
-                const std::int64_t k, T alpha, const T *a,
-                const std::int64_t lda, T beta, T *c, const std::int64_t ldc,
-                const std::vector<sycl::event> &deps) -> sycl::event {
+        sycl::event syrk_event;
+        try {
+            auto syrk_func =
+                [&](sycl::queue &q, oneapi::mkl::uplo upper_lower,
+                    oneapi::mkl::transpose transA, const std::int64_t n,
+                    const std::int64_t k, T alpha, const T *a,
+                    const std::int64_t lda, T beta, T *c,
+                    const std::int64_t ldc,
+                    const std::vector<sycl::event> &deps) -> sycl::event {
 #if defined(USE_ONEMATH_CUBLAS)
-            return mkl_blas::column_major::syrk(q, upper_lower, transA, n, k,
-                                                alpha, a, lda, beta, c, ldc,
-                                                deps);
+                return mkl_blas::column_major::syrk(q, upper_lower, transA, n,
+                                                    k, alpha, a, lda, beta, c,
+                                                    ldc, deps);
 #else
             if (is_row_major) {
                 return mkl_blas::row_major::syrk(q, upper_lower, transA, n, k,
@@ -104,69 +217,46 @@ static sycl::event syrk_impl(sycl::queue &exec_q,
                                                     ldc, deps);
             }
 #endif // USE_ONEMATH_CUBLAS
-        };
+            };
 
-        // we pass beta = 0, so passing upper or lower does not matter
-        static constexpr auto uplo = oneapi::mkl::uplo::upper;
-        syrk_event = syrk_func(
-            exec_q,
-            uplo,   // Specifies whether C’s data is stored in its upper
-                    // or lower triangle
-            transA, // Defines the transpose operation for matrix A:
-                    // 'N' indicates no transpose, 'T' for transpose,
-                    // or 'C' for a conjugate transpose.
-            n,      // Number of rows in op(A).
-                    // Number of rows and columns in C.
-            k,      // Number of columns in op(A).
-            T(1),   // Scaling factor for the rank-k update.
-            a,      // Pointer to the input matrix A.
-            lda,    // Leading dimension of matrix A, which is the
-                    // stride between successive rows (for row major layout).
-            T(0),   // Scaling factor for matrix C.
-            res,    // Pointer to output matrix c, where the result is stored.
-            ldc,    // Leading dimension of matrix C.
-            depends);
-    } catch (oneapi::mkl::exception const &e) {
-        error_msg
-            << "Unexpected MKL exception caught during syrk() call:\nreason: "
-            << e.what();
-        is_exception_caught = true;
-    } catch (sycl::exception const &e) {
-        error_msg << "Unexpected SYCL exception caught during syrk() call:\n"
-                  << e.what();
-        is_exception_caught = true;
-    }
+            // we pass beta = 0, so passing upper or lower does not matter
+            static constexpr auto uplo = oneapi::mkl::uplo::upper;
+            syrk_event = syrk_func(
+                exec_q,
+                uplo,   // Specifies whether C’s data is stored in its upper
+                        // or lower triangle
+                transA, // Defines the transpose operation for matrix A:
+                        // 'N' indicates no transpose, 'T' for transpose,
+                        // or 'C' for a conjugate transpose.
+                n,      // Number of rows in op(A).
+                        // Number of rows and columns in C.
+                k,      // Number of columns in op(A).
+                T(1),   // Scaling factor for the rank-k update.
+                a,      // Pointer to the input matrix A.
+                lda,    // Leading dimension of matrix A, which is the
+                     // stride between successive rows (for row major layout).
+                T(0), // Scaling factor for matrix C.
+                res,  // Pointer to output matrix c, where the result is stored.
+                ldc,  // Leading dimension of matrix C.
+                depends);
+        } catch (oneapi::mkl::exception const &e) {
+            error_msg << "Unexpected MKL exception caught during syrk() "
+                         "call:\nreason: "
+                      << e.what();
+            is_exception_caught = true;
+        } catch (sycl::exception const &e) {
+            error_msg
+                << "Unexpected SYCL exception caught during syrk() call:\n"
+                << e.what();
+            is_exception_caught = true;
+        }
 
-    if (is_exception_caught) // an unexpected error occurs
-    {
-        throw std::runtime_error(error_msg.str());
-    }
+        if (is_exception_caught) // an unexpected error occurs
+        {
+            throw std::runtime_error(error_msg.str());
+        }
 
-    // kernel to copy upper triangle to lower triangle
-    sycl::event copy_event = exec_q.submit([&](sycl::handler &h) {
-        h.depends_on(syrk_event);
-
-        h.parallel_for(
-            sycl::range<2>{static_cast<size_t>(n), static_cast<size_t>(n)},
-            [=](sycl::id<2> idx) {
-                std::int64_t i = idx[0];
-                std::int64_t j = idx[1];
-                if (j > i) {
-                    // result form row_major::syrk is row major and result form
-                    // column_major::syrk is column major, so copying upper
-                    // triangle to lower triangle is different for each case
-                    if (is_row_major) {
-                        // row-major: res[i][j] = res[i * ldc + j]
-                        res[j * ldc + i] = res[i * ldc + j];
-                    }
-                    else {
-                        // column-major: res[i][j] = res[i + j * ldc]
-                        res[i * ldc + j] = res[j * ldc + i];
-                    }
-                }
-            });
-    });
-    return copy_event;
+        return run_copy(exec_q, res, ldc, n, is_row_major, syrk_event);
 }
 
 std::pair<sycl::event, sycl::event>
@@ -175,64 +265,65 @@ std::pair<sycl::event, sycl::event>
          const dpctl::tensor::usm_ndarray &resultC,
          const std::vector<sycl::event> &depends)
 {
-    const int matrixA_nd = matrixA.get_ndim();
-    const int resultC_nd = resultC.get_ndim();
+        const int matrixA_nd = matrixA.get_ndim();
+        const int resultC_nd = resultC.get_ndim();
 
-    if ((matrixA_nd != 2) || (resultC_nd != 2)) {
-        throw py::value_error("The given arrays have incorrect dimensions.");
-    }
+        if ((matrixA_nd != 2) || (resultC_nd != 2)) {
+            throw py::value_error(
+                "The given arrays have incorrect dimensions.");
+        }
 
-    auto const &overlap = dpctl::tensor::overlap::MemoryOverlap();
-    if (overlap(matrixA, resultC)) {
-        throw py::value_error("Input and output matrices are overlapping "
-                              "segments of memory");
-    }
+        auto const &overlap = dpctl::tensor::overlap::MemoryOverlap();
+        if (overlap(matrixA, resultC)) {
+            throw py::value_error("Input and output matrices are overlapping "
+                                  "segments of memory");
+        }
 
-    if (!dpctl::utils::queues_are_compatible(
-            exec_q, {matrixA.get_queue(), resultC.get_queue()}))
-    {
-        throw py::value_error(
-            "USM allocations are not compatible with the execution queue.");
-    }
+        if (!dpctl::utils::queues_are_compatible(
+                exec_q, {matrixA.get_queue(), resultC.get_queue()}))
+        {
+            throw py::value_error(
+                "USM allocations are not compatible with the execution queue.");
+        }
 
-    const py::ssize_t *a_shape = matrixA.get_shape_raw();
-    const py::ssize_t *c_shape = resultC.get_shape_raw();
-    if (c_shape[0] != c_shape[1]) {
-        throw py::value_error("The output matrix should be square.");
-    }
-    if (a_shape[0] != c_shape[0]) {
-        throw py::value_error("The number of rows in A must be equal to "
-                              "the number of rows in result array.");
-    }
+        const py::ssize_t *a_shape = matrixA.get_shape_raw();
+        const py::ssize_t *c_shape = resultC.get_shape_raw();
+        if (c_shape[0] != c_shape[1]) {
+            throw py::value_error("The output matrix should be square.");
+        }
+        if (a_shape[0] != c_shape[0]) {
+            throw py::value_error("The number of rows in A must be equal to "
+                                  "the number of rows in result array.");
+        }
 
-    const bool is_matrixA_f_contig = matrixA.is_f_contiguous();
-    const bool is_matrixA_c_contig = matrixA.is_c_contiguous();
-    if (!is_matrixA_f_contig && !is_matrixA_c_contig) {
-        throw py::value_error(
-            "Input matrix is not c-contiguous nor f-contiguous.");
-    }
+        const bool is_matrixA_f_contig = matrixA.is_f_contiguous();
+        const bool is_matrixA_c_contig = matrixA.is_c_contiguous();
+        if (!is_matrixA_f_contig && !is_matrixA_c_contig) {
+            throw py::value_error(
+                "Input matrix is not c-contiguous nor f-contiguous.");
+        }
 
-    oneapi::mkl::transpose transA;
-    std::size_t src_nelems;
+        oneapi::mkl::transpose transA;
+        std::size_t src_nelems;
 
 // cuBLAS supports only column-major storage
 #if defined(USE_ONEMATH_CUBLAS)
-    const bool is_row_major = false;
-    std::int64_t n;
-    std::int64_t k;
+        const bool is_row_major = false;
+        std::int64_t n;
+        std::int64_t k;
 
-    if (is_matrixA_f_contig) {
-        transA = oneapi::mkl::transpose::N;
-        n = a_shape[0];
-        k = a_shape[1];
-        src_nelems = n * n;
-    }
-    else {
-        transA = oneapi::mkl::transpose::T;
-        k = a_shape[0];
-        n = a_shape[1];
-        src_nelems = k * k;
-    }
+        if (is_matrixA_f_contig) {
+            transA = oneapi::mkl::transpose::N;
+            n = a_shape[0];
+            k = a_shape[1];
+            src_nelems = n * n;
+        }
+        else {
+            transA = oneapi::mkl::transpose::T;
+            k = a_shape[0];
+            n = a_shape[1];
+            src_nelems = k * k;
+        }
 #else
     bool is_row_major = true;
     if (is_matrixA_f_contig) {
@@ -245,42 +336,43 @@ std::pair<sycl::event, sycl::event>
     src_nelems = n * n;
 #endif // USE_ONEMATH_CUBLAS
 
-    const std::int64_t lda = is_row_major ? k : n;
-    const std::int64_t ldc = n;
-    dpctl::tensor::validation::CheckWritable::throw_if_not_writable(resultC);
-    dpctl::tensor::validation::AmpleMemory::throw_if_not_ample(resultC,
-                                                               src_nelems);
+        const std::int64_t lda = is_row_major ? k : n;
+        const std::int64_t ldc = n;
+        dpctl::tensor::validation::CheckWritable::throw_if_not_writable(
+            resultC);
+        dpctl::tensor::validation::AmpleMemory::throw_if_not_ample(resultC,
+                                                                   src_nelems);
 
-    const int matrixA_typenum = matrixA.get_typenum();
-    const int resultC_typenum = resultC.get_typenum();
-    if (matrixA_typenum != resultC_typenum) {
-        throw py::value_error("Given arrays must be of the same type.");
-    }
+        const int matrixA_typenum = matrixA.get_typenum();
+        const int resultC_typenum = resultC.get_typenum();
+        if (matrixA_typenum != resultC_typenum) {
+            throw py::value_error("Given arrays must be of the same type.");
+        }
 
-    auto array_types = dpctl_td_ns::usm_ndarray_types();
-    const int type_id = array_types.typenum_to_lookup_id(matrixA_typenum);
-    syrk_impl_fn_ptr_t syrk_fn = syrk_dispatch_vector[type_id];
-    if (syrk_fn == nullptr) {
-        throw py::value_error(
-            "No syrk implementation is available for the specified data type "
-            "of the input and output arrays.");
-    }
+        auto array_types = dpctl_td_ns::usm_ndarray_types();
+        const int type_id = array_types.typenum_to_lookup_id(matrixA_typenum);
+        syrk_impl_fn_ptr_t syrk_fn = syrk_dispatch_vector[type_id];
+        if (syrk_fn == nullptr) {
+            throw py::value_error("No syrk implementation is available for the "
+                                  "specified data type "
+                                  "of the input and output arrays.");
+        }
 
-    const char *a_typeless_ptr = matrixA.get_data();
-    char *r_typeless_ptr = resultC.get_data();
+        const char *a_typeless_ptr = matrixA.get_data();
+        char *r_typeless_ptr = resultC.get_data();
 
 #if defined(USE_ONEMATH_CUBLAS)
-    sycl::event syrk_ev = syrk_fn(exec_q, transA, n, k, a_typeless_ptr, lda,
-                                  r_typeless_ptr, ldc, depends);
+        sycl::event syrk_ev = syrk_fn(exec_q, transA, n, k, a_typeless_ptr, lda,
+                                      r_typeless_ptr, ldc, depends);
 #else
     sycl::event syrk_ev = syrk_fn(exec_q, transA, n, k, a_typeless_ptr, lda,
                                   r_typeless_ptr, ldc, is_row_major, depends);
 #endif // USE_ONEMATH_CUBLAS
 
-    sycl::event args_ev =
-        dpctl::utils::keep_args_alive(exec_q, {matrixA, resultC}, {syrk_ev});
+        sycl::event args_ev = dpctl::utils::keep_args_alive(
+            exec_q, {matrixA, resultC}, {syrk_ev});
 
-    return std::make_pair(args_ev, syrk_ev);
+        return std::make_pair(args_ev, syrk_ev);
 }
 
 template <typename fnT, typename varT>
@@ -290,12 +382,13 @@ struct SyrkContigFactory
     {
         if constexpr (types::SyrkTypePairSupportFactory<varT>::is_defined) {
             return syrk_impl<varT>;
-        }
-        else {
-            return nullptr;
-        }
-    }
-};
+}
+else {
+    return nullptr;
+}
+} // namespace dpnp::extensions::blas
+}
+;
 
 void init_syrk_dispatch_vector(void)
 {
