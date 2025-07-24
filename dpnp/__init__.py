d280393c2dfb405d5a85229686aef3245bf317bb
Anton Volkov <antonwolfy@gmail.com>
2025-07-24 17:13:14 +0200

    Disable the w/a with OneMKL hanging

--- dpnp/__init__.py
+++ dpnp/__init__.py
@@ -30,7 +30,7 @@ import sys
 mypath = os.path.dirname(os.path.realpath(__file__))
 
 # workaround against hanging in OneMKL calls and in DPCTL
-os.environ.setdefault("SYCL_QUEUE_THREAD_POOL_SIZE", "6")
+# os.environ.setdefault("SYCL_QUEUE_THREAD_POOL_SIZE", "6")
 
 import dpctl
 
