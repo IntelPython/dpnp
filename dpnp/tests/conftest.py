21fc956a7193dd5dab9179700ec1b10dc1abc3ff
Anton Volkov <antonwolfy@gmail.com>
2025-09-09 18:56:31 +0200

    Print device ID with zero-padded to 4 digits

--- dpnp/tests/conftest.py
+++ dpnp/tests/conftest.py
@@ -140,7 +140,7 @@ def pytest_collection_modifyitems(config, items):
     print(
         f"DPNP Test scope includes all integer dtypes: {bool(dtype_config.all_int_types)}"
     )
-    print(f"DPNP current device ID: {get_dev_id(dev):#x}")
+    print(f"DPNP current device ID: 0x{get_dev_id(dev):04X}")
     print(f"DPNP current device is CPU: {is_cpu}")
     print(f"DPNP current device is GPU: {is_gpu}")
     print(f"DPNP current device supports fp64: {support_fp64}")
