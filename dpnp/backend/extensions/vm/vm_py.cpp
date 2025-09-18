b7a13d03b1ff88d8c8eabecda920e6667579cc98
Anton Volkov <antonwolfy@gmail.com>
2025-09-18 12:33:54 +0200

    Add erfc implementation to VM extension

--- dpnp/backend/extensions/vm/vm_py.cpp
+++ dpnp/backend/extensions/vm/vm_py.cpp
@@ -46,6 +46,7 @@
 #include "cosh.hpp"
 #include "div.hpp"
 #include "erf.hpp"
+#include "erfc.hpp"
 #include "exp.hpp"
 #include "exp2.hpp"
 #include "expm1.hpp"
@@ -99,6 +100,7 @@ PYBIND11_MODULE(_vm_impl, m)
     vm_ns::init_cosh(m);
     vm_ns::init_div(m);
     vm_ns::init_erf(m);
+    vm_ns::init_erfc(m);
     vm_ns::init_exp(m);
     vm_ns::init_exp2(m);
     vm_ns::init_expm1(m);
