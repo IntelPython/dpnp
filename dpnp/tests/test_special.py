f5f5841c53696315f499abacb1ee32306f01e002
Anton Volkov <antonwolfy@gmail.com>
2025-09-23 15:51:20 +0200

    Add tests to cover

--- dpnp/tests/test_special.py
+++ dpnp/tests/test_special.py
@@ -14,18 +14,19 @@ from .third_party.cupy.testing import installed, with_requires
 
 
 @with_requires("scipy")
-class TestErf:
+@pytest.mark.parametrize("func", ["erf", "erfc"])
+class TestCommon:
     @pytest.mark.parametrize(
         "dt", get_all_dtypes(no_none=True, no_float16=False, no_complex=True)
     )
-    def test_basic(self, dt):
+    def test_basic(self, func, dt):
         import scipy.special
 
         a = generate_random_numpy_array((2, 5), dtype=dt)
         ia = dpnp.array(a)
 
-        result = dpnp.special.erf(ia)
-        expected = scipy.special.erf(a)
+        result = getattr(dpnp.special, func)(ia)
+        expected = getattr(scipy.special, func)(a)
 
         # scipy >= 0.16.0 returns float64, but dpnp returns float32
         to_float32 = dt in (dpnp.bool, dpnp.float16)
@@ -34,29 +35,48 @@ class TestErf:
             result, expected, check_only_type_kind=only_type_kind
         )
 
-    def test_nan_inf(self):
+    def test_nan_inf(self, func):
         import scipy.special
 
         a = numpy.array([numpy.nan, -numpy.inf, numpy.inf])
         ia = dpnp.array(a)
 
-        result = dpnp.special.erf(ia)
-        expected = scipy.special.erf(a)
+        result = getattr(dpnp.special, func)(ia)
+        expected = getattr(scipy.special, func)(a)
         assert_allclose(result, expected)
 
-    def test_zeros(self):
+    def test_zeros(self, func):
         import scipy.special
 
         a = numpy.array([0.0, -0.0])
         ia = dpnp.array(a)
 
-        result = dpnp.special.erf(ia)
-        expected = scipy.special.erf(a)
+        result = getattr(dpnp.special, func)(ia)
+        expected = getattr(scipy.special, func)(a)
         assert_allclose(result, expected)
         assert_equal(dpnp.signbit(result), numpy.signbit(expected))
 
     @pytest.mark.parametrize("dt", get_complex_dtypes())
-    def test_complex(self, dt):
+    def test_complex(self, func, dt):
         x = dpnp.empty(5, dtype=dt)
         with pytest.raises(ValueError):
-            dpnp.special.erf(x)
+            getattr(dpnp.special, func)(x)
+
+
+class TestConsistency:
+
+    def test_erfc(self):
+        # TODO: replace with dpnp.random.RandomState, once pareto is added
+        rng = numpy.random.RandomState(1234)
+        n = 10000
+        a = rng.pareto(0.02, n) * (2 * rng.randint(0, 2, n) - 1)
+        a = dpnp.array(a)
+
+        res = 1 - dpnp.special.erf(a)
+        mask = dpnp.isfinite(res)
+        a = a[mask]
+
+        tol = 8 * dpnp.finfo(a).resolution
+        assert dpnp.allclose(
+            dpnp.special.erfc(a), res[mask], rtol=tol, atol=tol
+        )
--- dpnp/tests/test_usm_type.py
+++ dpnp/tests/test_usm_type.py
@@ -1296,10 +1296,11 @@ def test_choose(usm_type_x, usm_type_ind):
     assert z.usm_type == du.get_coerced_usm_type([usm_type_x, usm_type_ind])
 
 
+@pytest.mark.parametrize("func", ["erf", "erfc"])
 @pytest.mark.parametrize("usm_type", list_of_usm_types)
-def test_erf(usm_type):
+def test_erf_funcs(func, usm_type):
     x = dpnp.linspace(-3, 3, num=5, usm_type=usm_type)
-    y = dpnp.special.erf(x)
+    y = getattr(dpnp.special, func)(x)
     assert x.usm_type == y.usm_type == usm_type
 
 
