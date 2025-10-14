4e1ba3649d2e8ed0f100dc015ec9a98097499d49
Anton <100830759+antonwolfy@users.noreply.github.com>
2025-10-14 14:41:30 +0200

    Apply suggestions from code review
    Co-authored-by: vlad-perevezentsev <vladislav.perevezentsev@intel.com>

--- dpnp/dpnp_iface_arraycreation.py
+++ dpnp/dpnp_iface_arraycreation.py
@@ -2234,12 +2234,12 @@ def from_dlpack(x, /, *, device=None, copy=None):
     Raises
     ------
     TypeError
-        if `x` does not implement ``__dlpack__`` method
+        If `x` does not implement ``__dlpack__`` method.
     ValueError
-        if data of the input object resides on an unsupported device
+        If data of the input object resides on an unsupported device.
     DLPackCreationError
-        when `x` is allocated on a partitioned SYCL device, or with
-        a non-default context
+        When `x` is allocated on a partitioned SYCL device, or with
+        a non-default context.
 
     Notes
     -----
