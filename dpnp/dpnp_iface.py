defce442c878a58d4dafc1fe55df74b5831e1cca
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2025-11-12 05:18:57 -0800

    Move manipulation function exports from dpnp_iface.py to __init__.py

--- dpnp/dpnp_iface.py
+++ dpnp/dpnp_iface.py
@@ -76,8 +76,6 @@ __all__ = [
     "synchronize_array_data",
 ]
 
-from dpnp.dpnp_iface_manipulation import *
-from dpnp.dpnp_iface_manipulation import __all__ as __all__manipulation
 from dpnp.dpnp_iface_mathematical import *
 from dpnp.dpnp_iface_mathematical import __all__ as __all__mathematical
 from dpnp.dpnp_iface_nanfunctions import *
@@ -100,7 +98,6 @@ from .dpnp_utils import (
     use_origin_backend,
 )
 
-__all__ += __all__manipulation
 __all__ += __all__mathematical
 __all__ += __all__nanfunctions
 __all__ += __all__searching
