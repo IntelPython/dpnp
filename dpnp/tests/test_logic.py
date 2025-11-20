c5aff447c6d8990c014495142f19f0122d977c4c
Nikita Grigorian <nikita.grigorian@intel.com>
2025-11-19 19:03:58 -0800

    add isin tests

--- dpnp/tests/test_logic.py
+++ dpnp/tests/test_logic.py
@@ -795,3 +795,100 @@ def test_array_equal_nan(a):
     result = dpnp.array_equal(dpnp.array(a), dpnp.array(b), equal_nan=True)
     expected = numpy.array_equal(a, b, equal_nan=True)
     assert_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "a",
+    [
+        numpy.array([1, 2, 3, 4]),
+        numpy.array([[1, 2], [3, 4]]),
+    ],
+)
+@pytest.mark.parametrize(
+    "b",
+    [
+        numpy.array([2, 4, 6]),
+        numpy.array([[1, 3], [5, 7]]),
+    ],
+)
+def test_isin_basic(a, b):
+    dp_a = dpnp.array(a)
+    dp_b = dpnp.array(b)
+
+    expected = numpy.isin(a, b)
+    result = dpnp.isin(dp_a, dp_b)
+    assert_equal(result, expected)
+
+
+@pytest.mark.parametrize("dtype", get_all_dtypes())
+def test_isin_dtype(dtype):
+    a = numpy.array([1, 2, 3, 4], dtype=dtype)
+    b = numpy.array([2, 4], dtype=dtype)
+
+    dp_a = dpnp.array(a, dtype=dtype)
+    dp_b = dpnp.array(b, dtype=dtype)
+
+    expected = numpy.isin(a, b)
+    result = dpnp.isin(dp_a, dp_b)
+    assert_equal(result, expected)
+
+
+@pytest.mark.parametrize("sh_a, sh_b", [((3, 1), (1, 4)), ((2, 3, 1), (1, 1))])
+def test_isin_broadcast(sh_a, sh_b):
+    a = numpy.arange(numpy.prod(sh_a)).reshape(sh_a)
+    b = numpy.arange(numpy.prod(sh_b)).reshape(sh_b)
+
+    dp_a = dpnp.array(a)
+    dp_b = dpnp.array(b)
+
+    expected = numpy.isin(a, b)
+    result = dpnp.isin(dp_a, dp_b)
+    assert_equal(result, expected)
+
+
+def test_isin_scalar_elements():
+    a = numpy.array([1, 2, 3])
+    b = 2
+
+    dp_a = dpnp.array(a)
+    dp_b = dpnp.array(b)
+
+    expected = numpy.isin(a, b)
+    result = dpnp.isin(dp_a, dp_b)
+    assert_equal(result, expected)
+
+
+def test_isin_scalar_test_elements():
+    a = 2
+    b = numpy.array([1, 2, 3])
+
+    dp_a = dpnp.array(a)
+    dp_b = dpnp.array(b)
+
+    expected = numpy.isin(a, b)
+    result = dpnp.isin(dp_a, dp_b)
+    assert_equal(result, expected)
+
+
+def test_isin_empty():
+    a = numpy.array([], dtype=int)
+    b = numpy.array([1, 2, 3])
+
+    dp_a = dpnp.array(a)
+    dp_b = dpnp.array(b)
+
+    expected = numpy.isin(a, b)
+    result = dpnp.isin(dp_a, dp_b)
+    assert_equal(result, expected)
+
+
+def test_isin_errors():
+    a = dpnp.arange(5)
+    b = dpnp.arange(3)
+
+    # unsupported type for elements or test_elements
+    with pytest.raises(TypeError):
+        dpnp.isin(dict(), b)
+
+    with pytest.raises(TypeError):
+        dpnp.isin(a, dict())
