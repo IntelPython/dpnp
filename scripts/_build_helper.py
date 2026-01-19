5066b3f26653c332306a1da45c7b672b37df140c
Anton Volkov <antonwolfy@gmail.com>
2026-01-19 05:51:05 -0800

    Use sys.executable when launching Python from Python

--- scripts/_build_helper.py
+++ scripts/_build_helper.py
@@ -42,7 +42,7 @@ def get_dpctl_cmake_dir():
     """
 
     process = subprocess.Popen(
-        ["python", "-m", "dpctl", "--cmakedir"],
+        [sys.executable, "-m", "dpctl", "--cmakedir"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
