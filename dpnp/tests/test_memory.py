622876545b1a92ff47976e482a4689fd0a79d25e
Anton Volkov <antonwolfy@gmail.com>
2026-03-11 13:26:13 +0100

    Add tests to cover the issue

--- dpnp/tests/test_memory.py
+++ dpnp/tests/test_memory.py
@@ -42,3 +42,62 @@ class TestCreateData:
         pl = dpnp.ndarray((n, m), dtype=a.dtype, buffer=sl)
         assert pl.data.ptr == sl.data.ptr
         assert a.data.ptr != sl.data.ptr
+
+    def test_slices_2d(self):
+        # Create 2D array and verify slices have different pointers
+        a = dpnp.arange(12, dtype=float).reshape(3, 4)
+
+        # Each row should have a different pointer
+        row0_ptr = a[0].data.ptr
+        row1_ptr = a[1].data.ptr
+        row2_ptr = a[2].data.ptr
+
+        assert (
+            row0_ptr != row1_ptr
+        ), "a[0] and a[1] should have different pointers"
+        assert (
+            row1_ptr != row2_ptr
+        ), "a[1] and a[2] should have different pointers"
+
+        # Check byte offsets match expected stride
+        stride = a.strides[0]  # stride between rows in bytes
+        assert row1_ptr - row0_ptr == stride
+        assert row2_ptr - row1_ptr == stride
+
+    def test_slices_multidimensional(self):
+        # 3D array
+        a = dpnp.zeros((5, 10, 20), dtype=dpnp.int32)
+
+        # Different slices along first axis should have different pointers
+        slice0_ptr = a[0].data.ptr
+        slice1_ptr = a[1].data.ptr
+
+        assert slice0_ptr != slice1_ptr
+        assert slice1_ptr - slice0_ptr == a.strides[0]
+
+    def test_repeated_access(self):
+        a = dpnp.arange(20).reshape(4, 5)
+
+        # Multiple accesses to same slice should give same ptr value
+        ptr1 = a[2].data.ptr
+        ptr2 = a[2].data.ptr
+
+        assert ptr1 == ptr2, "Same slice should have consistent ptr value"
+
+        # But different slices should have different ptrs
+        assert a[0].data.ptr != a[2].data.ptr
+
+    def test_array_on_view_with_slicing(self):
+        # Original array
+        a = dpnp.arange(24, dtype=dpnp.float32).reshape(6, 4)
+
+        # Create view using slicing
+        view = a[2:5]
+
+        # Construct new array from view
+        new_arr = dpnp.ndarray(view.shape, dtype=view.dtype, buffer=view)
+
+        # Pointers should match
+        assert new_arr.data.ptr == view.data.ptr
+        # And should be different from base array
+        assert new_arr.data.ptr != a.data.ptr
