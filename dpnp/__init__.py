9ed637e77de583f9b35122eecbe9711b7b07ab0d
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2026-03-31 03:21:29 -0700

    Remove the remaining dpctl_ext imports

--- dpnp/__init__.py
+++ dpnp/__init__.py
@@ -60,9 +60,7 @@ if sys.platform == "win32":  # pragma: no cover
                 [os.getenv("PATH", ""), dll_path]
             )
 
-# TODO: revert to `from dpctl.tensor...`
-# when dpnp fully migrates dpctl/tensor
-from dpctl_ext.tensor import __array_api_version__, DLDeviceType
+from dpnp.tensor import __array_api_version__, DLDeviceType
 
 from .dpnp_array import dpnp_array as ndarray
 from .dpnp_array_api_info import __array_namespace_info__
