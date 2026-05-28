7530aa9716a18d38331336743353bc66ab9b9372
Vladislav Perevezentsev <vladislav.perevezentsev@intel.com>
2026-05-28 04:47:57 -0700

    Comment lines for future dpnp logo

--- doc/conf.py
+++ doc/conf.py
@@ -132,8 +132,10 @@ html_theme_options = {}
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_logo = "_static/dpnp.svg"
-html_favicon = "_static/dpnp.svg"
+# html_logo = "_static/dpnp.svg"
+# html_favicon = "_static/dpnp.svg"
+html_css_files = ["custom.css"]
+html_js_files = ["custom.js"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
