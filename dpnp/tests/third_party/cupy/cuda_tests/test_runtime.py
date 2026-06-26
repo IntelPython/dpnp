f7a12a31386da07c2382491d586e72eb3db37620
Anton Volkov <antonwolfy@gmail.com>
2026-06-26 12:39:08 +0200

    Update test_assumed_runtime_version

---
+++ dpnp/tests/third_party/cupy/cuda_tests/test_runtime.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+import pickle
+import sys
+
+import pytest
+
+import dpnp as cupy
+
+pytest.skip(
+    "cupy.cuda.runtime / nvrtc internals are not supported",
+    allow_module_level=True,
+)
+
+# CUDA runtime internals have no dpnp equivalent
+# from cupy.cuda import driver
+# from cupy.cuda import nvrtc
+# from cupy.cuda import runtime
+
+
+class TestExceptionPicklable:
+
+    def test(self):
+        e1 = runtime.CUDARuntimeError(1)
+        e2 = pickle.loads(pickle.dumps(e1))
+        assert e1.args == e2.args
+        assert str(e1) == str(e2)
+
+
+class TestMemPool:
+
+    @pytest.mark.skipif(
+        runtime.is_hip, reason="HIP does not support async allocator"
+    )
+    @pytest.mark.skipif(
+        driver._is_cuda_python() and runtime.runtimeGetVersion() < 11020,
+        reason="cudaMemPool_t is supported since CUDA 11.2",
+    )
+    @pytest.mark.skipif(
+        not driver._is_cuda_python() and driver.get_build_version() < 11020,
+        reason="cudaMemPool_t is supported since CUDA 11.2",
+    )
+    @pytest.mark.skipif(
+        runtime.deviceGetAttribute(runtime.cudaDevAttrMemoryPoolsSupported, 0)
+        == 0,
+        reason="cudaMemPool_t is not supported on device 0",
+    )
+    def test_mallocFromPoolAsync(self):
+        # also test create/destroy a pool
+        props = runtime.MemPoolProps(
+            runtime.cudaMemAllocationTypePinned,
+            runtime.cudaMemHandleTypeNone,
+            runtime.cudaMemLocationTypeDevice,
+            0,
+        )  # on device 0
+        pool = runtime.memPoolCreate(props)
+        assert pool > 0
+        s = cupy.cuda.Stream()
+        ptr = runtime.mallocFromPoolAsync(128, pool, s.ptr)
+        assert ptr > 0
+        runtime.freeAsync(ptr, s.ptr)
+        runtime.memPoolDestroy(pool)
+
+
+@pytest.mark.skipif(
+    runtime.is_hip, reason="This assumption is correct only in CUDA"
+)
+def test_assumed_runtime_version():
+    # When CUDA Python is enabled, CuPy calculates the CUDA runtime version
+    # from NVRTC version. This test ensures that the assumption is correct
+    # by running the same logic in non-CUDA Python environment.
+    # When this fails, `runtime.runtimeGetVersion()` logic needs to be fixed.
+    major, minor = nvrtc.getVersion()
+    local_ver = runtime._getLocalRuntimeVersion()
+    # On Windows, starting from CUDA 13.0, cudaRuntimeGetVersion() always
+    # returns major * 1000 regardless of the minor version (nvbugs 5955788,
+    # 5523579). Accept either form on Windows + CUDA >= 13.
+    if sys.platform == "win32" and major >= 13:
+        assert local_ver in (major * 1000, major * 1000 + minor * 10)
+    else:
+        assert local_ver == major * 1000 + minor * 10
+
+
+def test_major_version():
+    major = runtime._getCUDAMajorVersion()
+    if runtime.is_hip:
+        assert major == 0
+    else:
+        assert 10 < major < 20
